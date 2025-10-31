b91f3fbc06554e81019d452037ac5ab6b1630964
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-01 07:45:42 +0900

    [update]マネージャーを追加

---
+++ Assets/OldAssets/TPS_Mock/Scripts/PlayerManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Mock.TPS
+{
+    public class PlayerManager : MonoBehaviour
+    {
+        private PlayerMover _playerMover;
+
+        private void OnEnable()
+        {
+            _playerMover = new PlayerMover(transform, Camera.main.transform);
+        }
+
+        private void Update()
+        {
+            Vector3 inputDirection = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+            transform.position += _playerMover.CalcPlayerVelocityByInputDirection(in inputDirection);
+        }
+    }
+}
