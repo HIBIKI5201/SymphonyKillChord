8b0a3c6c17350a0690fa9a9666100d1a21ec59a2
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-01 07:55:32 +0900

    [add]入力バッファを追加

---
+++ Assets/OldAssets/TPS_Mock/Scripts/InputBuffer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Mock.TPS
+{
+    /// <summary>
+    ///     入力バッファクラス。
+    /// </summary>
+    public class InputBuffer : MonoBehaviour
+    {
+        /// <summary> 視点移動方向プロパティ </summary>
+        public Vector2 LookDirection => _lookDirection;
+
+        /// <summary> 移動方向プロパティ </summary>
+        public Vector3 MoveDirection => _moveDirection;
+
+        private Vector2 _lookDirection;
+        private Vector3 _moveDirection;
+
+        private void Update()
+        {
+            _moveDirection = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+            _moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        }
+    }
+}
