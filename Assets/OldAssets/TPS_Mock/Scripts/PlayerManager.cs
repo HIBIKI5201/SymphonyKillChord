da3b0e814d926e3b270edb7e3485177878551536
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-01 07:56:16 +0900

    [update]入力バッファから移動入力を受け取るように

--- Assets/OldAssets/TPS_Mock/Scripts/PlayerManager.cs
+++ Assets/OldAssets/TPS_Mock/Scripts/PlayerManager.cs
@@ -2,18 +2,23 @@
 
 namespace Mock.TPS
 {
+    /// <summary>
+    ///     プレイヤーの管理クラス。
+    /// </summary>
     public class PlayerManager : MonoBehaviour
     {
+        private InputBuffer _inputBuffer;
         private PlayerMover _playerMover;
 
         private void OnEnable()
         {
             _playerMover = new PlayerMover(transform, Camera.main.transform);
+            _inputBuffer = FindAnyObjectByType<InputBuffer>();
         }
 
         private void Update()
         {
-            Vector3 inputDirection = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+            Vector3 inputDirection = _inputBuffer.MoveDirection;
             transform.position += _playerMover.CalcPlayerVelocityByInputDirection(in inputDirection);
         }
     }
