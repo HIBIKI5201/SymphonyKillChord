6ec96f9fcae6070e88d95dcfcefa4ddf6634eda5
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-06 19:51:42 +0900

    [update]キャラクターのダメージヒットメソッドを追加

--- Assets/OldAssets/TPS_Mock/Scripts/PlayerManager.cs
+++ Assets/OldAssets/TPS_Mock/Scripts/PlayerManager.cs
@@ -6,9 +6,9 @@ namespace Mock.TPS
     ///     プレイヤーの管理クラス。
     /// </summary>
     [RequireComponent(typeof(Rigidbody))]
-    public class PlayerManager : MonoBehaviour
+    public class PlayerManager : MonoBehaviour, ICharacter
     {
-        public void Init(InputBuffer inputBuffer, CameraManager cameraManager)
+        public void Init(InputBuffer inputBuffer, CameraManager cameraManager, HealthbarManager healthbarManager)
         {
             _inputBuffer = inputBuffer;
 
@@ -17,7 +17,11 @@ namespace Mock.TPS
             _playerMover = new PlayerMover(_playerStatus, _config,
                 transform, Camera.main.transform, rb);
             _playerAttacker = new PlayerAttacker(_playerStatus, cameraManager.transform);
+            _healthEntity = new HealthEntity(_playerStatus.MaxHealth);
+            _healthEntity.OnHealthChanged += healthbarManager.SetHealthBar;
         }
+
+        public void TakeDamage(float damage) => _healthEntity.TakeDamage(damage);
 
         [SerializeField, Tooltip("プレイヤーのステータス")]
         private PlayerStatus _playerStatus;
@@ -28,6 +32,7 @@ namespace Mock.TPS
         private InputBuffer _inputBuffer;
         private PlayerMover _playerMover;
         private PlayerAttacker _playerAttacker;
+        private HealthEntity _healthEntity;
 
         private Vector3 _moveInput;
 
