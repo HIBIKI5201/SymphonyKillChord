5cca362292ecc5cbbebafaec16203c848977a2d8
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-06 20:17:54 +0900

    [add]敵のコンテナを追加

---
+++ Assets/OldAssets/TPS_Mock/Scripts/EnemyContainer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Mock.TPS
+{
+    public class EnemyContainer : MonoBehaviour
+    {
+        private EnemyManager[] _enemies;
+
+        private void Awake()
+        {
+            _enemies = FindObjectsByType<EnemyManager>(FindObjectsSortMode.None);
+        }
+    }
+}
