dae7cf8d43311547bbb61a3725c811acdd662ef2
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-06 17:46:00 +0900

    [update]カメラを移動する

--- Assets/OldAssets/TPS_Mock/Scripts/CameraMover.cs
+++ Assets/OldAssets/TPS_Mock/Scripts/CameraMover.cs
@@ -15,6 +15,14 @@ namespace Mock.TPS
             _target = target;
         }
 
+        public void Update()
+        {
+            // カメラ位置をターゲットのオフセット位置に設定。
+            Vector3 targetPosition = _target.position + _config.CameraOffset;
+            _camera.position = Vector3.Lerp(_camera.position, targetPosition,
+                Mathf.Clamp01(Time.deltaTime * _config.CameraRotationSpeed));
+        }
+
         private readonly CameraConfig _config;
         private readonly Transform _camera;
         private readonly Transform _target;
