f96c65af4c4150f464d80ef6afd88cf9fb5c10f1
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-06 18:42:58 +0900

    [add]攻撃クラスを追加

---
+++ Assets/OldAssets/TPS_Mock/Scripts/PlayerAttacker.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Mock.TPS
+{
+    public class PlayerAttacker
+    {
+        public PlayerAttacker(PlayerStatus status)
+        {
+            _status = status;
+        }
+
+        public void FindAttackTarget(Vector3 origin, Vector3 direction)
+        {
+            if (Physics.Raycast(origin, direction, out RaycastHit hitInfo, _status.AttackRange))
+            {
+
+            }
+        }
+
+        private readonly PlayerStatus _status;
+    }
+}
