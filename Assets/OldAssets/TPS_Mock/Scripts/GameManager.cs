6b07a86d025ede6caf216bb868140b8de0ff1c53
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-06 18:33:08 +0900

    [add]ゲームマネージャーを追加

---
+++ Assets/OldAssets/TPS_Mock/Scripts/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Mock.TPS
+{
+    /// <summary>
+    ///     ゲーム全体の管理クラス。
+    /// </summary>
+    public class GameManager : MonoBehaviour
+    {
+        [SerializeField]
+        private InputBuffer _inputBuffer;
+    }
+}
