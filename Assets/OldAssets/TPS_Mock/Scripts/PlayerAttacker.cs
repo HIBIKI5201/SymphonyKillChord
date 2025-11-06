1efc8e6abb4abea8d5202ebf93063fb1518e84d4
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-06 19:21:28 +0900

    [update]攻撃対象をカメラから探すように

--- Assets/OldAssets/TPS_Mock/Scripts/PlayerAttacker.cs
+++ Assets/OldAssets/TPS_Mock/Scripts/PlayerAttacker.cs
@@ -4,19 +4,30 @@ namespace Mock.TPS
 {
     public class PlayerAttacker
     {
-        public PlayerAttacker(PlayerStatus status)
+        public PlayerAttacker(PlayerStatus status, Transform camera)
         {
             _status = status;
+            _camera = camera;
         }
 
-        public void FindAttackTarget(Vector3 origin, Vector3 direction)
+        public void Attack()
+        {
+            ICharacter target = FindAttackTarget(_camera.position, _camera.forward);
+            if (target == null) { return; }
+            Debug.Log($"Attack Target: {target.gameObject.name}");
+        }
+
+        public ICharacter FindAttackTarget(Vector3 origin, Vector3 direction)
         {
             if (Physics.Raycast(origin, direction, out RaycastHit hitInfo, _status.AttackRange))
             {
-
+                return hitInfo.transform.GetComponent<ICharacter>();
             }
+
+            return null;
         }
 
         private readonly PlayerStatus _status;
+        private readonly Transform _camera;
     }
 }
