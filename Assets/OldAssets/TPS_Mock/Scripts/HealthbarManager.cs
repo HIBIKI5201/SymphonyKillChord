826db6b37cb9b466a15bb4999de0e7dd370b4dba
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-06 19:46:03 +0900

    [update]ゲージを変更するメソッドを追加

---
+++ Assets/OldAssets/TPS_Mock/Scripts/HealthbarManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Mock.TPS
+{
+    public class HealthbarManager : MonoBehaviour
+    {
+        [SerializeField]
+        private Image _healthBar;
+
+        public void SetHealthBar(float currentHealth, float maxHealth)
+        {
+            _healthBar.fillAmount = currentHealth / maxHealth;
+        }
+    }
+}
