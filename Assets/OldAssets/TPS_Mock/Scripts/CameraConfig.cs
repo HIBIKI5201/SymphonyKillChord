0f4ef5b923c3830e5e7908b0012ae6eaf2ad4343
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-06 17:46:36 +0900

    [update]コンフィグにカメラのパラメータを追加

--- Assets/OldAssets/TPS_Mock/Scripts/CameraConfig.cs
+++ Assets/OldAssets/TPS_Mock/Scripts/CameraConfig.cs
@@ -8,6 +8,12 @@ namespace Mock.TPS
     [CreateAssetMenu(fileName = nameof(CameraConfig), menuName = "Mock/TPS/" + nameof(CameraConfig))]
     public class CameraConfig : ScriptableObject
     {
+        public float CameraRotationSpeed => _cameraRotationSpeed;
+        public Vector3 CameraOffset => _cameraOffset;
 
+        [SerializeField]
+        private float _cameraRotationSpeed = 3f;
+        [SerializeField]
+        private Vector3 _cameraOffset = new Vector3(0f, 2f, -4f);
     }
 }
