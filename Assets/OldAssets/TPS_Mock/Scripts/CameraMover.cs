6c512567ff47a582a4182f4728f0e991224a92b0
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-06 19:33:12 +0900

    [update]縦回転させる

--- Assets/OldAssets/TPS_Mock/Scripts/CameraMover.cs
+++ Assets/OldAssets/TPS_Mock/Scripts/CameraMover.cs
@@ -22,18 +22,18 @@ namespace Mock.TPS
 
         public void RotateCamera(Vector2 input)
         {
-            float cameraRotation = input.x * _config.CameraRotationSpeed;
+            float yaw = input.x * _config.CameraRotationSpeed;
+            float pitch = -input.y * _config.CameraRotationSpeed;
 
-            _currentCameraRotation = Quaternion.Euler(
-                0, _currentCameraRotation.eulerAngles.y + cameraRotation, 0);
+            _currentPitch = Mathf.Clamp(_currentPitch + pitch, _config.PicthRange.x, _config.PicthRange.y);
+            _currentYaw += yaw;
+
+            _currentCameraRotation = Quaternion.Euler(_currentPitch, _currentYaw, 0f);
         }
 
         private void UpdateMoveCamera()
         {
-            // カメラオフセットを回転させる。
             Vector3 rotatedCameraOffset = _currentCameraRotation * _config.CameraOffset;
-
-            // ターゲット位置からのカメラ目標位置を計算。
             Vector3 targetPosition = _target.position + rotatedCameraOffset;
 
             _camera.position = Vector3.Lerp(
@@ -45,14 +45,10 @@ namespace Mock.TPS
 
         private void UpdateRotateCamera()
         {
-            // 注視点オフセットを回転させる。
             Vector3 rotatedLookAtOffset = _currentCameraRotation * _config.CameraLookAtOffset;
-
-            // 注視点方向を算出。
             Vector3 currentLookAtPosition = _target.position + rotatedLookAtOffset;
-            Quaternion targetRotation = Quaternion.LookRotation(
-                currentLookAtPosition - _camera.position);
 
+            Quaternion targetRotation = Quaternion.LookRotation(currentLookAtPosition - _camera.position);
             _camera.rotation = Quaternion.Slerp(
                 _camera.rotation,
                 targetRotation,
@@ -64,6 +60,8 @@ namespace Mock.TPS
         private readonly Transform _camera;
         private readonly Transform _target;
 
+        private float _currentYaw = 0f;
+        private float _currentPitch = 0f;
         private Quaternion _currentCameraRotation = Quaternion.identity;
     }
 }
