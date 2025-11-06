49594bf4a17e8c6d3c96026125b5e5a6062f523d
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-06 20:11:07 +0900

    [update]ワールド座標の位置に移動するように

--- Assets/OldAssets/TPS_Mock/Scripts/HealthbarManager.cs
+++ Assets/OldAssets/TPS_Mock/Scripts/HealthbarManager.cs
@@ -6,11 +6,19 @@ namespace Mock.TPS
     public class HealthbarManager : MonoBehaviour
     {
         [SerializeField]
-        private Image _healthBar;
+        private Image _bar;
+        [SerializeField]
+        private Image _guage;
 
         public void SetHealthBar(float currentHealth, float maxHealth)
         {
-            _healthBar.fillAmount = currentHealth / maxHealth;
+            _guage.fillAmount = currentHealth / maxHealth;
+        }
+
+        public void MovePosition(Vector3 worldPosition)
+        {
+            Vector2 screenPoint = Camera.main.WorldToScreenPoint(worldPosition);
+            _bar.rectTransform.position = screenPoint;
         }
     }
 }
