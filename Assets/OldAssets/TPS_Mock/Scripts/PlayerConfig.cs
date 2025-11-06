17f9d7030112c108531e080afc3aee84173bd601
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-06 16:02:17 +0900

    [add]プレイヤーの設定クラスを追加

---
+++ Assets/OldAssets/TPS_Mock/Scripts/PlayerConfig.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Mock.TPS
+{
+    /// <summary>
+    ///     プレイヤーの設定クラス。
+    /// </summary>
+    [CreateAssetMenu(fileName = nameof(PlayerConfig), menuName = "Mock/TPS/" + nameof(PlayerConfig))]
+    public class PlayerConfig :ScriptableObject
+    {
+        public bool IsCameraFlipX => _isCameraFlipX;
+
+        [SerializeField, Tooltip("カメラのX回転を反転")]
+        private bool _isCameraFlipX;
+    }
+}
