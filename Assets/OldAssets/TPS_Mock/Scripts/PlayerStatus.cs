f89047b956f904c4b474f30bd0f8827d0a0f06aa
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-06 15:58:50 +0900

    [add]ステータスクラスを追加

---
+++ Assets/OldAssets/TPS_Mock/Scripts/PlayerStatus.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace Mock.TPS
+{
+    /// <summary>
+    ///     プレイヤーのステータスクラス。
+    /// </summary>
+    [Serializable]
+    public class PlayerStatus
+    {
+        public float MoveSpeed => _moveSpeed;
+        public float JumpForce => _jumpForce;
+        public float RotationSpeed => _rotationSpeed;
+
+        [SerializeField]
+        private float _moveSpeed = 5f;
+        [SerializeField]
+        private float _jumpForce = 5f;
+
+        [SerializeField]
+        private float _rotationSpeed = 3f;
+    }
+}
