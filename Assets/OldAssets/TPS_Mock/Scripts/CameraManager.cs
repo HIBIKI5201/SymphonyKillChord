bfc8e19899afdba709f3b14db334e74a228077a6
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-06 16:25:46 +0900

    [add]カメラの管理クラスを追加

---
+++ Assets/OldAssets/TPS_Mock/Scripts/CameraManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Mock.TPS
+{
+    /// <summary>
+    ///     カメラの管理クラス。
+    /// </summary>
+    public class CameraManager : MonoBehaviour
+    {
+        private CameraMover _mover;
+
+
+    }
+}
