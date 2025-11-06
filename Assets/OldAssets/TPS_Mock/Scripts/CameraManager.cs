03a9f7847de025111a916d55bc01a463379ed694
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-06 16:28:12 +0900

    [update]カメラを保証する

--- Assets/OldAssets/TPS_Mock/Scripts/CameraManager.cs
+++ Assets/OldAssets/TPS_Mock/Scripts/CameraManager.cs
@@ -1,14 +1,19 @@
-﻿using UnityEngine;
+﻿using Unity.Cinemachine;
+using UnityEngine;
 
 namespace Mock.TPS
 {
     /// <summary>
     ///     カメラの管理クラス。
     /// </summary>
+    [RequireComponent(typeof(CinemachineCamera))]
     public class CameraManager : MonoBehaviour
     {
         private CameraMover _mover;
 
-
+        private void Awake()
+        {
+            _mover = new CameraMover(transform);
+        }
     }
 }
