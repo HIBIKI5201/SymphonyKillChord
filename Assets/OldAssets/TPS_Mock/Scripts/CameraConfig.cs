2375d72077b53ab0875ff51b23a1a5a657f58e54
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-06 16:33:38 +0900

    [add]カメラのコンフィグクラスを追加

---
+++ Assets/OldAssets/TPS_Mock/Scripts/CameraConfig.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Mock.TPS
+{
+    /// <summary>
+    ///     カメラの設定クラス。
+    /// </summary>
+    [CreateAssetMenu(fileName = nameof(CameraConfig), menuName = "Mock/TPS/" + nameof(CameraConfig))]
+    public class CameraConfig : ScriptableObject
+    {
+
+    }
+}
