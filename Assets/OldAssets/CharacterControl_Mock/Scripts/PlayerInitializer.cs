70d8104c4d8917f48f6386d2eae2458bbe0b679c
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-11 22:27:03 +0900

    [update]入力登録を行う

--- Assets/OldAssets/CharacterControl_Mock/Scripts/PlayerInitializer.cs
+++ Assets/OldAssets/CharacterControl_Mock/Scripts/PlayerInitializer.cs
@@ -8,17 +8,18 @@ namespace Mock.CharacterControl
     public class PlayerInitializer : MonoBehaviour
     {
         [SerializeField]
+        private InputBuffer _inputBuffer;
+        [SerializeField]
         private SymphonyAnimeController _symphonyAnimeController;
 
         private PlayerManager _playerManager;
         private void Awake()
         {
+            if (_symphonyAnimeController == null) { return; }
             _playerManager = new(_symphonyAnimeController);
-        }
 
-        private void Update()
-        {
-            _playerManager?.Update();
+            if (_inputBuffer == null) { return; }
+            _playerManager.InputRegister(_inputBuffer);
         }
     }
 }
