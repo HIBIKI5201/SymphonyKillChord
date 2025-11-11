899fb53e6d8d69935fd3dbe6279d159ea01938f4
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-11 22:05:56 +0900

    [update]サマリーを追加

--- Assets/OldAssets/CharacterControl_Mock/Scripts/SymphonyAnimeController.cs
+++ Assets/OldAssets/CharacterControl_Mock/Scripts/SymphonyAnimeController.cs
@@ -2,12 +2,13 @@
 
 namespace Mock.CharacterControl
 {
+    /// <summary>
+    ///     Symphonyのアニメーションを管理するクラス
+    /// </summary>
     [DisallowMultipleComponent]
     [RequireComponent(typeof(Animator))]
     public class SymphonyAnimeController : MonoBehaviour
     {
-        
-
         private Animator _animator;
 
         private void Awake()
