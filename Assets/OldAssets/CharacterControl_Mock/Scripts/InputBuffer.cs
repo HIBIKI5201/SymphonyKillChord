c19c2ce67c56612aff7e235614149fc7c91254c5
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-11 22:26:05 +0900

    [add]インプットバッファを追加

---
+++ Assets/OldAssets/CharacterControl_Mock/Scripts/InputBuffer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Mock.CharacterControl
+{
+    [RequireComponent(typeof(PlayerInput))]
+    public class InputBuffer : MonoBehaviour
+    {
+        public InputAction MoveAction => _playerInput.actions[_moveActionName];
+
+        [SerializeField]
+        private string _moveActionName = "Move";
+
+        private PlayerInput _playerInput;
+
+        private void Awake()
+        {
+            _playerInput = GetComponent<PlayerInput>();
+        }
+    }
+}
