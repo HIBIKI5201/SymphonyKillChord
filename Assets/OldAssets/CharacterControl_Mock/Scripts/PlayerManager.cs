d30942ad9ef23d400a12eb3006d1cb7e2a3943d0
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-11 22:26:50 +0900

    [update]移動アクションを実装

--- Assets/OldAssets/CharacterControl_Mock/Scripts/PlayerManager.cs
+++ Assets/OldAssets/CharacterControl_Mock/Scripts/PlayerManager.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System.Threading;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
 
 namespace Mock.CharacterControl
 {
@@ -9,14 +12,32 @@ namespace Mock.CharacterControl
     {
         public PlayerManager(SymphonyAnimeController animeController)
         {
+            if (animeController == null) { return; }
+
             _animeController = animeController;
+            if (!animeController.TryGetComponent(out _rigidbody)) { Debug.LogError($"{animeController.name}に{nameof(Rigidbody)}がありません。"); }
+            if (!animeController.TryGetComponent(out _agent)) { Debug.LogError($"{animeController.name}に{nameof(NavMeshAgent)}がありません。"); }
         }
 
-        public void Update()
+        public void InputRegister(InputBuffer inputBuffer, CancellationToken token = default)
         {
+            inputBuffer.MoveAction.performed += HandleMove;
+            inputBuffer.MoveAction.canceled += HandleMove;
 
+            token.Register(() =>
+            {
+                inputBuffer.MoveAction.performed -= HandleMove;
+                inputBuffer.MoveAction.canceled -= HandleMove;
+            });
         }
 
         private readonly SymphonyAnimeController _animeController;
+        private readonly Rigidbody _rigidbody;
+        private readonly NavMeshAgent _agent;
+
+        private void HandleMove(InputAction.CallbackContext context)
+        {
+            Vector2 input = context.ReadValue<Vector2>();
+        }
     }
 }
