21b97d0aac445a1cb89f6267053b5f4732fdfdab
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-12 00:37:17 +0900

    [update]レシーバーを継承する

--- Assets/OldAssets/CharacterControl_Mock/Scripts/SymphonyAnimeController.cs
+++ Assets/OldAssets/CharacterControl_Mock/Scripts/SymphonyAnimeController.cs
@@ -7,8 +7,17 @@ namespace Mock.CharacterControl
     /// </summary>
     [DisallowMultipleComponent]
     [RequireComponent(typeof(Animator))]
-    public class SymphonyAnimeController : MonoBehaviour
+    public class SymphonyAnimeController : MonoBehaviour, IRootMotionReciever
     {
+        public bool IsRootMotion 
+        {
+            get => _animator.applyRootMotion;
+            set => _animator.applyRootMotion = value;
+        }
+
+        void IRootMotionReciever.ActiveRootMotion() => _animator.applyRootMotion = true;
+        void IRootMotionReciever.InactiveRootMotion() => _animator.applyRootMotion = false;
+
         private Animator _animator;
 
         private void Awake()
