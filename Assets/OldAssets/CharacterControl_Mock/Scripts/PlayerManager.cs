37218bc7788df1ea8ef367755ef630ef23902f9b
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-11 22:10:03 +0900

    [add]プレイヤーのマネージャークラスを追加

---
+++ Assets/OldAssets/CharacterControl_Mock/Scripts/PlayerManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Mock.CharacterControl
+{
+    /// <summary>
+    ///     プレイヤーの管理クラス。
+    /// </summary>
+    public class PlayerManager
+    {
+        public PlayerManager(SymphonyAnimeController animeController)
+        {
+            _animeController = animeController;
+        }
+
+        public void Update()
+        {
+
+        }
+
+        private readonly SymphonyAnimeController _animeController;
+    }
+}
