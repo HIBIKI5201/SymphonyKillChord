96fbe909384d4ba3689601c83de955eef7baf66a
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-11 22:31:02 +0900

    [update]ステータスを初期化するように

--- Assets/OldAssets/CharacterControl_Mock/Scripts/PlayerInitializer.cs
+++ Assets/OldAssets/CharacterControl_Mock/Scripts/PlayerInitializer.cs
@@ -8,6 +8,8 @@ namespace Mock.CharacterControl
     public class PlayerInitializer : MonoBehaviour
     {
         [SerializeField]
+        private PlayerStatus _status;
+        [SerializeField]
         private InputBuffer _inputBuffer;
         [SerializeField]
         private SymphonyAnimeController _symphonyAnimeController;
@@ -16,7 +18,8 @@ namespace Mock.CharacterControl
         private void Awake()
         {
             if (_symphonyAnimeController == null) { return; }
-            _playerManager = new(_symphonyAnimeController);
+            _playerManager = new(_status,
+                _symphonyAnimeController);
 
             if (_inputBuffer == null) { return; }
             _playerManager.InputRegister(_inputBuffer);
