90f44efc9c4748d7a56340756e2d724cc95fa230
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-11 22:05:46 +0900

    [add]プレイヤーの初期化クラスを追加

---
+++ Assets/OldAssets/CharacterControl_Mock/Scripts/PlayerInitializer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Mock.CharacterControl
+{
+    /// <summary>
+    ///     プレイヤーを初期化するクラス。
+    /// </summary>
+    public class PlayerInitializer : MonoBehaviour
+    {
+        [SerializeField]
+        private SymphonyAnimeController _symphonyAnimeController;
+
+
+    }
+}
