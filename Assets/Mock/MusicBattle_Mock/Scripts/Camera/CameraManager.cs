9dde57b88fce563fe5ae7f338eee2ea5501d3e94
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-19 23:52:43 +0900

    [update]モードごとに実行タイミングを変えられるように

--- Assets/Mock/MusicBattle_Mock/Scripts/Camera/CameraManager.cs
+++ Assets/Mock/MusicBattle_Mock/Scripts/Camera/CameraManager.cs
@@ -25,10 +25,40 @@ namespace Mock.MusicBattle.Camera
 
         public void ChangeUpdateMode(CameraUpdateModeEnum mode)
         {
-
+            _mode = mode;
         }
 
         private ILockOnTargetContainer _targetContainer;
+        private IInputBuffer _inputBuffer;
+
+        private CameraUpdateModeEnum _mode = CameraUpdateModeEnum.Update;
+
+        private void Update()
+        {
+            if (_mode != CameraUpdateModeEnum.Update) { return; }
+            Tick(Time.deltaTime);
+        }
+
+        private void FixedUpdate()
+        {
+            if (_mode != CameraUpdateModeEnum.FixedUpdate) { return; }
+            Tick(Time.fixedDeltaTime);
+        }
+
+        private void LateUpdate()
+        {
+            if (_mode != CameraUpdateModeEnum.LateUpdate) { return; }
+            Tick(Time.deltaTime);
+        }
+
+        /// <summary>
+        ///     1フレームごとの更新を行う。
+        /// </summary>
+        /// <param name="deltaTime"> デルタタイム </param>
+        private void Tick(float deltaTime)
+        {
+
+        }
 
         private void HandleLookAction(Vector2 value)
         {
