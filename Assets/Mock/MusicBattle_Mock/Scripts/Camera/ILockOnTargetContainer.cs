70366990267ca22fafc870b6699a074e7eb0bc64
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-20 02:26:55 +0900

    [update]インデクサープロパティを追加

--- Assets/Mock/MusicBattle_Mock/Scripts/Camera/ILockOnTargetContainer.cs
+++ Assets/Mock/MusicBattle_Mock/Scripts/Camera/ILockOnTargetContainer.cs
@@ -5,6 +5,7 @@ namespace Mock.MusicBattle.Camera
 {
     public interface ILockOnTargetContainer 
     {
+        public Transform this[int index] => Targets[index % Targets.Count];
         public IReadOnlyList<Transform> Targets { get; }
     }
 }
