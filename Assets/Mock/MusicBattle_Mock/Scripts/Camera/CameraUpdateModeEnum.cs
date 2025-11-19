d34ba3778d58aa574a9d13e025ce4c88a7b9251a
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-19 23:47:57 +0900

    [add]カメラのアップデートモードを追加

---
+++ Assets/Mock/MusicBattle_Mock/Scripts/Camera/CameraUpdateModeEnum.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace Mock.MusicBattle
+{
+    public enum CameraUpdateModeEnum : byte
+    {
+        Update = 0,
+        FixedUpdate = 1,
+        LateUpdate = 2,
+    }
+}
