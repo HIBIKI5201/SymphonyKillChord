936740845bc109899059ba8d2a28864a1b9195a2
kenkatou354 <kenkatou354@gmail.com>
2025-11-20 00:02:51 +0900

    [Add]ゲームマネージャークラスを追加

---
+++ Assets/Mock/MusicBattle_Mock/Scripts/Basis/GameManager.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+
+namespace Mock.Basis
+{
+    /// <summary>ゲーム全体の管理クラス</summary>
+    [DefaultExecutionOrder(-900)]
+    public class GameManager : MonoBehaviour
+    {
+        private void Awake()
+        {
+            
+        }
+    }
+    
+}
