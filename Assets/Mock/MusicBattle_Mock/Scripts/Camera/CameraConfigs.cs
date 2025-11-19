233cafebc0fe5f901440eca1b0ac37532aa11169
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-20 00:50:49 +0900

    [add]カメラのコンフィグクラスを追加

---
+++ Assets/Mock/MusicBattle_Mock/Scripts/Camera/CameraConfigs.cs
@@ -0,0 +1,51 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+namespace Mock.MusicBattle.Camera
+{
+    /// <summary>
+    ///     カメラの設定データ。
+    /// </summary>
+    /// 
+    [CreateAssetMenu(
+        fileName = nameof(CameraConfigs),
+        menuName = "MusicBattle/" + nameof(CameraConfigs))]
+    public class CameraConfigs : ScriptableObject
+    {
+        public bool IsCameraFlipX => _isCameraFlipX;
+
+        public Vector3 CameraOffset => _cameraOffset;
+        public Vector3 CameraLookAtOffset => _cameraLookAtOffset;
+
+        public float CameraFollowDamping => _cameraFollowDamping;
+        public float CameraLookAtDamping => _cameraLookAtDamping;
+
+        public float CameraRotationSpeed => _cameraRotationSpeed;
+
+        public float PitchRangeMin => _pitchRange.x;
+        public float PitchRangeMax => _pitchRange.y;
+        public float CameraCollisionRadius => _cameraCollisionRadius;
+
+
+        [SerializeField, Tooltip("カメラのX回転を反転")]
+        private bool _isCameraFlipX;
+
+        [SerializeField]
+        private Vector3 _cameraOffset = new Vector3(0f, 2f, -4f);
+        [SerializeField]
+        private Vector3 _cameraLookAtOffset = new Vector3(0f, 1.5f, 0f);
+
+        [SerializeField]
+        private float _cameraFollowDamping = 0f;
+        [SerializeField]
+        private float _cameraLookAtDamping = 0f;
+
+        [SerializeField]
+        private float _cameraRotationSpeed = 3f;
+        [SerializeField, MinMaxRangeSlider(-90, 90)]
+        private Vector2 _pitchRange = new Vector2(-30f, 60f);
+
+        [SerializeField]
+        private float _cameraCollisionRadius = 0.3f;
+    }
+}
