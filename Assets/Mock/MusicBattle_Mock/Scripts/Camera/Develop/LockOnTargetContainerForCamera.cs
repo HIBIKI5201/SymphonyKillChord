df9a213988e521e654bb2e3328b4badefb000069
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-20 02:13:43 +0900

    [add]ロックオンターゲットのコンテナを追加

---
+++ Assets/Mock/MusicBattle_Mock/Scripts/Camera/Develop/LockOnTargetContainerForCamera.cs
@@ -0,0 +1,17 @@
+using Mock.MusicBattle.Camera;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mock.MusicBattle.Develop
+{
+    /// <summary>
+    ///     カメラのデバッグ用のロックオンターゲットのコンテナ。
+    /// </summary>
+    public class LockOnTargetContainerForCamera : MonoBehaviour, ILockOnTargetContainer
+    {
+        public IReadOnlyList<Transform> Targets => _targets;
+
+        [SerializeField]
+        private List<Transform> _targets = new();
+    }
+}
