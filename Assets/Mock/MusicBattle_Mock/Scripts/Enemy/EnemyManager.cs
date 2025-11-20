b2aec7b8eb15cea4da6368c4f597ba2984866d44
kenkatou354 <kenkatou354@gmail.com>
2025-11-21 05:16:37 +0900

    [clear]ロックオンクラスの役割が一部Moverクラスと重複してたので削除

--- Assets/Mock/MusicBattle_Mock/Scripts/Enemy/EnemyManager.cs
+++ Assets/Mock/MusicBattle_Mock/Scripts/Enemy/EnemyManager.cs
@@ -3,34 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mock.MusicBattle.Camera;
 using Unity.Collections;
+using UnityEngine.InputSystem.iOS;
 
 namespace Mock.MusicBattle.Enemy
 {
     /// <summary>
     /// 敵の主要な処理を一括して管理するクラス。
     /// </summary>
+    [RequireComponent(typeof(Rigidbody))]
     public class EnemyManager : MonoBehaviour
     {
-        public void Init()
+        public void Init(Transform target, Transform enemy)
         {
             Rigidbody rb = GetComponent<Rigidbody>();
             _healthEntity = new HealthEntity(_enemyStatus.MaxHealth);
-            _enemyController = new EnemyController(_target, _enemy, _enemyStatus, rb);
+            _enemyMover = new EnemyMover(target, enemy, _enemyStatus, rb);
+            _lockOnController = new LockOnController();
         }
 
         public void TakeDamage(float damage) => _healthEntity.TakeDamage(damage);
 
         private void FixedUpdate()
         {
-            if (_enemyController == null) return;
-            _enemyController.MoveTo();
+            if (_enemyMover == null) return;
+            _enemyMover.MoveTo();
         }
 
 
         [SerializeField] private EnemyStatus _enemyStatus;
         private HealthEntity _healthEntity;
-        private EnemyController _enemyController;
-        private Transform _enemy; //どうやってエネミーとプレイヤーを取得しようかな。
-        private Transform _target;
+        private EnemyMover _enemyMover;
+        private LockOnController _lockOnController;
     }
 }
