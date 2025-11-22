2a660c67e474661680204a8b812afb5da472af33
kenkatou354 <kenkatou354@gmail.com>
2025-11-23 00:10:48 +0900

    [Add]ゲームマネージャークラスにエネミーの初期化処理を追加

--- Assets/Mock/MusicBattle_Mock/Scripts/Basis/GameManager.cs
+++ Assets/Mock/MusicBattle_Mock/Scripts/Basis/GameManager.cs
@@ -1,4 +1,5 @@
 using System;
+using Mock.MusicBattle.Enemy;
 using UnityEngine;
 
 namespace Mock.MusicBattle.Basis
@@ -7,9 +8,21 @@ namespace Mock.MusicBattle.Basis
     [DefaultExecutionOrder(-900)]
     public class GameManager : MonoBehaviour
     {
+         private EnemyContainer _enemyContainer;
+        [SerializeField] private Transform _target;
+        [SerializeField] private EnemyManager _enemyManager;
+        [SerializeField] private EnemyStatus _enemystatus;
+        private EnemyFactory _factory;
         private void Awake()
         {
-            
+            _enemyContainer = new EnemyContainer();
+            _factory = gameObject.AddComponent<EnemyFactory>();
+            _factory.Init(_enemyContainer,transform,_enemyManager);
+        }
+
+        private void Start()
+        {
+            _factory.Spawn(_enemystatus,transform.position);
         }
     }
     
