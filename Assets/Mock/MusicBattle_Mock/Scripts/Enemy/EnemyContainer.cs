ec5d0f027557acf691bd2efca022c462ffefa621
kenkatou354 <kenkatou354@gmail.com>
2025-11-22 21:46:08 +0900

    [Add]エネミーのコンテナクラスを追加

---
+++ Assets/Mock/MusicBattle_Mock/Scripts/Enemy/EnemyContainer.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using Mock.MusicBattle.Enemy;
+using UnityEngine;
+
+namespace Mock.MusicBattle.Enemy
+{
+    /// <summary>
+    /// シーン内のエネミー(EnemyManager)を一元管理するコンテナ。
+    /// 生存中の敵リストを保持し、敵の追加・死亡時の削除を行う。
+    /// </summary>
+    public class EnemyContainer : MonoBehaviour
+    {
+        public EnemyManager this[int index] => 0 < _enemies.Count ? _enemies[index % _enemies.Count] : null;
+        private List<EnemyManager> _enemies = new();
+        
+        public void Register(EnemyManager enemy)
+        {
+            _enemies.Add(enemy);
+            enemy.OnDeath += () => _enemies.Remove(enemy);
+        }
+    }
+}
