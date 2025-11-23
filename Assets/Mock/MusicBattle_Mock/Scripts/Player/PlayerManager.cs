d3aedaa883961c8807169f0ff4636ee18fe7a433
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2025-11-24 00:09:17 +0900

    [update]playermanager

---
+++ Assets/Mock/MusicBattle_Mock/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,68 @@
+using Mock.MusicBattle.Basis;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mock.MusicBattle
+{
+    public class PlayerManager : MonoBehaviour
+    {
+        public void Init(InputBuffer inputBuffer, GameManager gameManager)
+        {
+            _inputBuffer = inputBuffer;
+            InputEventRegister(_inputBuffer);
+        }
+
+        public void TakeDamage(float damage)
+        {
+            // HelthEntityが未実装のため、仮実装
+        }
+
+        [SerializeField, Tooltip("プレイヤーのステータス")]
+        private PlayerStatus _playerStatus;
+        [SerializeField, Tooltip("コンフィグ")]
+        private PlayerConfig _config;
+
+        private InputBuffer _inputBuffer;
+        private HashSet<Collision> _hitGrounds = new();
+
+        private void OnDisable()
+        {
+            InputEventUnregister(_inputBuffer);
+        }
+
+        private void InputEventRegister(InputBuffer inputBuffer)
+        {
+            if (inputBuffer == null)
+            {
+                Debug.LogError($"{nameof(InputBuffer)} is null");
+                return;
+            }
+            inputBuffer.MoveAction.Performed += OnInputMove;
+            inputBuffer.MoveAction.Canceled += OnInputMove;
+            inputBuffer.AttackAction.Started += OnInputAttack;
+        }
+
+        private void InputEventUnregister(InputBuffer inputBuffer)
+        {
+            if (inputBuffer == null)
+            {
+                Debug.LogError($"{nameof(InputBuffer)} is null");
+                return;
+            }
+            inputBuffer.MoveAction.Performed -= OnInputMove;
+            inputBuffer.MoveAction.Canceled -= OnInputMove;
+            inputBuffer.AttackAction.Started -= OnInputAttack;
+        }
+
+        private void OnInputMove(Vector2 input)
+        {
+
+        }
+
+        private void OnInputAttack(float input)
+        {
+
+        }
+    }
+}
