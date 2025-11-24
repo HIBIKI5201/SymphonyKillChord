e48750043e87a6b360add95563d71f109a3ca45d
kenkatou354 <kenkatou354@gmail.com>
2025-11-25 08:38:53 +0900

    [fix]ゲームマネージャーからエネミーの初期化処理を削除。

--- Assets/Mock/MusicBattle_Mock/Scripts/Basis/GameManager.cs
+++ Assets/Mock/MusicBattle_Mock/Scripts/Basis/GameManager.cs
@@ -1,5 +1,4 @@
-using System;
-using Mock.MusicBattle.Enemy;
+
 using UnityEngine;
 
 namespace Mock.MusicBattle.Basis
@@ -8,21 +7,6 @@ namespace Mock.MusicBattle.Basis
     [DefaultExecutionOrder(-900)]
     public class GameManager : MonoBehaviour
     {
-        [SerializeField] private EnemyManager _enemyManager;
-        [SerializeField] private EnemyStatus _enemystatus;
-        [SerializeField] private Transform _player;
-        private EnemyContainer _enemyContainer;
-        private EnemyFactory _factory;
-
-        private void Awake()
-        {
-            _enemyContainer = new EnemyContainer();
-            _factory = new EnemyFactory(_enemyContainer, _player, _enemyManager);
-        }
-
-        private void Start()
-        {
-            _factory.Spawn(_enemystatus, transform.position);
-        }
+      
     }
 }
