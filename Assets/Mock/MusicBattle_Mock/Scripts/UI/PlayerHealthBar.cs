cbc85b2714bb82abb7582c9fecfe3f9e15136c27
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-29 02:28:28 +0900

    [update]ヘルスバーの発火方向を変更

--- Assets/Mock/MusicBattle_Mock/Scripts/UI/PlayerHealthBar.cs
+++ Assets/Mock/MusicBattle_Mock/Scripts/UI/PlayerHealthBar.cs
@@ -1,3 +1,5 @@
+using Mock.MusicBattle.Character;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
@@ -27,6 +29,32 @@ namespace Mock.MusicBattle.UI
             Debug.Assert(_redBar != null, $"Failed to find element: {ELEMENT_NAME_RED_BAR}");
         }
 
+        public void Initialize(HealthEntity healthEntity, CancellationToken token = default)
+        {
+            _token = token;
+
+            healthEntity.OnHealthChanged += ChangeHealthBarHandler;
+            healthEntity.OnDeath += () =>
+            {
+                healthEntity.OnHealthChanged -= ChangeHealthBarHandler;
+            };
+        }
+
+        private const string UXML_RESOURCES_PATH = "PlayerHealthBar";
+        private const string ELEMENT_NAME_GREEN_BAR = "green-guage";
+        private const string ELEMENT_NAME_RED_BAR = "red-guage";
+
+        private CancellationToken _token;
+        private VisualElement _greenBar;
+        private VisualElement _redBar;
+
+        /// <summary>
+        ///     イベント登録用のヘルスバー変更ハンドラー。
+        /// </summary>
+        /// <param name="current"></param>
+        /// <param name="max"></param>
+        private void ChangeHealthBarHandler(float current, float max) => ChangeHealthBar(current, max, _token);
+
         /// <summary>
         ///     ヘルスバーの割合を変更する。
         /// </summary>
@@ -34,7 +62,7 @@ namespace Mock.MusicBattle.UI
         /// <param name="max"></param>
         /// <param name="token"></param>
         /// <returns></returns>
-        public async Task ChangeHealthBar(float current, float max, CancellationToken token = default)
+        private async void ChangeHealthBar(float current, float max, CancellationToken token = default)
         {
             float proportion = Mathf.Clamp01(current / max);
 
@@ -42,12 +70,5 @@ namespace Mock.MusicBattle.UI
             await Awaitable.WaitForSecondsAsync(1f, token);
             await _redBar.ChangeBarAsync(proportion, 0.6f, token);
         }
-
-        private const string UXML_RESOURCES_PATH = "PlayerHealthBar";
-        private const string ELEMENT_NAME_GREEN_BAR = "green-guage";
-        private const string ELEMENT_NAME_RED_BAR = "red-guage";
-
-        private VisualElement _greenBar;
-        private VisualElement _redBar;
     }
 }
