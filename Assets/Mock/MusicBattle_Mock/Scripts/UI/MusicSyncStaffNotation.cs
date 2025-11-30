9eccf989553083002df58177cbf60028288fe591
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-30 16:56:40 +0900

    [update]UXMLファイルを解析して取得

--- Assets/Mock/MusicBattle_Mock/Scripts/UI/MusicSyncStaffNotation.cs
+++ Assets/Mock/MusicBattle_Mock/Scripts/UI/MusicSyncStaffNotation.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -9,5 +10,48 @@ namespace Mock.MusicBattle.UI
     [UxmlElement]
     public partial class MusicSyncStaffNotation : VisualElement
     {
+        public MusicSyncStaffNotation()
+        {
+            // UXMLを読み込んで要素を取得する。
+            VisualTreeAsset treeAsset = Resources.Load<VisualTreeAsset>(UXML_RESOURCES_PATH);
+            if (treeAsset == null)
+            {
+                Debug.LogError($"Failed to load UXML at path: {UXML_RESOURCES_PATH}");
+                return;
+            }
+
+            TemplateContainer container = treeAsset.Instantiate();
+            hierarchy.Add(container);
+
+            VisualElement lines = container.Q<VisualElement>(ELEMENT_NAME_STAFF_LINE_CONTAINER);
+            _staffLines = lines.Children().ToArray();
+            _noteContainer = container.Q<VisualElement>(ELEMENT_NAME_NOTE_CONTAINER);
+
+            Debug.Assert(_staffLines != null, $"Failed to find element: {ELEMENT_NAME_STAFF_LINE_CONTAINER}");
+            Debug.Assert(_noteContainer != null, $"Failed to find element: {ELEMENT_NAME_NOTE_CONTAINER}");
+        }
+
+        public void Update(float currentMeasure)
+        {
+            MoveStaffLines(currentMeasure);
+        }
+
+        private const string UXML_RESOURCES_PATH = "MusicSyncStaffNotation";
+
+        private const string ELEMENT_NAME_STAFF_LINE_CONTAINER = "staff-line-container";
+        private const string ELEMENT_NAME_NOTE_CONTAINER = "note-container";
+
+        private VisualElement[] _staffLines;
+        private VisualElement _noteContainer;
+
+        private void MoveStaffLines(float currentMeasure)
+        {
+            float measure = currentMeasure % 4;
+            for (int i = 0; i < _staffLines.Length; i++)
+            {
+                VisualElement line = _staffLines[i];
+                line.style.left = Length.Percent(-25f * i * measure);
+            }
+        }
     }
 }
