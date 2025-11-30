c74fff87ec466c7e1d33c29e5ed19d5e4501fced
kenkatou354 <kenkatou354@gmail.com>
2025-11-30 15:47:01 +0900

    [add]エネミーの攻撃処理を追加

--- Assets/Mock/MusicBattle_Mock/Scripts/Enemy/EnemyAttack.cs
+++ Assets/Mock/MusicBattle_Mock/Scripts/Enemy/EnemyAttack.cs
@@ -1,39 +1,62 @@
 using Mock.MusicBattle.Enemy;
-using UnityEngine;
 using Mock.MusicBattle.MusicSync;
+using System.Threading;
+using UnityEngine;
 
 namespace Mock.MusicBattle
 {
     public class EnemyAttack
     {
-        public EnemyAttack(EnemyManager enemy, MusicSyncManager music)
+        public EnemyAttack(EnemyManager enemy, MusicSyncManager music,
+            EnemyMusicSO encount, EnemyMusicSO battle)
         {
+            if (music == null) Debug.LogError("music is NULL!");
+            if (encount == null) Debug.LogError("encount is NULL!");
+            if (battle == null) Debug.LogError("battle is NULL!");
+
             _enemyManager = enemy;
             _musicSyncManager = music;
+            _encount = encount;
+            _battale = battle;
+            _enemyManager.OnAttack += OnAttackHandler;
+            _enemyManager.OnOutOfRange += CancelScheduled;
         }
-        
-        private void ScheduledAttack()
+
+        public void OnAttackHandler()
         {
-           
-            if (_barTimingInfo.BarFlg % 2 == 1) return;
-
-            if (_barTimingInfo.TimeSignature % 2 == 1) return;
-
-            if (_barTimingInfo.TargetBeat % 3 == 0)
-            {
-                Attack();
-            }
+            ScheduledEncount();
         }
+
+
+        private void ScheduledBattale()
+        {
+            BarTimingInfo barTimingInfo = new BarTimingInfo(_battale.BarFlg, _battale.TimeSignature, _battale.TargetBeat);
+            _cancellationTokenSource = new CancellationTokenSource();
+            _musicSyncManager.RegisterAction(barTimingInfo, Attack, _cancellationTokenSource.Token);
+        }
+        private void ScheduledEncount()
+        {
+            BarTimingInfo barTimingInfo = new BarTimingInfo(_encount.BarFlg, _encount.TimeSignature, _encount.TargetBeat);
+            _cancellationTokenSource = new CancellationTokenSource();
+            _musicSyncManager.RegisterAction(barTimingInfo, Attack, _cancellationTokenSource.Token);
+        }
+        private void CancelScheduled()
+        {
+            _cancellationTokenSource.Cancel();
+        }
+
+        private EnemyMusicSO _encount;
+        private EnemyMusicSO _battale;
+        private CancellationTokenSource _cancellationTokenSource;
+        private EnemyManager _enemyManager;
+        private MusicSyncManager _musicSyncManager;
 
         private void Attack()
         {
-            
+            Debug.Log("Attack!");
+            ScheduledBattale();
         }
 
-        private bool _inrange = false;
-        private bool _attackTiming = false;
-        private EnemyManager _enemyManager;
-        private MusicSyncManager _musicSyncManager;
-        private BarTimingInfo _barTimingInfo;
+
     }
 }
