da7763bc17a52e7f61ada4c3139ace7f74528cbc
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-30 14:52:30 +0900

    [update]インスタンスイベントに変更

--- Assets/Mock/MusicBattle_Mock/Scripts/UI/DamageTextPool.cs
+++ Assets/Mock/MusicBattle_Mock/Scripts/UI/DamageTextPool.cs
@@ -6,31 +6,41 @@ namespace Mock.MusicBattle.UI
 {
     public class DamageTextPool
     {
+        public DamageTextPool(VisualElement root)
+        {
+            _root = root;
+            _pool = new(
+            createFunc: Create,
+            actionOnGet: Get,
+            actionOnRelease: Release
+        );
+        }
+
         public void ShowDamageText(float damage, Vector3 position)
         {
             DamageTextEntity entity = _pool.Get();
             entity.Show(damage, position);
         }
 
-        private ObjectPool<DamageTextEntity> _pool = new(
-            createFunc: Create,
-            actionOnGet: Get,
-            actionOnRelease: Release
-        );
+        private readonly VisualElement _root;
 
-        private static DamageTextEntity Create()
+        private ObjectPool<DamageTextEntity> _pool;
+
+        private DamageTextEntity Create()
         {
             DamageTextEntity entity = new();
             entity.Initialize(() => Release(entity), 2);
+            _root.Add(entity);
+
             return entity;
         }
 
-        private static void Get(DamageTextEntity entity)
+        private void Get(DamageTextEntity entity)
         {
             entity.style.visibility = Visibility.Visible;
         }
 
-        private static void Release(DamageTextEntity entity)
+        private void Release(DamageTextEntity entity)
         {
             entity.style.visibility = Visibility.Hidden;
         }
