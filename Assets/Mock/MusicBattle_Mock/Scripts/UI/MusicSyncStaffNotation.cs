b225284159acdea50389c1244ba3a74cdeec6b9a
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-30 16:17:44 +0900

    [add]五線譜UIを追加

---
+++ Assets/Mock/MusicBattle_Mock/Scripts/UI/MusicSyncStaffNotation.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Mock.MusicBattle.UI
+{
+    /// <summary>
+    ///     音楽同期システムの五線譜表記UI要素。
+    /// </summary>
+    [UxmlElement]
+    public partial class MusicSyncStaffNotation : VisualElement
+    {
+    }
+}
