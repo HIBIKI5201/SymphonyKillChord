4d436127553a4a7703cea3618982a985a5b1dde9
kenkatou354 <kenkatou354@gmail.com>
2025-11-30 15:43:53 +0900

    [add]レンジに入ったらイベントを通知する

--- Assets/Mock/MusicBattle_Mock/Scripts/Enemy/EnemyMover.cs
+++ Assets/Mock/MusicBattle_Mock/Scripts/Enemy/EnemyMover.cs
@@ -1,5 +1,6 @@
 using UnityEngine.AI;
 using UnityEngine;
+using System;
 
 namespace Mock.MusicBattle.Enemy
 {/// <summary>
@@ -7,57 +8,71 @@ namespace Mock.MusicBattle.Enemy
  /// </summary>
     public class EnemyMover
     {
-        
+
         /// <summary> コンストラクタ。 </summary>
-        public EnemyMover(Transform target,Transform enemy,
-            EnemyStatus enemyStatus,NavMeshAgent agent)
+        public EnemyMover(Transform target, Transform enemy,
+            EnemyStatus enemyStatus, NavMeshAgent agent)
         {
             _target = target;
             _enemy = enemy;
             _agent = agent;
             Init(enemyStatus);
         }
-        
+        public event Action OnAttack;
+        public event Action OnOutOfRange;
+
         /// <summary> 初期化処理をする。 </summary>
         public void Init(EnemyStatus enemyStatus)
         {
             _enemystatus = enemyStatus;
         }
-        
+
         /// <summary> ターゲットとの距離を返す。 </summary>
         public float DistanceToTarget()
         {
             return Vector3.Distance(_target.position, _enemy.position);
         }
-        
+
         /// <summary>
         ///     射程内までプレイヤーに近づく。
         /// </summary>
         public void MoveTo()
         {
-            if(!_agent.isOnNavMesh) return; 
-            if (_enemy == null ||_target == null || _agent ==null) return;
+            if (!_agent.isOnNavMesh) return;
+            if (_enemy == null || _target == null || _agent == null) return;
 
-            float distance =  DistanceToTarget();
+            float distance = DistanceToTarget();
             //射程外　＝＞　近づく。
             if (distance > _enemystatus.AttackRange)
             {
+                if (_encount && !_inRange)
+                {
+                    OnOutOfRange?.Invoke();
+                }
+                _encount = false;
+                _inRange = true;
                 _agent.isStopped = false;
                 _agent.SetDestination(_target.position);
             }
             //射程内＝＞止まって攻撃処理へ。
             else
             {
+                if (_inRange)
+                {
+                    OnAttack?.Invoke();
+                    _inRange = false;
+                }
                 _agent.isStopped = true;
-                
+                _encount = true;
             }
         }
-        
-        private EnemyStatus  _enemystatus;
+
+        private EnemyStatus _enemystatus;
         private Transform _target;
         private Transform _enemy;
         private NavMeshAgent _agent;
-        private bool _isDead = false;
-      
+        private bool _encount = false;
+        private bool _inRange = false;
+
     }
 }
