aae41c7427c3e700f715bd37751eb754b81d26da
kenkatou354 <kenkatou354@gmail.com>
2025-11-30 15:56:12 +0900

    [add]ゲームマネージャーにmusic関連の初期化処理を追加

--- Assets/Mock/MusicBattle_Mock/Scripts/Basis/GameManager.cs
+++ Assets/Mock/MusicBattle_Mock/Scripts/Basis/GameManager.cs
@@ -1,12 +1,15 @@
 
-using System;
-using System.Collections;
-using UnityEngine;using Mock.MusicBattle.Basis;
+using CriWare;
+using Mock.MusicBattle.Basis;
 using Mock.MusicBattle.Battle;
 using Mock.MusicBattle.Camera;
 using Mock.MusicBattle.Develop;
 using Mock.MusicBattle.Enemy;
+using Mock.MusicBattle.MusicSync;
 using Mock.MusicBattle.Player;
+using System;
+using System.Collections;
+using UnityEngine;
 using Random = UnityEngine.Random;
 
 namespace Mock.MusicBattle.Basis
@@ -16,27 +19,34 @@ namespace Mock.MusicBattle.Basis
     public class GameManager : MonoBehaviour
     {
         [Header("Player")]
-        [SerializeField] 
+        [SerializeField]
         private PlayerManager _playerManager;
-        [SerializeField] 
+        [SerializeField]
         private InputBuffer _inputBuffer;
         [SerializeField]
         private CameraManager _cameraManager;
-        
+
         [Header("Enemy")]
-        [SerializeField] 
+        [SerializeField]
         private EnemyManager _enemyManager;
         [SerializeField]
         private EnemyStatus _enemystatus;
-        [SerializeField] 
+        [SerializeField]
         private Transform _player;
         [SerializeField]
         private float _enemySpawnTime = 1f;
-        
+
+        [Header("MusicSync")]
+        [SerializeField, Tooltip("音楽同期マネージャー")]
+        private MusicSyncManager _musicSyncManager;
+        [Header("音楽同期システム初期化パラメータ")]
+        [SerializeField] private MusicSystemInitSO _musicSystemInitSO;
+        [SerializeField] private CriAtomSource _source;
+
         private float _xrange = 50f;
         private float _yrange = 1f;
         private float _zrange = 50f;
-        
+
         private EnemyFactory _factory;
         private LockOnManager _lockOnManager;
         private EnemyContainer _enemyContainer;
@@ -49,6 +59,7 @@ namespace Mock.MusicBattle.Basis
 
         private void Start()
         {
+            _musicSyncManager.Init(_source, _musicSystemInitSO.Bgm,_musicSystemInitSO.BgmProperTime,_musicSystemInitSO.StartOffset);
             StartCoroutine(SpawnLoop());
         }
 
@@ -56,7 +67,7 @@ namespace Mock.MusicBattle.Basis
         {
             _lockOnManager = new LockOnManager(_cameraManager.transform,
                 _enemyContainer, _inputBuffer);
-            _cameraManager.Init(_inputBuffer,_lockOnManager);
+            _cameraManager.Init(_inputBuffer, _lockOnManager);
             _playerManager.Init(_inputBuffer);
         }
 
@@ -64,18 +75,21 @@ namespace Mock.MusicBattle.Basis
         {
             _enemyContainer = new EnemyContainer();
             _factory = new EnemyFactory(_enemyContainer,
-                _playerManager.transform, _enemyManager);
+                _playerManager.transform, _enemyManager,
+                _musicSyncManager);
         }
         private IEnumerator SpawnLoop()
         {
+       
             while (true)
             {
+                
                 Vector3 RandamPos = new Vector3(
                     Random.Range(-_xrange, _xrange),
                     _yrange,
                     Random.Range(-_zrange, _zrange));
-                
-                _factory.Spawn(_enemystatus,RandamPos);
+
+                _factory.Spawn(_enemystatus, RandamPos);
 
                 yield return new WaitForSeconds(_enemySpawnTime);
             }
