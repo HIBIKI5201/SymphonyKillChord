9a635bb087e9d21cb1a3b134ec90fbd72d427f07
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-30 13:30:17 +0900

    [update]ツリーアセットをロード

--- Assets/Mock/MusicBattle_Mock/Scripts/UI/DamageTextEntity.cs
+++ Assets/Mock/MusicBattle_Mock/Scripts/UI/DamageTextEntity.cs
@@ -1,8 +1,26 @@
+using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace Mock.MusicBattle.UI
 {
-    public class DamageTextEntity : VisualElement
+    /// <summary>
+    ///     ダメージテキストの実体クラス。
+    /// </summary>
+    [UxmlElement]
+    public partial class DamageTextEntity : VisualElement
     {
+        public DamageTextEntity()
+        {
+            VisualTreeAsset treeAsset = Resources.Load<VisualTreeAsset>(UXML_RESOURCES_PATH);
+            if (treeAsset == null)
+            {
+                Debug.LogError($"Failed to load UXML at path: {UXML_RESOURCES_PATH}");
+                return;
+            }
+            TemplateContainer container = treeAsset.Instantiate();
+            hierarchy.Add(container);
+        }
+
+        private const string UXML_RESOURCES_PATH = "DamageTextEntity";
     }
 }
