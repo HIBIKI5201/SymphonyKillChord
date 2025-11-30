d8ff0b63c7fe835b40f8162c807f1633a544ebba
HIBIKI_5201 <takuto5201@gmail.com>
2025-11-30 13:41:01 +0900

    [update]プールを実装

--- Assets/Mock/MusicBattle_Mock/Scripts/UI/DamageTextPool.cs
+++ Assets/Mock/MusicBattle_Mock/Scripts/UI/DamageTextPool.cs
@@ -1,9 +1,38 @@
 using UnityEngine;
+using UnityEngine.Pool;
+using UnityEngine.UIElements;
 
 namespace Mock.MusicBattle.UI
 {
     public class DamageTextPool
     {
+        public void ShowDamageText(float damage, Vector3 position)
+        {
+            DamageTextEntity entity = _pool.Get();
+            entity.BindData(damage, position);
+        }
 
+        private ObjectPool<DamageTextEntity> _pool = new(
+            createFunc: Create,
+            actionOnGet: Get,
+            actionOnRelease: Release
+        );
+
+        private static DamageTextEntity Create()
+        {
+            DamageTextEntity entity = new();
+            entity.Initialize(() => Release(entity));
+            return entity;
+        }
+
+        private static void Get(DamageTextEntity entity)
+        {
+            entity.style.visibility = Visibility.Visible;
+        }
+
+        private static void Release(DamageTextEntity entity)
+        {
+            entity.style.visibility = Visibility.Hidden;
+        }
     }
 }
