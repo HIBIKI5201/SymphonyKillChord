09251814fbf4cc1d29177c6a58d6ccdc5ad64754
HIBIKI_5201 <takuto5201@gmail.com>
2025-12-03 22:11:23 +0900

    [fix]インデント修正

--- Assets/Mock/MusicBattle_Mock/Scripts/Basis/InputActionEntity.cs
+++ Assets/Mock/MusicBattle_Mock/Scripts/Basis/InputActionEntity.cs
@@ -1,9 +1,8 @@
 using System;
-using System.Collections.Generic;
 using UnityEngine.InputSystem;
 
 namespace Mock.MusicBattle.Basis
-{ 
+{
     /// <summary>
     ///     入力アクションのイベントラッパークラス。
     /// </summary>
