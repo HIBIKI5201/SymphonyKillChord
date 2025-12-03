71e590f872a280f4e3cb272929a141eb24e9fb95
kenkatou354 <kenkatou354@gmail.com>
2025-12-03 18:15:14 +0900

    [hotfix]入力はVector3にしてプロパティはFloatにした。

--- Assets/Mock/MusicBattle_Mock/Scripts/Enemy/EnemySpawnSO.cs
+++ Assets/Mock/MusicBattle_Mock/Scripts/Enemy/EnemySpawnSO.cs
@@ -6,21 +6,19 @@ namespace Mock.MusicBattle.Enemy
     public class EnemySpawnSO : ScriptableObject
     {
         /// <summary> スポーン範囲 X </summary>
-        public float XRange => _xRange;
+        public float XRange => _spawnRange.x;
 
         /// <summary> スポーン範囲 Y </summary>
-        public float YRange => _yRange;
+        public float YRange => _spawnRange.y;
 
         /// <summary> スポーン範囲 Z </summary>
-        public float ZRange => _zRange;
+        public float ZRange => _spawnRange.z;
 
         /// <summary> 最大出現数 </summary>
         public int MaxEnemyCount => _maxEnemyCount;
 
         [Header("スポーン範囲（Vector3 の各軸に相当）")]
-        [SerializeField] private float _xRange;
-        [SerializeField] private float _yRange;
-        [SerializeField] private float _zRange;
+        [SerializeField] private Vector3 _spawnRange = new Vector3(2f, 0f, 2f);
 
         [Header("最大同時スポーン数")]
         [SerializeField] private int _maxEnemyCount = 3;
