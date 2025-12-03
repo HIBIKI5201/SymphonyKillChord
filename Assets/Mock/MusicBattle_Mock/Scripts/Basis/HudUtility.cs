ae1419cd00c3335341f605243128e21537c68ec9
kenkatou354 <kenkatou354@gmail.com>
2025-12-03 22:53:24 +0900

    [add]HUDのUtilityクラスを追加

---
+++ Assets/Mock/MusicBattle_Mock/Scripts/Basis/HudUtility.cs
@@ -0,0 +1,31 @@
+using Mock.MusicBattle.Basis;
+using Mock.MusicBattle.MusicSync;
+using Mock.MusicBattle.Player;
+using Mock.MusicBattle.UI;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.HID;
+
+namespace Mock.MusicBattle
+{
+    public static class HudUtility
+    {
+        public static void Init(IngameHUDManager hud, PlayerManager player,
+                        CriMusicBuffer musicBuffer, InputBuffer inputBuffer
+            , CancellationToken destroyToken)
+        {
+            hud.InitializePlayerHealthBar(player.HealthEntityPlayer);
+            hud.Initialize(musicBuffer);
+            inputBuffer.AttackAction.Started += Action_started;
+
+            destroyToken.Register(() => inputBuffer.AttackAction.Started -= Action_started);
+
+            void Action_started(float a)
+            {
+                hud.CreateNote((float)(musicBuffer.CurrentBeat / 4d));
+            }
+        }
+     
+    }
+}
