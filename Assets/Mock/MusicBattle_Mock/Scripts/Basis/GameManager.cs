9c09b74f213795cc5691535a4cc1bdbf1ba24b06
kenkatou354 <kenkatou354@gmail.com>
2025-12-09 11:00:26 +0900

    [add]ゲームマネージャーにコンテナに必要な情報を渡す処理を追加

--- Assets/Mock/MusicBattle_Mock/Scripts/Basis/GameManager.cs
+++ Assets/Mock/MusicBattle_Mock/Scripts/Basis/GameManager.cs
@@ -55,6 +55,7 @@ namespace Mock.MusicBattle.Basis
 
         private void Start()
         {
+            
             _musicSyncManager.Init(_source, _musicSystemInitSO.Bgm, _musicSystemInitSO.BgmProperTime, _musicSystemInitSO.StartOffset);
             StartCoroutine(EnemyUtility.SpawnLoop(
                 _enemyContainer,
@@ -79,6 +80,9 @@ namespace Mock.MusicBattle.Basis
                 _enemyContainer, _player,
                 _enemyManager, _musicSyncManager,
                 _lockOnManager, _hudManager);
+            _enemyContainer.SetCharacter(_playerManager);
+            _enemyContainer.SetCamera(_cameraManager);
+            _enemyContainer.SetLockOnManager(_lockOnManager);
         }
     }
 }
