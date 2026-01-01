de2aa4e266c720a67962f1c753146cbfd4dd7e13
HIBIKI_5201 <takuto5201@gmail.com>
2026-01-02 03:07:43 +0900

    [update]データベースに取得メソッドを追加

--- Assets/Mock/MusicBattle_Mock/Scripts/MusicSync/SignatureDatabase.cs
+++ Assets/Mock/MusicBattle_Mock/Scripts/MusicSync/SignatureDatabase.cs
@@ -9,8 +9,14 @@ namespace Mock.MusicBattle.MusicSync
         menuName = EditorConstraint.CREATE_ASSET_PATH + nameof(SignatureDatabase))]
     public class SignatureDatabase : ScriptableObject
     {
-        public ReadOnlySpan<SignatureData> SignatureDataSpan => _signatureDataArray;
+        /// <summary> 拍子情報を返す </summary>
+        public SignatureData[] SignatureDataSpan => _signatureDataArray;
 
+        /// <summary>
+        ///     入力された拍子に対応する色を取得します。
+        /// </summary>
+        /// <param name="signature"></param>
+        /// <returns></returns>
         public Color GetColorBySignature(float signature)
         {
             foreach (var data in _signatureDataArray)
@@ -20,12 +26,22 @@ namespace Mock.MusicBattle.MusicSync
                     return data.Color;
                 }
             }
+
             Debug.LogWarning($"Signature {signature} not found. Returning default color.");
             return Color.white;
         }
 
+        [Serializable]
         public struct SignatureData
         {
+            public SignatureData(float signature) : this(signature, Color.white) { }
+
+            public SignatureData(float signature, Color color)
+            {
+                _signature = signature;
+                _color = color;
+            }
+
             public float Signature => _signature;
             public Color Color => _color;
 
@@ -36,6 +52,6 @@ namespace Mock.MusicBattle.MusicSync
         }
 
         [SerializeField, Tooltip("拍子と色の関連付けデータの配列。")]
-        private SignatureData[] _signatureDataArray;
+        private SignatureData[] _signatureDataArray = { new(1), new(2), new(3), new(4) };
     }
 }
