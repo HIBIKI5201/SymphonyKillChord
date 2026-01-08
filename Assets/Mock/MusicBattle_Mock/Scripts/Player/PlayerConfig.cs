b913deda7d0d94ae251fd5de8fcb4698add5ff43
HIBIKI_5201 <takuto5201@gmail.com>
2026-01-09 06:54:25 +0900

    [update]設置判定の閾値をコンフィグに移動

--- Assets/Mock/MusicBattle_Mock/Scripts/Player/PlayerConfig.cs
+++ Assets/Mock/MusicBattle_Mock/Scripts/Player/PlayerConfig.cs
@@ -12,12 +12,23 @@ namespace Mock.MusicBattle.Player
         #region パブリックプロパティ
         /// <summary> 攻撃時に無視するレイヤーマスクを取得します。 </summary>
         public LayerMask IgnoreAttackLayer => _ignoreAttackLayer;
+
+        /// <summary> 地面判定における法線の垂直閾値。 </summary>
+        public float GroundNormalVerticalThreshold => _groundNormalVerticalThreshold;
+        #endregion
+
+        #region 定数
+        private const float DEFAULT_GROUND_NORMAL_VERTICAL_THRESHOLD = 0.5f;
         #endregion
 
         #region インスペクター表示フィールド
         /// <summary> 攻撃が当たらないレイヤーマスク。 </summary>
         [SerializeField, Tooltip("攻撃が当たらないレイヤーマスク。")]
         private LayerMask _ignoreAttackLayer;
+
+        /// <summary> 地面判定における法線の垂直閾値。 </summary>
+        [SerializeField, Range(0, 1), Tooltip("地面判定における法線の垂直閾値。")]
+        private float _groundNormalVerticalThreshold = DEFAULT_GROUND_NORMAL_VERTICAL_THRESHOLD;
         #endregion
     }
 }
