8e15a7aae1a7907e77f6b74f1cb1e047aa6dc01a
HIBIKI_5201 <takuto5201@gmail.com>
2026-01-09 07:37:05 +0900

    [add]ログを出すSAを追加

---
+++ Assets/Mock/MusicBattle_Mock/Scripts/Player/SpecialAttack/SpecialAttackLog.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Mock.MusicBattle.Player
+{
+    public class SpecialAttackLog : ISpecialAttackModule
+    {
+        public void Excute()
+        {
+
+        }
+    }
+}
