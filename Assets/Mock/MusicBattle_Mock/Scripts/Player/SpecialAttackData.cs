f89f71f79dbef9a724c8ecbc832e09a9dae894ea
HIBIKI_5201 <takuto5201@gmail.com>
2026-01-09 07:57:30 +0900

    [update]アサート機能を追加

--- Assets/Mock/MusicBattle_Mock/Scripts/Player/SpecialAttackData.cs
+++ Assets/Mock/MusicBattle_Mock/Scripts/Player/SpecialAttackData.cs
@@ -1,6 +1,7 @@
 using Mock.MusicBattle.MusicSync;
 using SymphonyFrameWork.Attribute;
 using System;
+using UnityEditorInternal;
 using UnityEngine;
 
 namespace Mock.MusicBattle.Player
@@ -8,9 +9,12 @@ namespace Mock.MusicBattle.Player
     [Serializable]
     public struct SpecialAttackData
     {
+        #region プロパティ
         public RythemPatternData RythemPattern => _rythemPattern;
         public ISpecialAttackModule[] Modules => _modules;
+        #endregion
 
+        #region Publicメソッド
         public void Excute()
         {
             foreach (ISpecialAttackModule module in _modules)
@@ -18,10 +22,26 @@ namespace Mock.MusicBattle.Player
                 module?.Execute();
             }
         }
+        #endregion
 
+        #region シリアライズフィールド
         [SerializeField]
         private RythemPatternData _rythemPattern;
         [SerializeReference, SubclassSelector]
         private ISpecialAttackModule[] _modules;
+        #endregion
+
+        #region デバッグ
+        public void Assert(UnityEngine.Object context = null)
+        {
+            Debug.Assert(_rythemPattern != null, "リズムパターンがありません。", context);
+            Debug.Assert(0 < _modules.Length, "モジュールが割り当てられていません。", context);
+            for (int i = 0; i < _modules.Length; i++)
+            {
+                ISpecialAttackModule module = _modules[i];
+                Debug.Assert(module != null, $"{i}番目のモジュールがnullです。",context);
+            }
+        }
+        #endregion
     }
 }
