4d71efede2320df8ec492ab04970745952172aa7
HIBIKI_5201 <takuto5201@gmail.com>
2026-01-09 06:54:11 +0900

    [update]プロパティに硬直時間を追加しアサートを実装

--- Assets/Mock/MusicBattle_Mock/Scripts/Player/PlayerStatus.cs
+++ Assets/Mock/MusicBattle_Mock/Scripts/Player/PlayerStatus.cs
@@ -23,48 +23,68 @@ namespace Mock.MusicBattle.Player
         public float AttackPower => _attackPower;
         /// <summary> 攻撃範囲。 </summary>
         public float AttackRange => _attackRange;
+        /// <summary> 攻撃時の硬直時間。 </summary>
+        public float PostAttackMoveLockDuration => _postAttackMoveLockDuration;
         /// <summary> 最大ヘルス。 </summary>
         public float MaxHealth => _maxHealth;
         /// <summary> スペシャルアタックパターン群。 </summary>
         public RythemPatternData[] SpecialAttackPatterns => _specialAttackPatterns;
-        /// <summary> 地面判定における法線の垂直閾値。 </summary>
-        public float GroundNormalVerticalThreshold => _groundNormalVerticalThreshold;
         #endregion
 
-        #region 定数
-        private const float DEFAULT_GROUND_NORMAL_VERTICAL_THRESHOLD = 0.5f;
-        #endregion
+        public void Assert()
+        {
+            // 移動。
+            Debug.Assert(0 < _moveSpeed, "移動速度が設定されていません。", this);
+            Debug.Assert(0 < _walkAccelerationDuration, "歩行時の加速時間が設定されていません。", this);
+            Debug.Assert(0 < _stopAccelerationDuration, "停止時の加速時間が設定されていません。", this);
+            Debug.Assert(0 < _rotationDamping, "回転減衰が設定されていません。", this);
+
+            // 攻撃。
+            Debug.Assert(0 < _attackPower, "攻撃力が設定されていません。", this);
+            Debug.Assert(0 < _attackRange, "攻撃範囲が設定されていません。", this);
+            Debug.Assert(0 < _postAttackMoveLockDuration, "攻撃時の硬直時間が設定されていません。", this);
+
+            // 防御
+            Debug.Assert(0 < _maxHealth, "体力が設定されていません。", this);
+
+            // 音楽同期。
+            Debug.Assert(0 < _specialAttackPatterns.Length, "スペシャルアタックパターン群が空です。", this);
+        }
 
         #region インスペクター表示フィールド
+        [Header("移動")]
         /// <summary> 移動速度。 </summary>
-        [SerializeField, Tooltip("移動速度。")]
-        private float _moveSpeed;
+        [SerializeField, Tooltip("移動速度。"), Min(0)]
+        private float _moveSpeed = 1;
         /// <summary> 歩行時の加速時間。 </summary>
-        [SerializeField, Tooltip("歩行時の加速時間。")]
-        private float _walkAccelerationDuration;
+        [SerializeField, Tooltip("歩行時の加速時間。"), Min(0)]
+        private float _walkAccelerationDuration = 0.1f;
         /// <summary> 停止時の加速時間。 </summary>
-        [SerializeField, Tooltip("停止時の加速時間。")]
-        private float _stopAccelerationDuration;
+        [SerializeField, Tooltip("停止時の加速時間。"), Min(0)]
+        private float _stopAccelerationDuration = 0.1f;
         /// <summary> 回転減衰。 </summary>
-        [SerializeField, Tooltip("回転減衰。")]
-        private float _rotationDamping;
+        [SerializeField, Tooltip("回転減衰。"), Min(0)]
+        private float _rotationDamping = 0.01f;
 
+        [Header("攻撃")]
         /// <summary> 攻撃力。 </summary>
-        [SerializeField, Tooltip("攻撃力。")]
-        private float _attackPower;
+        [SerializeField, Tooltip("攻撃力。"), Min(0)]
+        private float _attackPower = 10;
         /// <summary> 攻撃範囲。 </summary>
-        [SerializeField, Tooltip("攻撃範囲。")]
-        private float _attackRange;
-        /// <summary> 最大ヘルス。 </summary>
-        [SerializeField, Tooltip("最大ヘルス。")]
-        private float _maxHealth;
+        [SerializeField, Tooltip("攻撃範囲。"), Min(0)]
+        private float _attackRange = 10;
+        [SerializeField, Tooltip("攻撃時の硬直時間"), Min(0)]
+        private float _postAttackMoveLockDuration = 1;
+
+        [Header("防御")]
+        /// <summary> 体力。 </summary>
+        [SerializeField, Tooltip("体力。")]
+        private float _maxHealth = 100;
+
+        [Header("音楽同期")]
         /// <summary> スペシャルアタックパターン群。 </summary>
         [SerializeField, Tooltip("スペシャルアタックパターン群。")]
         private RythemPatternData[] _specialAttackPatterns;
-
-        /// <summary> 地面判定における法線の垂直閾値。 </summary>
-        [SerializeField, Range(0, 1), Tooltip("地面判定における法線の垂直閾値。")]
-        private float _groundNormalVerticalThreshold = DEFAULT_GROUND_NORMAL_VERTICAL_THRESHOLD;
         #endregion
     }
 }
