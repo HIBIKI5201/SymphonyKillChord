53c81c70f7f523f361d45f800644513694b68f90
HIBIKI_5201 <takuto5201@gmail.com>
2026-01-09 17:40:50 +0900

    [add]拍のパラメータで長さを取る

---
+++ Assets/Mock/MusicBattle_Mock/Scripts/Utility/BeatLength.cs
@@ -0,0 +1,30 @@
+using Mock.MusicBattle.MusicSync;
+using UnityEngine;
+
+namespace Mock.MusicBattle.Utility
+{
+    public struct BeatLength
+    {
+        public BeatLength(float signature, float count)
+        {
+            _signature = signature;
+            _count = count;
+        }
+
+        public double GetLength(double bpm)
+        {
+            double beatSeconds = 60d / bpm;
+
+            double barSeconds = beatSeconds * 4d;// 1小節は4/4固定。
+            double unitSeconds = barSeconds / _signature;
+
+            return unitSeconds * _count;
+        }
+        public double GetLength(IMusicBuffer buffer) => GetLength(buffer.CurrentBpm);
+
+        [SerializeField, Tooltip("拍子")]
+        private float _signature;
+        [SerializeField, Tooltip("数")]
+        private float _count;
+    }
+}
