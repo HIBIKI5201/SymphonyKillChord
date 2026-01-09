59380bc4e3b61b1ff1a63266c0e83ec3e25b2b99
HIBIKI_5201 <takuto5201@gmail.com>
2026-01-09 16:13:13 +0900

    [update]VFXモジュールを実装

--- Assets/Mock/MusicBattle_Mock/Scripts/Player/SpecialAttack/Module/SpecialAttackVFXModule.cs
+++ Assets/Mock/MusicBattle_Mock/Scripts/Player/SpecialAttack/Module/SpecialAttackVFXModule.cs
@@ -1,19 +1,53 @@
+using Mock.MusicBattle.Player;
+using System.Threading;
 using UnityEngine;
+using UnityEngine.VFX;
 
 namespace Mock.MusicBattle
 {
-    public class SpecialAttackVFXModule : MonoBehaviour
+    public class SpecialAttackVFXModule : ISpecialAttackModule
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        public void Execute(SpecialAttackDTO dto)
         {
-        
+            if (_vfxPrefab == null) { return; } 
+
+            Vector3 pos = dto.Player.transform.position;
+            pos += _offset;
+
+            VisualEffect vfx = GetVFX(dto.DestroyToken);
+
+            vfx.transform.position = pos;
+            vfx.Play();
         }
 
-        // Update is called once per frame
-        void Update()
+        [SerializeField, Tooltip("使用するVFX")]
+        private VisualEffect _vfxPrefab;
+        [SerializeField, Tooltip("発動者からのオフセット")]
+        private Vector3 _offset;
+
+        private VisualEffect _vfxInstance;
+
+        private VisualEffect GetVFX(CancellationToken token)
         {
-        
+            if (_vfxInstance != null) 
+            { 
+                return _vfxInstance;
+            }
+
+            return CreateVFX(token);
+        }
+
+        private VisualEffect CreateVFX(CancellationToken token)
+        {
+            VisualEffect vfx = Object.Instantiate(_vfxPrefab);
+            _vfxInstance = vfx;
+            token.Register(() =>
+            {
+                Object.Destroy(vfx.gameObject);
+                _vfxInstance = null;
+            });
+
+            return vfx;
         }
     }
 }
