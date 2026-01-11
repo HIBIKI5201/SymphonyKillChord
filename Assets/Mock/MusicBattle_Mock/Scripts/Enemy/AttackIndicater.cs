b34040e7c93e0b18937b611eee681399f2fbbe7b
HIBIKI_5201 <takuto5201@gmail.com>
2026-01-11 20:44:45 +0900

    [update]デカールを移動させるメソッドを実装

--- Assets/Mock/MusicBattle_Mock/Scripts/Enemy/AttackIndicater.cs
+++ Assets/Mock/MusicBattle_Mock/Scripts/Enemy/AttackIndicater.cs
@@ -8,8 +8,21 @@ namespace Mock.MusicBattle.Enemy
         public AttackIndicater(DecalProjector decal)
         {
             _decal = decal;
+            _transform = _decal.transform;
+        }
+
+        public void Move(float range)
+        {
+            Vector3 size = _decal.size;
+            size.z = range;
+            _decal.size = size;
+
+            Vector3 pos = _transform.localPosition;
+            pos.z = range / 2;
+            _transform.localPosition = pos;
         }
 
         private readonly DecalProjector _decal;
+        private readonly Transform _transform;
     }
 }
