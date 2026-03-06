ebecadcc684f2dd2f2fb9828e294bd19ab90eadb
HIBIKI_5201 <takuto5201@gmail.com>
2026-03-06 20:35:09 +0900

    [add]初期化クラスを追加

---
+++ Assets/DevelopProducts/Design/Architecture/Scripts/6.Composition/Initializer.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace DevelopProducts.Architecture.Composition
+{
+    public class Initializer : MonoBehaviour
+    {
+        public void Awake()
+        {
+            
+        }
+    }
+}
