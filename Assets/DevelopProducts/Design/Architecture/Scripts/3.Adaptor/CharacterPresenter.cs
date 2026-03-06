7951c48f14b4506c743e30cb9a70bcdefbb93db9
HIBIKI_5201 <takuto5201@gmail.com>
2026-03-06 22:24:20 +0900

    [update]エンティティを返すように

--- Assets/DevelopProducts/Design/Architecture/Scripts/3.Adaptor/CharacterPresenter.cs
+++ Assets/DevelopProducts/Design/Architecture/Scripts/3.Adaptor/CharacterPresenter.cs
@@ -9,16 +9,33 @@ namespace DevelopProducts.Architecture.Adaptor
         {
             _entity = entity;
             _view = view;
+
+            _entity.OnHealthChanged += HandleHealthChanged;
+            _view.UpdateHealth(_entity.CurrentHealth, _entity.MaxHealth);
         }
 
+        /// <summary> 管理しているキャラクターエンティティ。 </summary>
+        public CharacterEntity Entity => _entity;
+
+        /// <summary>
+        ///     キャラクターを移動させる。
+        /// </summary>
+        /// <param name="dir"> 移動方向。 </param>
         public void Move(Vector2 dir)
         {
-            Vector2 velocity = dir * _entity.Speed;
-            Debug.Log($"Velocity {velocity}");
-            _view.Move(dir);
+            _view.Move(dir * _entity.Speed);
         }
 
-        private CharacterEntity _entity;
-        private ICharacterView _view;
+        /// <summary>
+        ///     体力の変化をハンドルする。
+        /// </summary>
+        /// <param name="currentHealth"> 現在の体力。 </param>
+        private void HandleHealthChanged(float currentHealth)
+        {
+            _view.UpdateHealth(currentHealth, _entity.MaxHealth);
+        }
+
+        private readonly CharacterEntity _entity;
+        private readonly ICharacterView _view;
     }
 }
