4088a15a6b0705e139787725350c703b7d02cf0e
HIBIKI_5201 <takuto5201@gmail.com>
2026-03-06 22:24:45 +0900

    [update]バリデーションを追加

--- Assets/DevelopProducts/Design/Architecture/Scripts/5.View/InputBuffer.cs
+++ Assets/DevelopProducts/Design/Architecture/Scripts/5.View/InputBuffer.cs
@@ -5,12 +5,19 @@ namespace DevelopProducts.Architecture.View
 {
     public class InputBuffer : MonoBehaviour
     {
+        /// <summary>
+        ///     コントローラーをセットする。
+        /// </summary>
+        /// <param name="controller"> セットするコントローラー。 </param>
         public void SetController(CharacterController controller)
         {
             _controller = controller;
         }
 
-        public void Update()
+        /// <summary>
+        ///     更新処理。
+        /// </summary>
+        private void Update()
         {
             bool pressedW = Input.GetKey(KeyCode.W);
             bool pressedA = Input.GetKey(KeyCode.A);
@@ -35,9 +42,9 @@ namespace DevelopProducts.Architecture.View
                 dir += Vector2.right;
             }
 
-            if (pressedW || pressedA || pressedS || pressedD)
+            if (dir.sqrMagnitude > 0)
             {
-                _controller.Move(dir);
+                _controller.Move(dir.normalized);
             }
         }
 
