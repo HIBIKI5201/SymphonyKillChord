61f527012cee0ec5536bd93993be528c45b6b195
HIBIKI_5201 <takuto5201@gmail.com>
2026-03-06 22:09:53 +0900

    [update]キャラクターを動かすメソッドを実装

--- Assets/DevelopProducts/Design/Architecture/Scripts/5.View/CharacterView.cs
+++ Assets/DevelopProducts/Design/Architecture/Scripts/5.View/CharacterView.cs
@@ -1,27 +1,38 @@
 using UnityEngine;
 using DevelopProducts.Architecture.Adaptor;
 using CharacterController = DevelopProducts.Architecture.Adaptor.CharacterController;
+using DevelopProducts.Architecture.Domain;
 
 namespace DevelopProducts.Architecture.View
 {
-    public class CharacterView : MonoBehaviour
+    [RequireComponent(typeof(Rigidbody))]
+    public class CharacterView : MonoBehaviour, ICharacterView
     {
         public void SetController(CharacterController controller)
         {
             _controller = controller;
         }
 
-        void Start()
+        void ICharacterView.Move(Vector2 vel)
         {
-        
+            _rb.AddForce(new Vector3(vel.x, 0, vel.y), ForceMode.Impulse);
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
-
+        private Rigidbody _rb;
         private CharacterController _controller;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody>();
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.TryGetComponent(out CharacterView view))
+            {
+                view.TakeDmage();
+                _controller.AddDamage();
+            }
+        }
     }
 }
