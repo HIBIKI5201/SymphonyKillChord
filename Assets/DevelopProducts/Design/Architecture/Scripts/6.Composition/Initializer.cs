d25cf640772a90345add1052e7496ac735393d9e
HIBIKI_5201 <takuto5201@gmail.com>
2026-03-06 20:54:53 +0900

    [update]初期化を行う

--- Assets/DevelopProducts/Design/Architecture/Scripts/6.Composition/Initializer.cs
+++ Assets/DevelopProducts/Design/Architecture/Scripts/6.Composition/Initializer.cs
@@ -1,4 +1,8 @@
+using DevelopProducts.Architecture.Domain;
+using DevelopProducts.Architecture.InfraStructure;
+using DevelopProducts.Architecture.View;
 using UnityEngine;
+using CharacterController = DevelopProducts.Architecture.Adaptor.CharacterController;
 
 namespace DevelopProducts.Architecture.Composition
 {
@@ -6,7 +10,17 @@ namespace DevelopProducts.Architecture.Composition
     {
         public void Awake()
         {
-            
+            CharacterEntity entity = new(_characterStatus.Name, _characterStatus.Health);
+            CharacterController controller = new(entity);
+            _characterView.SetController(controller);
+            _buffer.SetController(controller);
         }
+
+        [SerializeField]
+        private CharacterStatus _characterStatus;
+        [SerializeField]
+        private CharacterView _characterView;
+        [SerializeField]
+        private InputBuffer _buffer;
     }
 }
