09fb4e552760ad3ae3472190e5d3072f44573267
HIBIKI_5201 <takuto5201@gmail.com>
2026-03-06 20:53:38 +0900

    [update]体力の初期化を行う

--- Assets/DevelopProducts/Design/Architecture/Scripts/1.Domain/CharacterEntity.cs
+++ Assets/DevelopProducts/Design/Architecture/Scripts/1.Domain/CharacterEntity.cs
@@ -1,8 +1,26 @@
 using UnityEngine;
+using System;
 
 namespace DevelopProducts.Architecture.Domain
 {
     public class CharacterEntity
     {
+        public CharacterEntity(string name, float health)
+        {
+            if (health <= 0) { throw new Exception($"{health}. Health must be greater than zero."); }
+
+            _name = name;
+            _maxHealth = health;
+            _currentHealth = health;
+        }
+
+        public void TakeDamage(float damage)
+        {
+            _currentHealth = Mathf.Max(_currentHealth - damage, 0);
+        }
+
+        private readonly string _name;
+        private readonly float _maxHealth;
+        private float _currentHealth;
     }
 }
