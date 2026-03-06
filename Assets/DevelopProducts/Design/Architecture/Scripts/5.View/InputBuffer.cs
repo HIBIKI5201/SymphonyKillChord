6b9ae887f734dfa6197c9add42d10150c01fef6f
HIBIKI_5201 <takuto5201@gmail.com>
2026-03-06 22:09:39 +0900

    [update]入力を受け取る

--- Assets/DevelopProducts/Design/Architecture/Scripts/5.View/InputBuffer.cs
+++ Assets/DevelopProducts/Design/Architecture/Scripts/5.View/InputBuffer.cs
@@ -10,6 +10,37 @@ namespace DevelopProducts.Architecture.View
             _controller = controller;
         }
 
+        public void Update()
+        {
+            bool pressedW = Input.GetKey(KeyCode.W);
+            bool pressedA = Input.GetKey(KeyCode.A);
+            bool pressedS = Input.GetKey(KeyCode.S);
+            bool pressedD = Input.GetKey(KeyCode.D);
+
+            Vector2 dir = Vector2.zero;
+            if (pressedW)
+            {
+                dir += Vector2.up;
+            }
+            if (pressedA)
+            {
+                dir += Vector2.left;
+            }
+            if (pressedS)
+            {
+                dir += Vector2.down;
+            }
+            if (pressedD)
+            {
+                dir += Vector2.right;
+            }
+
+            if (pressedW || pressedA || pressedS || pressedD)
+            {
+                _controller.Move(dir);
+            }
+        }
+
         private CharacterController _controller;
     }
 }
