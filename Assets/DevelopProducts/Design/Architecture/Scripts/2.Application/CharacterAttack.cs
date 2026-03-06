cc51f3cd642618cb5c4c357f8d1e4fe4f4d812ab
HIBIKI_5201 <takuto5201@gmail.com>
2026-03-06 20:53:55 +0900

    [update]攻撃クラスの初期化を追加

--- Assets/DevelopProducts/Design/Architecture/Scripts/2.Application/CharacterAttack.cs
+++ Assets/DevelopProducts/Design/Architecture/Scripts/2.Application/CharacterAttack.cs
@@ -1,8 +1,15 @@
+using DevelopProducts.Architecture.Domain;
 using UnityEngine;
 
 namespace DevelopProducts.Architecture.Application
 {
     public class CharacterAttack
     {
+        public CharacterAttack(CharacterEntity entity)
+        {
+            _entity = entity;
+        }
+
+        private readonly CharacterEntity _entity;
     }
 }
