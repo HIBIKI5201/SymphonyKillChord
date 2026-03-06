c5b44295cb21d64982e6dce18230581d44d90eb5
HIBIKI_5201 <takuto5201@gmail.com>
2026-03-06 20:54:12 +0900

    [update]攻撃クラスを生成するように

--- Assets/DevelopProducts/Design/Architecture/Scripts/3.Adaptor/CharacterController.cs
+++ Assets/DevelopProducts/Design/Architecture/Scripts/3.Adaptor/CharacterController.cs
@@ -1,8 +1,16 @@
+using DevelopProducts.Architecture.Application;
+using DevelopProducts.Architecture.Domain;
 using UnityEngine;
 
 namespace DevelopProducts.Architecture.Adaptor
 {
     public class CharacterController
     {
+        public CharacterController(CharacterEntity entity)
+        {
+            _attacker = new CharacterAttack(entity);
+        }
+
+        private CharacterAttack _attacker;
     }
 }
