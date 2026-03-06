426f4d0756bf09f0cbb9e8e478de29e2040710db
HIBIKI_5201 <takuto5201@gmail.com>
2026-03-06 20:36:21 +0900

    [add]キャラクターのステータスを追加

---
+++ Assets/DevelopProducts/Design/Architecture/Scripts/4.InfraStructure/CharacterStatus.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace DevelopProducts.Architecture.InfraStructure
+{
+    [CreateAssetMenu(fileName = nameof(CharacterStatus),
+        menuName = "DevelopProducts/" + nameof(CharacterStatus), order = 1)]
+    public class CharacterStatus : ScriptableObject
+    {
+    }
+}
