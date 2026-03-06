960ba091a57281c713c511fec43a0638c07523af
HIBIKI_5201 <takuto5201@gmail.com>
2026-03-06 22:10:32 +0900

    [update]プレゼンターに移動させる

--- Assets/DevelopProducts/Design/Architecture/Scripts/3.Adaptor/CharacterController.cs
+++ Assets/DevelopProducts/Design/Architecture/Scripts/3.Adaptor/CharacterController.cs
@@ -6,11 +6,24 @@ namespace DevelopProducts.Architecture.Adaptor
 {
     public class CharacterController
     {
-        public CharacterController(CharacterEntity entity)
+        public CharacterController(CharacterAttack attacker, CharacterPresenter presenter)
         {
-            _attacker = new CharacterAttack(entity);
+            _attacker = attacker;
+            _presenter = presenter;
         }
 
+        public void AddDamage(CharacterPresenter target)
+        {
+            _attacker.AddDamage(target);
+        }
+
+        public void Move(Vector2 dir)
+        {
+            Debug.Log($"Move: {dir}");
+            _presenter.Move(dir);
+        }
+
+        private CharacterPresenter _presenter;
         private CharacterAttack _attacker;
     }
 }
