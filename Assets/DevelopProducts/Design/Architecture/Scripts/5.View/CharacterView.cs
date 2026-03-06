deb08cd701b6e613f8a15c778164becc00269c79
HIBIKI_5201 <takuto5201@gmail.com>
2026-03-06 22:24:36 +0900

    [update]コントローラーを返すように

--- Assets/DevelopProducts/Design/Architecture/Scripts/5.View/CharacterView.cs
+++ Assets/DevelopProducts/Design/Architecture/Scripts/5.View/CharacterView.cs
@@ -8,17 +8,40 @@ namespace DevelopProducts.Architecture.View
     [RequireComponent(typeof(Rigidbody))]
     public class CharacterView : MonoBehaviour, ICharacterView
     {
+        /// <summary> 管理しているコントローラー。 </summary>
+        public CharacterController Controller => _controller;
+
+        /// <summary>
+        ///     コントローラーをセットする。
+        /// </summary>
+        /// <param name="controller"> セットするコントローラー。 </param>
         public void SetController(CharacterController controller)
         {
             _controller = controller;
         }
 
+        /// <summary>
+        ///     キャラクターを移動させる。
+        /// </summary>
+        /// <param name="vel"> 速度ベクトル。 </param>
         void ICharacterView.Move(Vector2 vel)
         {
             _rb.AddForce(new Vector3(vel.x, 0, vel.y), ForceMode.Impulse);
         }
 
+        /// <summary>
+        ///     体力の表示を更新する。
+        /// </summary>
+        /// <param name="currentHealth"> 現在の体力。 </param>
+        /// <param name="maxHealth"> 最大体力。 </param>
+        void ICharacterView.UpdateHealth(float currentHealth, float maxHealth)
+        {
+            Debug.Log($"{name} Health: {currentHealth} / {maxHealth}");
+        }
+
+        [SerializeField, Tooltip("操作するRigidbody。")]
         private Rigidbody _rb;
+
         private CharacterController _controller;
 
         private void Awake()
@@ -30,8 +53,8 @@ namespace DevelopProducts.Architecture.View
         {
             if (collision.gameObject.TryGetComponent(out CharacterView view))
             {
-                view.TakeDmage();
-                _controller.AddDamage();
+                // 衝突した相手にダメージを与える
+                _controller.AddDamage(view.Controller.Presenter);
             }
         }
     }
