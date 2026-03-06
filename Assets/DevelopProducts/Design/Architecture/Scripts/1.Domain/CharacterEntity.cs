0f645c6a6130cdf6e857b90546cb729b2015e063
HIBIKI_5201 <takuto5201@gmail.com>
2026-03-06 22:10:16 +0900

    [update]速度を受け取る

--- Assets/DevelopProducts/Design/Architecture/Scripts/1.Domain/CharacterEntity.cs
+++ Assets/DevelopProducts/Design/Architecture/Scripts/1.Domain/CharacterEntity.cs
@@ -5,13 +5,14 @@ namespace DevelopProducts.Architecture.Domain
 {
     public class CharacterEntity
     {
-        public CharacterEntity(string name, float health)
+        public CharacterEntity(string name, float health, float speed)
         {
             if (health <= 0) { throw new Exception($"{health}. Health must be greater than zero."); }
 
             _name = name;
             _maxHealth = health;
             _currentHealth = health;
+            _speed = speed;
         }
 
         public void TakeDamage(float damage)
@@ -19,8 +20,11 @@ namespace DevelopProducts.Architecture.Domain
             _currentHealth = Mathf.Max(_currentHealth - damage, 0);
         }
 
+        public float Speed => _speed;
+
         private readonly string _name;
         private readonly float _maxHealth;
         private float _currentHealth;
+        private readonly float _speed;
     }
 }
