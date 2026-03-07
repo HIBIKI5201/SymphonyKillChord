5ffb2bd29bfafafd2a75d4f252f1433e90ed80b0
HIBIKI_5201 <takuto5201@gmail.com>
2026-03-07 18:36:55 +0900

    [add]ディスコードの環境変数保持クラスを追加

---
+++ SinfoniaOperator/SinfoniaOperator/DiscordEnvironment.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SinfoniaStudio.SinfoniaOperator
+{
+    internal struct DiscordEnvironment
+    {
+        public DiscordEnvironment(
+            string discordBotTokenKey,
+            string discordChannelIDKey)
+        {
+            EnvironmentVariable discordBotToken = new(discordBotTokenKey);
+            EnvironmentVariable discordChannelID = new(discordChannelIDKey);
+
+            if (EnvironmentValidator.Validate([discordBotToken, discordChannelID])) { return; }
+
+            DiscordBotToken = discordBotToken.Value;
+            if (!ulong.TryParse(discordChannelID.Value, out DiscordChannelID))
+            {
+                Console.WriteLine($"DISCORD_CHANNEL_IDが数値に変換できませんでした。\nvalue {discordChannelID.Value}");
+                return;
+            }
+        }
+
+        public readonly string DiscordBotToken;
+        public readonly ulong DiscordChannelID;
+
+        public override string ToString()
+        {
+            StringBuilder sb = new();
+            sb.AppendLine($"DiscordBotToken: {(string.IsNullOrEmpty(DiscordBotToken) ? "null or empty" : "set")}, ");
+            sb.AppendLine($"DiscordChannelID: {DiscordChannelID}");
+            return sb.ToString();                ;
+        }
+    }
+}
