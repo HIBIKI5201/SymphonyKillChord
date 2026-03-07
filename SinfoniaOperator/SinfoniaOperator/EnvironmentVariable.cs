765eed0e0e3ff9cf8c402d0593a42f65b39197c0
HIBIKI_5201 <takuto5201@gmail.com>
2026-03-07 19:07:05 +0900

    [update]暗黙的に変換する機能を追加

--- SinfoniaOperator/SinfoniaOperator/EnvironmentVariable.cs
+++ SinfoniaOperator/SinfoniaOperator/EnvironmentVariable.cs
@@ -11,5 +11,25 @@ namespace SinfoniaStudio.SinfoniaOperator
             Key = key;
             Value = Environment.GetEnvironmentVariable(key) ?? string.Empty;
         }
+
+        public static implicit operator string(EnvironmentVariable variable)
+        {
+            if (string.IsNullOrEmpty(variable.Value))
+            {
+                throw new InvalidOperationException($"環境変数 {variable.Key} が見つかりませんでした。");
+            }
+            
+            return variable.Value;
+        }
+
+        public static implicit operator ulong(EnvironmentVariable variable)
+        {
+            if (!ulong.TryParse(variable.Value, out ulong result))
+            {
+                throw new InvalidOperationException($"環境変数 {variable.Key} の値 {variable.Value} を数値に変換できませんでした。");
+            }
+
+            return result;
+        }
     }
 }
