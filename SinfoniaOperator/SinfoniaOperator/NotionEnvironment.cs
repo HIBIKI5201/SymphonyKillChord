c875b0af1ae270351b41792eddce7d65582dad34
HIBIKI_5201 <takuto5201@gmail.com>
2026-03-07 18:37:06 +0900

    [add]Notionの環境変数保持クラスを追加

---
+++ SinfoniaOperator/SinfoniaOperator/NotionEnvironment.cs
@@ -0,0 +1,57 @@
+using System.Text;
+
+namespace SinfoniaStudio.SinfoniaOperator
+{
+    internal readonly struct NotionEnvironment
+    {
+        public NotionEnvironment(string notionTokenKey,
+                string databaseIDKey,
+                string datePropertyNameKey,
+                string namePropertyNameKey,
+                string statusPropertyNameKey,
+                string taskDoneStatusNameKey)
+        {
+            EnvironmentVariable notionToken = new(notionTokenKey);
+            EnvironmentVariable databaseID = new(databaseIDKey);
+            EnvironmentVariable datePropertyName = new(datePropertyNameKey);
+            EnvironmentVariable namePropertyName = new(namePropertyNameKey);
+            EnvironmentVariable statusPropertyName = new(statusPropertyNameKey);
+            EnvironmentVariable taskDoneStatusNam = new(taskDoneStatusNameKey);
+
+            if (EnvironmentValidator.Validate([
+                notionToken,
+                databaseID,
+                datePropertyName,
+                namePropertyName,
+                statusPropertyName,
+                taskDoneStatusNam]))
+                { return; }
+
+            NotionToken = notionToken.Value;
+            DatabaseID = databaseID.Value;
+            DatePropertyName = datePropertyName.Value;
+            NamePropertyName = namePropertyName.Value;
+            StatusPropertyName = statusPropertyName.Value;
+            TaskDoneStatusNam = taskDoneStatusNam.Value;
+        }
+
+        public readonly string NotionToken;
+        public readonly string DatabaseID;
+        public readonly string DatePropertyName;
+        public readonly string NamePropertyName;
+        public readonly string StatusPropertyName;
+        public readonly string TaskDoneStatusNam;
+
+        public override string ToString()
+        {
+            StringBuilder sb = new();
+            sb.AppendLine($"NotionToken: {(string.IsNullOrEmpty(NotionToken) ? "null or empty" : "set")}");
+            sb.AppendLine($"DatabaseID: {DatabaseID}");
+            sb.AppendLine($"DatePropertyName: {DatePropertyName}");
+            sb.AppendLine($"NamePropertyName: {NamePropertyName}");
+            sb.AppendLine($"StatusPropertyName: {StatusPropertyName}");
+            sb.AppendLine($"TaskDoneStatusNam: {TaskDoneStatusNam}");
+            return sb.ToString();
+        }
+    }
+}
