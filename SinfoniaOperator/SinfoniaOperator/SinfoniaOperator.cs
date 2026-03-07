6f90a0502340ce1e5bcde03fba085ec01aa6bd25
HIBIKI_5201 <takuto5201@gmail.com>
2026-03-07 19:09:25 +0900

    [update]try-catchするように

--- SinfoniaOperator/SinfoniaOperator/SinfoniaOperator.cs
+++ SinfoniaOperator/SinfoniaOperator/SinfoniaOperator.cs
@@ -16,17 +16,26 @@ namespace SinfoniaStudio.SinfoniaOperator
 
         public static async Task Main()
         {
-            DiscordEnvironment discordEnv = new DiscordEnvironment(
-                DISCORD_BOT_TOKEN,
-                DISCORD_CHANNEL_ID);
-            NotionEnvironment notionEnv = new NotionEnvironment(
-                NOTION_TOKEN,
-                NOTION_DATABASE_ID,
-                NOTION_DATABASE_DATE_PROPERTY,
-                NOTION_DATABASE_NAME_PROPERTY,
-                NOTION_DATABASE_STATUS_PROPERTY,
-                NOTION_DATABASE_STATUS_TASK_DONE_PROPERTY);
-
+            DiscordEnvironment discordEnv = default;
+            NotionEnvironment notionEnv = default;
+            try
+            {
+                discordEnv = new DiscordEnvironment(
+                    DISCORD_BOT_TOKEN,
+                    DISCORD_CHANNEL_ID);
+                notionEnv = new NotionEnvironment(
+                    NOTION_TOKEN,
+                    NOTION_DATABASE_ID,
+                    NOTION_DATABASE_DATE_PROPERTY,
+                    NOTION_DATABASE_NAME_PROPERTY,
+                    NOTION_DATABASE_STATUS_PROPERTY,
+                    NOTION_DATABASE_STATUS_TASK_DONE_PROPERTY);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return;
+            }
             Console.WriteLine("環境変数のチェックが完了しました。\n" +
                 $"discord env {discordEnv}\n" +
                 $"notion env {notionEnv}");
