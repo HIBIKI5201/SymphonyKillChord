066d2facc90c83a9ff4d07c683fa881d179082d7
ayato0913a <ayato0913a@gmail.com>
2026-03-11 18:44:48 +0900

    [ feat ] 入力フェーズを表すクラスを作成

--- Assets/DevelopProducts/Design/Persistent/Scripts/1.Domain/Input/InputPheseId.cs
+++ Assets/DevelopProducts/Design/Persistent/Scripts/1.Domain/Input/InputPheseId.cs
@@ -3,8 +3,41 @@ using UnityEngine;
 
 namespace DevelopProducts.Persistent.Domain.Input
 {
+    /// <summary>
+    ///     入力フェーズの識別子を表すクラス。
+    /// </summary>
     public class InputPheseId : IEquatable<InputPheseId>
     {
+        public int Value { get; }
 
+        public InputPheseId(int value)
+        {
+            Value = value;
+        }
+
+        public override int GetHashCode()
+        {
+            return Value;
+        }
+
+        public bool Equals(InputPheseId other)
+        {
+            return Value == other.Value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is InputPheseId other && Equals(other);
+        }
+
+        public static bool operator ==(InputPheseId left, InputPheseId right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(InputPheseId left, InputPheseId right)
+        {
+            return !left.Equals(right);
+        }
     }
 }
