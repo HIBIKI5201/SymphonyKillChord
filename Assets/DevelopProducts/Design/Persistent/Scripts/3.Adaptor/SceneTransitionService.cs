2432f997289c837a0e06d90f96494f41660d4e51
ayato0913a <ayato0913a@gmail.com>
2026-03-11 18:05:09 +0900

    [ feat ] シーン遷移する用のクラス作成

--- Assets/DevelopProducts/Design/Persistent/Scripts/3.Adaptor/SceneTransitionService.cs
+++ Assets/DevelopProducts/Design/Persistent/Scripts/3.Adaptor/SceneTransitionService.cs
@@ -4,45 +4,48 @@ using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneTransitionService : ISceneTransitionService
+namespace DevelopProducts.Persistent.Adaptor
 {
-    public async Task<bool> ChangeSceneAsync(string fromSceneName,
-        string toSceneName,
-        CancellationTokenSource cancellationTokenSource)
+    public class SceneTransitionService : ISceneTransitionService
     {
-        if (string.IsNullOrEmpty(toSceneName))
+        public async Task<bool> ChangeSceneAsync(string fromSceneName,
+            string toSceneName,
+            CancellationTokenSource cancellationTokenSource)
         {
-            Debug.LogError("シーン名が無効です。");
-            return false;
-        }
-
-        if (!SceneLoader.GetExistScene(toSceneName, out _))
-        {
-            bool loadSuccess = await SceneLoader.LoadScene(toSceneName,
-                null,
-                LoadSceneMode.Additive,
-                cancellationTokenSource.Token);
-
-            if (!loadSuccess)
+            if (string.IsNullOrEmpty(toSceneName))
             {
-                Debug.LogError($"シーンのロードに失敗 : {toSceneName}");
+                Debug.LogError("シーン名が無効です。");
                 return false;
             }
-        }
 
-        SceneLoader.SetActiveScene(toSceneName);
-
-        if (!string.IsNullOrEmpty(fromSceneName) && SceneLoader.GetExistScene(fromSceneName, out _))
-        {
-            bool unloadSuccess = await SceneLoader.UnloadScene(fromSceneName,
-                null,
-                cancellationTokenSource.Token);
-            if (!unloadSuccess)
+            if (!SceneLoader.GetExistScene(toSceneName, out _))
             {
-                Debug.LogError($"シーンのアンロードに失敗 : {fromSceneName}");
-                return false;
+                bool loadSuccess = await SceneLoader.LoadScene(toSceneName,
+                    null,
+                    LoadSceneMode.Additive,
+                    cancellationTokenSource.Token);
+
+                if (!loadSuccess)
+                {
+                    Debug.LogError($"シーンのロードに失敗 : {toSceneName}");
+                    return false;
+                }
             }
+
+            SceneLoader.SetActiveScene(toSceneName);
+
+            if (!string.IsNullOrEmpty(fromSceneName) && SceneLoader.GetExistScene(fromSceneName, out _))
+            {
+                bool unloadSuccess = await SceneLoader.UnloadScene(fromSceneName,
+                    null,
+                    cancellationTokenSource.Token);
+                if (!unloadSuccess)
+                {
+                    Debug.LogError($"シーンのアンロードに失敗 : {fromSceneName}");
+                    return false;
+                }
+            }
+            return true;
         }
-        return true;
     }
 }
