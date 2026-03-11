deaf1ed9e82e858bb987e72ccbfa863b8d5ac5c6
ayato0913a <ayato0913a@gmail.com>
2026-03-11 21:11:33 +0900

    [ feat ] UnityのInputActionの入力をバッファに記録するユースケースクラスを作成

--- Assets/DevelopProducts/Design/Persistent/Scripts/2.Application/BufferInputActionUsecase.cs
+++ Assets/DevelopProducts/Design/Persistent/Scripts/2.Application/BufferInputActionUsecase.cs
@@ -1,6 +1,26 @@
+using DevelopProducts.Persistent.Domain.Input;
 using UnityEngine;
 
-public class BufferInputActionUsecase
+namespace DevelopProducts.Persistent.Application
 {
-    
+    /// <summary>
+    ///     UnityのInputActionの入力をバッファに記録するユースケース。
+    /// </summary>
+    public class BufferInputActionUsecase
+    {
+        public BufferInputActionUsecase(IInputBufferWriter inputBufferWriter)
+        {
+            _inputBufferWriter = inputBufferWriter;
+        }
+
+        public void Execute(InputActionId actionId,
+            InputPheseId pheseId,
+            float timestamp)
+        {
+            BufferedInput input = new BufferedInput(actionId, pheseId, timestamp);
+            _inputBufferWriter.Push(input);
+        }
+
+        private readonly IInputBufferWriter _inputBufferWriter;
+    }
 }
