4aacec7b3f23d4c6295f96c885e1c2f92a73244a
ayato0913a <ayato0913a@gmail.com>
2026-03-12 06:13:37 +0900

    [ fix ] 今の形に変更

--- Assets/DevelopProducts/Design/Persistent/Scripts/1.Domain/Input/InputActionIds.cs
+++ Assets/DevelopProducts/Design/Persistent/Scripts/1.Domain/Input/InputActionIds.cs
@@ -8,18 +8,21 @@ namespace DevelopProducts.Persistent.Domain.Input
     /// </summary>
     public static class InputActionIds
     {
-        public static readonly InputActionId Move = new InputActionId(1);
-        public static readonly InputActionId Attack = new InputActionId(2);
-        public static readonly InputActionId Skill = new InputActionId(3);
+        // Common
+        public static readonly InputActionId Option = new InputActionId(1);
 
+        // InGame
+        public static readonly InputActionId Move = new InputActionId(10);
+
+        // OutGame
         public static readonly InputActionId Submit = new InputActionId(100);
         public static readonly InputActionId Cancel = new InputActionId(101);
 
         public static string GetActionName(InputActionId actionId)
         {
+            if(actionId == Option) return nameof(Option);
+
             if (actionId == Move) return nameof(Move);
-            if (actionId == Attack) return nameof(Attack);
-            if (actionId == Skill) return nameof(Skill);
 
             if (actionId == Submit) return nameof(Submit);
             if (actionId == Cancel) return nameof(Cancel);
