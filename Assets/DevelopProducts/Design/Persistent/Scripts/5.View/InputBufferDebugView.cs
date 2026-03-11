22c4e52554ba32e63cbdcd2d11e7dc1dc57f2d0a
ayato0913a <ayato0913a@gmail.com>
2026-03-11 23:46:09 +0900

    [ feat] 入力履歴のデバッグ表示をするクラスを作成

--- Assets/DevelopProducts/Design/Persistent/Scripts/5.View/InputBufferDebugView.cs
+++ Assets/DevelopProducts/Design/Persistent/Scripts/5.View/InputBufferDebugView.cs
@@ -1,16 +1,42 @@
+using DevelopProducts.Persistent.Application;
+using TMPro;
 using UnityEngine;
 
-public class InputBufferDebugView : MonoBehaviour
+namespace DevelopProducts.Persistent.View
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    /// <summary>
+    ///     入力履歴のデバッグ表示を行うクラス。
+    /// </summary>
+    public class InputBufferDebugView : MonoBehaviour
     {
-        
-    }
+        public void Initialize(IInputBufferReader inputBufferReader) 
+        {
+            _inputBufferReader = inputBufferReader;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [SerializeField] private TMP_Text _text;
+        [SerializeField] private int _previewCount;
+
+        private IInputBufferReader _inputBufferReader;
+
+        private void Update()
+        {
+            if (_inputBufferReader == null || _text == null)
+            {
+                return;
+            }
+
+            int count = Mathf.Min(_inputBufferReader.Count, _previewCount);
+
+            System.Text.StringBuilder sb = new();
+
+            for (int i = 0; i < count; i++)
+            {
+                int offset = count - 1 - i;
+                sb.AppendLine(_inputBufferReader.GetLast(offset).ToString());
+            }
+
+            _text.text = sb.ToString();
+        }
     }
 }
