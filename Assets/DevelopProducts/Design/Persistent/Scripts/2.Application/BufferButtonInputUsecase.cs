8ae22ce5640086546848b3708a18bb1623f77982
ayato0913a <ayato0913a@gmail.com>
2026-03-11 21:11:59 +0900

    [ feat ] ボタン入力をバッファに記録するユースケースクラスを作成

--- Assets/DevelopProducts/Design/Persistent/Scripts/2.Application/BufferButtonInputUsecase.cs
+++ Assets/DevelopProducts/Design/Persistent/Scripts/2.Application/BufferButtonInputUsecase.cs
@@ -1,6 +1,26 @@
+using DevelopProducts.Persistent.Domain.Input;
 using UnityEngine;
 
-public class BufferButtonInputUsecase
+namespace DevelopProducts.Persistent.Application
 {
-    
+    /// <summary>
+    ///     ボタン入力をバッファに記録するユースケース。
+    /// </summary>
+    public class BufferButtonInputUsecase
+    {
+        public BufferButtonInputUsecase(IInputBufferWriter inputBufferWriter)
+        {
+            _inputBufferWriter = inputBufferWriter;
+        }
+
+        public void Execute(InputActionId actionId,
+            InputPheseId pheseId,
+            float timestamp)
+        {
+            BufferedInput input = new BufferedInput(actionId, pheseId, timestamp);
+            _inputBufferWriter.Push(input);
+        }
+
+        private readonly IInputBufferWriter _inputBufferWriter;
+    }
 }
