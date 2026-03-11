3f1da36b9281e34ea8c87ceec7e784dcfb99c204
ayato0913a <ayato0913a@gmail.com>
2026-03-12 01:12:27 +0900

    [ feat ] 入力Mapを切り替える用のインターフェイスを作成

--- Assets/DevelopProducts/Design/Persistent/Scripts/2.Application/SwichInputMapUseCase.cs
+++ Assets/DevelopProducts/Design/Persistent/Scripts/2.Application/SwichInputMapUseCase.cs
@@ -1,6 +1,34 @@
 using UnityEngine;
+using DevelopProducts.Persistent.Domain.Input;
 
-public class SwichInputMapUseCase
+namespace DevelopProducts.Persistent.Application
 {
-    
+    /// <summary>
+    ///     入力マップを切り替えるユースケース。
+    ///     シーンが変わった時など異なる入力マップを使用する場合などに使用される。
+    /// </summary>
+    public class SwichInputMapUseCase
+    {
+        public SwichInputMapUseCase(IInputMapController inputMapController)
+        {
+            _inputMapController = inputMapController;
+        }
+
+        public void ToInGame()
+        {
+            _inputMapController.EnableCommonWith(InputMapIds.InGame);
+        }
+
+        public void ToOutGame()
+        {
+            _inputMapController.EnableCommonWith(InputMapIds.OutGame);
+        }
+
+        public void DisableAll()
+        {
+            _inputMapController.DisableAll();
+        }
+
+        private readonly IInputMapController _inputMapController;
+    }
 }
