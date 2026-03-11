e7f0772d14253229a52e8b90a6e636f7e4de74dd
ayato0913a <ayato0913a@gmail.com>
2026-03-11 17:06:21 +0900

    [ feat ] 実装

--- Assets/DevelopProducts/Design/Persistent/Scripts/6.Composition/PersistentEntryPoint.cs
+++ Assets/DevelopProducts/Design/Persistent/Scripts/6.Composition/PersistentEntryPoint.cs
@@ -1,16 +1,45 @@
+using SymphonyFrameWork.System;
+using System.Threading;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class PersistentEntryPoint : MonoBehaviour
+namespace DevelopProducts.Persistent.Composition
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    /// <summary>
+    ///     常駐シーン起動時のエントリーポイントとなるクラス。
+    /// </summary>
+    public class PersistentEntryPoint : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private string _firstSceneName;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private CancellationTokenSource _cancellationTokenSource;
+
+        private async void Start()
+        {
+            _cancellationTokenSource = new CancellationTokenSource();
+
+            if (!SceneLoader.GetExistScene(_firstSceneName, out _))
+            {
+                bool success = await SceneLoader.LoadScene(_firstSceneName,
+                    null,
+                    LoadSceneMode.Additive,
+                    _cancellationTokenSource.Token
+                    );
+
+                if (!success)
+                {
+                    Debug.LogError($"初回ロードに失敗 : {_firstSceneName}");
+                    return;
+                }
+            }
+
+            SceneLoader.SetActiveScene(_firstSceneName);
+        }
+
+        private void OnDestroy()
+        {
+            _cancellationTokenSource.Cancel();
+            _cancellationTokenSource.Dispose();
+        }
     }
 }
