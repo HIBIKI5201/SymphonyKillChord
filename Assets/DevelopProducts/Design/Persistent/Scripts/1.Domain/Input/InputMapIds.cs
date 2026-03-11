b1af3a12dcfb1a93395731cd879f53de6d201b8e
ayato0913a <ayato0913a@gmail.com>
2026-03-12 00:50:48 +0900

    [ feat ] 入力マップのIDを定義するクラスを作成

--- Assets/DevelopProducts/Design/Persistent/Scripts/1.Domain/Input/InputMapIds.cs
+++ Assets/DevelopProducts/Design/Persistent/Scripts/1.Domain/Input/InputMapIds.cs
@@ -1,6 +1,24 @@
 using UnityEngine;
 
-public class InputMapIds
+namespace DevelopProducts.Persistent.Domain.Input
 {
-    
+    /// <summary>
+    ///     入力マップのIDを定義するクラス。
+    ///     入力マップは、ゲーム内のアクションと入力を紐づけるためのもの。
+    /// </summary>
+    public readonly struct InputMapIds
+    {
+        public static readonly InputMapId Common = new(1);
+        public static readonly InputMapId InGame = new(2);
+        public static readonly InputMapId OutGame = new(3);
+
+        public static string GetMapName(InputMapId mapId)
+        {
+            if (mapId == Common) return nameof(Common);
+            if (mapId == InGame) return nameof(InGame);
+            if (mapId == OutGame) return nameof(OutGame);
+
+            return $"Unknown({mapId.Value})";
+        }
+    }
 }
