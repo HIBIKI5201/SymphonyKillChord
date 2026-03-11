0213d8e7d7afddf81084ef6b0266a015a2b0d254
ayato0913a <ayato0913a@gmail.com>
2026-03-11 17:30:08 +0900

    [ feat ] 実装

--- Assets/DevelopProducts/Design/Persistent/Scripts/2.Application/ISceneTransitionService.cs
+++ Assets/DevelopProducts/Design/Persistent/Scripts/2.Application/ISceneTransitionService.cs
@@ -1,6 +1,8 @@
+using System.Threading;
+using System.Threading.Tasks;
 using UnityEngine;
 
-public class ISceneTransitionService
+public interface ISceneTransitionService
 {
-    
+    Task<bool> ChangeSceneAsync(string fromSceneName, string toSceneName, CancellationTokenSource cancellationTokenSource);
 }
