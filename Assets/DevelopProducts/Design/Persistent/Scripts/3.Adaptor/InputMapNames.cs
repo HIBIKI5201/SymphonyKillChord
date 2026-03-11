e34124e066b901e398afc076513344508c200555
ayato0913a <ayato0913a@gmail.com>
2026-03-12 02:40:50 +0900

    [ fix ] 定数型で扱うように

--- Assets/DevelopProducts/Design/Persistent/Scripts/3.Adaptor/InputMapNames.cs
+++ Assets/DevelopProducts/Design/Persistent/Scripts/3.Adaptor/InputMapNames.cs
@@ -1,6 +1,14 @@
 using UnityEngine;
 
-public class InputMapNames
+namespace DevelopProducts.Persistent.Adaptor
 {
-    
+    /// <summary>
+    ///     UnityのInputSystemのInputMapの名前を定数で管理するクラス。
+    /// </summary>
+    public static class InputMapNames
+    {
+        public const string Common = "Common";
+        public const string InGame = "InGame";
+        public const string OutGame = "OutGame";
+    }
 }
