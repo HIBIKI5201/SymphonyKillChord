fcf4f946487dbe8d1eaf8511912d02531c2efedb
ayato0913a <ayato0913a@gmail.com>
2026-03-11 17:29:58 +0900

    [ feat ] 実装

--- Assets/DevelopProducts/Design/Persistent/Scripts/3.Adaptor/SceneTransitionService.cs
+++ Assets/DevelopProducts/Design/Persistent/Scripts/3.Adaptor/SceneTransitionService.cs
@@ -1,6 +1,48 @@
+using SymphonyFrameWork.System;
+using System.Threading;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class SceneTransitionService
+public class SceneTransitionService : ISceneTransitionService
 {
-    
+    public async Task<bool> ChangeSceneAsync(string fromSceneName,
+        string toSceneName,
+        CancellationTokenSource cancellationTokenSource)
+    {
+        if (string.IsNullOrEmpty(toSceneName))
+        {
+            Debug.LogError("シーン名が無効です。");
+            return false;
+        }
+
+        if (!SceneLoader.GetExistScene(toSceneName, out _))
+        {
+            bool loadSuccess = await SceneLoader.LoadScene(toSceneName,
+                null,
+                LoadSceneMode.Additive,
+                cancellationTokenSource.Token);
+
+            if (!loadSuccess)
+            {
+                Debug.LogError($"シーンのロードに失敗 : {toSceneName}");
+                return false;
+            }
+        }
+
+        SceneLoader.SetActiveScene(toSceneName);
+
+        if (!string.IsNullOrEmpty(fromSceneName) && SceneLoader.GetExistScene(fromSceneName, out _))
+        {
+            bool unloadSuccess = await SceneLoader.UnloadScene(fromSceneName,
+                null,
+                cancellationTokenSource.Token);
+            if (!unloadSuccess)
+            {
+                Debug.LogError($"シーンのアンロードに失敗 : {fromSceneName}");
+                return false;
+            }
+        }
+        return true;
+    }
 }
