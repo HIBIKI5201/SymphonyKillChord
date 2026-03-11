fa2cf25a5c20e1c838377dc32db9b7380c90351d
ayato0913a <ayato0913a@gmail.com>
2026-03-11 20:19:42 +0900

    [ feat ]  バッファに記録する入力1件分イベントを表す構造体を作成

--- Assets/DevelopProducts/Design/Persistent/Scripts/1.Domain/Input/BufferedInput.cs
+++ Assets/DevelopProducts/Design/Persistent/Scripts/1.Domain/Input/BufferedInput.cs
@@ -1,6 +1,26 @@
 using UnityEngine;
 
-public class BufferedInput
+namespace DevelopProducts.Persistent.Domain.Input
 {
-    
+    /// <summary>
+    ///     バッファに記録する入力1件分イベントを表す構造体。
+    /// </summary>
+    public readonly struct BufferedInput
+    {
+        public BufferedInput(InputActionId actionId, InputPheseId pheseId, float timestamp)
+        {
+            ActionId = actionId;
+            PheseId = pheseId;
+            Timestamp = timestamp;
+        }
+
+        public InputActionId ActionId { get; }
+        public InputPheseId PheseId { get; }
+        public float Timestamp { get; }
+
+        public override string ToString()
+        {
+            return $"BufferedInput(ActionId: {InputActionIds.GetActionName(ActionId)}, PheseId: {InputPheseIds.GetPheseName(PheseId)}, Timestamp: {Timestamp})";
+        }
+    }
 }
