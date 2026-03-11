7984f579c605783bc9e076d4001c45fb7280afca
ayato0913a <ayato0913a@gmail.com>
2026-03-12 07:58:12 +0900

    [ feat ] シーンでのUI入力の初期化クラスを作成

--- Assets/DevelopProducts/Design/Persistent/Scripts/6.Composition/SceneInputViewInstaller.cs
+++ Assets/DevelopProducts/Design/Persistent/Scripts/6.Composition/SceneInputViewInstaller.cs
@@ -1,16 +1,55 @@
+using DevelopProducts.Persistent.View;
 using UnityEngine;
 
-public class SceneInputViewInstaller : MonoBehaviour
+namespace DevelopProducts.Persistent.Composition
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    /// <summary>
+    ///     シーンに配置して、SceneInputViewをインストールするクラス。
+    ///     モバイル向けの入力ボタンのViewを初期化する。
+    /// </summary>
+    public class SceneInputViewInstaller : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private MobileInputButtonView[] _mobileInputButtonViews;
+        [SerializeField] private MobileMoveButtonView[] _mobileMoveButtonViews;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Start()
+        {
+            PersistentInputInstaller persistentInstaller = FindFirstObjectByType<PersistentInputInstaller>();
+
+            if (persistentInstaller == null)
+            {
+                Debug.LogError("PersistentInputInstaller が見つかりません。");
+                return;
+            }
+            if(_mobileInputButtonViews != null)
+            {
+                for (int i = 0; i < _mobileInputButtonViews.Length; i++)
+                {
+                    if (_mobileInputButtonViews[i] == null)
+                    {
+                        continue;
+                    }
+
+                    _mobileInputButtonViews[i].Initialize(
+                        persistentInstaller.BufferButtonInputUsecase,
+                        persistentInstaller.TimestampProvider);
+                }
+            }
+
+            if(_mobileMoveButtonViews != null)
+            {
+                for (int i = 0; i < _mobileMoveButtonViews.Length; i++)
+                {
+                    if (_mobileMoveButtonViews[i] == null)
+                    {
+                        continue;
+                    }
+
+                    _mobileMoveButtonViews[i].Initialize(
+                        persistentInstaller.BufferMoveInputUsecase,
+                        persistentInstaller.TimestampProvider);
+                }
+            }
+        }
     }
 }
