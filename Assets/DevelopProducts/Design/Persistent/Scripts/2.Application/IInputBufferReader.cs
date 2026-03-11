7fa87fecf58b1ef41a0d0042d2462603f3623af4
ayato0913a <ayato0913a@gmail.com>
2026-03-11 20:20:26 +0900

    [ add ] 入力を読み取る用のインターフェイスを作成

--- Assets/DevelopProducts/Design/Persistent/Scripts/2.Application/IInputBufferReader.cs
+++ Assets/DevelopProducts/Design/Persistent/Scripts/2.Application/IInputBufferReader.cs
@@ -1,6 +1,30 @@
+using DevelopProducts.Persistent.Domain.Input;
 using UnityEngine;
 
-public class IInputBufferReader
+namespace DevelopProducts.Persistent.Application
 {
-    
+    /// <summary>
+    ///     入力履歴を読み取るためのインターフェース。
+    /// </summary>
+    public interface IInputBufferReader
+    {
+        /// <summary> 現在の履歴件数 </summary>
+        int Count { get; }
+
+        /// <summary>
+        ///     古い順に入力履歴を取得する。
+        ///     indexは0から始まり、0は最も古い入力を指す。
+        /// </summary>
+        /// <param name="index">取得する履歴のインデックス</param>
+        /// <returns>指定したインデックスの入力履歴</returns>
+        BufferedInput GetAt(int index);
+
+        /// <summary>
+        ///     新しい順に入力履歴を取得する。
+        ///     オフセットは0から始まり、0は最新の入力を指す。
+        /// </summary>
+        /// <param name="offset">取得する履歴のオフセット</param>
+        /// <returns>指定したオフセットの入力履歴</returns>
+        BufferedInput GetLast(int offset = 0);
+    }
 }
