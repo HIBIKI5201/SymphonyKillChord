85bce66d1d7c4be51e7398df853a456954519bff
ChouTakushin <takushin.chou@gmail.com>
2026-03-15 07:52:13 +0900

    [update] SymphonyFrameworkに基づいた独自セーブデータシステムを作成。アーキテクチャ規定の内容に合わせてコード構成を調整する

---
+++ Assets/DevelopProducts/Research/Chou/OutGame/Scripts/SaveData/1.Domain/KillChordGameData.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+namespace Research.Chou.OutGame
+{
+    /// <summary>
+    ///     セーブデータクラス
+    /// </summary>
+    public class KillChordGameData
+    {
+        public KillChordGameData()
+        {
+            // TODO 仮の初期値。
+            Gold = 0;
+            HpMax = 100;
+            Attack = 10;
+            CritRate = 0.1f;
+            CritScale = 1.3f;
+            Equipments = new List<int>(new int[] { 0, 0, 0 });
+            Skills = new List<int>(new int[] { 0, 0, 0 });
+            MissionProgress = new List<int>();
+            EquipmentUnlock = new List<int>();
+            SkillUnlock = new List<int>();
+            MissionUnlock = new List<int>();
+        }
+        /// <summary>お金</summary>
+        public long Gold { get; set; }
+        /// <summary>HP最大値</summary>
+        public float HpMax { get; set; }
+        /// <summary>基礎攻撃力</summary>
+        public float Attack { get; set; }
+        /// <summary>クリティカル確率</summary>
+        public float CritRate { get; set; }
+        /// <summary>クリティカル倍率</summary>
+        public float CritScale { get; set; }
+        /// <summary>装備中の装備ID</summary>
+        public List<int> Equipments { get; set; }
+        /// <summary>装備中のスキルID</summary>
+        public List<int> Skills { get; set; }
+        /// <summary>ミッション進捗</summary>
+        public List<int> MissionProgress { get; set; }
+        /// <summary>装備解放状況</summary>
+        public List<int> EquipmentUnlock { get; set; }
+        /// <summary>スキル解放状況</summary>
+        public List<int> SkillUnlock { get; set; }
+        /// <summary>ミッション解放状況</summary>
+        public List<int> MissionUnlock { get; set; }
+    }
+}
---
+++ Assets/DevelopProducts/Research/Chou/OutGame/Scripts/SaveData/4.InfraStructure/SaveGamePipeline.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+namespace Research.Chou.OutGame
+{
+    /// <summary>
+    ///     セーブ処理の実装。
+    /// </summary>
+    public class SaveGamePipeline
+    {
+        public SaveGamePipeline(SaveDataEntity saveDataEntity, SaveLoadEvents saveLoadEvents)
+        {
+            _isSaving = false;
+            _saveDataEntity = saveDataEntity;
+            _saveLoadEvents = saveLoadEvents;
+        }
+
+        #region パブリックメソッド
+        /// <summary>
+        ///     セーブ処理を行う。
+        /// </summary>
+        /// <param name="data">セーブデータ</param>
+        public void  SaveAsync(KillChordGameData data)
+        {
+            if (_isSaving) return;
+
+            SaveAsyncTask().Forget();
+            _saveData = _saveDataEntity.SaveData;
+            SetSaveData(data);
+            _saveDataEntity.Save();
+        }
+        #endregion
+
+        private SaveDataEntity _saveDataEntity;
+        private KillChordGameData _saveData;
+        private SaveLoadEvents _saveLoadEvents;
+        private bool _isSaving;
+
+        #region プライベートメソッド
+        /// <summary>
+        ///     セーブ開始イベントを発火し、一定時間後にセーブ終了イベントを発火する。
+        /// </summary>
+        /// <returns></returns>
+        private async Awaitable SaveAsyncTask()
+        {
+            _isSaving = true;
+            _saveLoadEvents.InvokeSaveStart();
+            await Awaitable.WaitForSecondsAsync(Constants.SAVE_LOAD_WAIT_DUR);
+            _isSaving = false;
+            _saveLoadEvents.InvokeSaveEnd();
+        }
+        /// <summary>
+        ///     セーブデータの値をSymphonyFrameworkのセーブデータオブジェクトに設定する。
+        /// </summary>
+        /// <param name="newData"></param>
+        private void SetSaveData(KillChordGameData newData)
+        {
+            _saveData.Gold = newData.Gold;
+            _saveData.HpMax = newData.HpMax;
+            _saveData.Attack = newData.Attack;
+            _saveData.CritRate = newData.CritRate;
+            _saveData.CritScale = newData.CritScale;
+            _saveData.MissionProgress = newData.MissionProgress;
+            _saveData.MissionUnlock = newData.MissionUnlock;
+            _saveData.EquipmentUnlock = newData.EquipmentUnlock;
+            _saveData.SkillUnlock = newData.SkillUnlock;
+        }
+        #endregion
+
+    }
+}
