3a70ccd5ac6d3b036821b7861255a54e082403a0
ayato0913a <ayato0913a@gmail.com>
2026-03-16 08:33:17 +0900

    [ feat ] クリア条件を表すインターフェイスを作成

--- Assets/DevelopProducts/Design/GameMode/Scripts/1.Domain/IClearCondition.cs
+++ Assets/DevelopProducts/Design/GameMode/Scripts/1.Domain/IClearCondition.cs
@@ -1,6 +1,13 @@
-using UnityEngine;
-
-public class IClearCondition
+namespace DevelopProducts.Design.GameMode.Domain
 {
-    
+    /// <summary>
+    ///     クリア条件を表すインターフェース。
+    /// </summary>
+    public interface IClearCondition
+    {
+        /// <summary> 条件を満たしているかどうかを評価するメソッド。 </summary>
+        bool IsSatisfied(StageRuntimeContext context);
+        /// <summary> 条件の説明を取得するメソッド。 </summary>
+        string GetDescription();
+    }
 }
