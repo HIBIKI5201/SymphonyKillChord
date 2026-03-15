f5ef52a46be3792f79df946784c4b6e71f06f1e4
ayato0913a <ayato0913a@gmail.com>
2026-03-16 08:00:16 +0900

    [ feat ] 敵のidや名前を決めるためのSOクラスを作成

--- Assets/DevelopProducts/Design/GameMode/Scripts/4.InfraStructure/EnemyDefinition.cs
+++ Assets/DevelopProducts/Design/GameMode/Scripts/4.InfraStructure/EnemyDefinition.cs
@@ -1,7 +1,14 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "EnemyDefinition", menuName = "Scriptable Objects/EnemyDefinition")]
-public class EnemyDefinition : ScriptableObject
+namespace DevelopProducts.Design.GameMode.InfraStructure
 {
-    
+    [CreateAssetMenu(fileName = "EnemyDefinition", menuName = "Scriptable Objects/EnemyDefinition")]
+    public class EnemyDefinition : ScriptableObject
+    {
+        public string Id => _id;
+        public string DisplayName => _displayName;
+
+        [SerializeField] private string _id;
+        [SerializeField] private string _displayName;
+    }
 }
