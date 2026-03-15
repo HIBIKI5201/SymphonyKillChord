c2e4730a50de77c2e03be832e2427a99c725b6ce
ayato0913a <ayato0913a@gmail.com>
2026-03-16 08:29:36 +0900

    [ feat ] ステージの実行時コンテキストを表すクラス

--- Assets/DevelopProducts/Design/GameMode/Scripts/1.Domain/StageRuntimeContext.cs
+++ Assets/DevelopProducts/Design/GameMode/Scripts/1.Domain/StageRuntimeContext.cs
@@ -1,6 +1,25 @@
 using UnityEngine;
 
-public class StageRuntimeContext
+namespace DevelopProducts.Design.GameMode.Domain
 {
-    
+    /// <summary>
+    ///     Domain層で使用される、ステージの実行時コンテキストを表すクラス。
+    /// </summary>
+    public class StageRuntimeContext
+    {
+        public StageRuntimeContext(PlayerRuntimeState playerRuntimeState, EnemyKillStatics enemyKillStatics, StageTimeState stageTimeState)
+        {
+            _playerRuntimeState = playerRuntimeState;
+            _enemyKillStatics = enemyKillStatics;
+            _stageTimeState = stageTimeState;
+        }
+
+        public PlayerRuntimeState PlayerRuntimeState => _playerRuntimeState;
+        public EnemyKillStatics EnemyKillStatics => _enemyKillStatics;
+        public StageTimeState StageTimeState => _stageTimeState;
+
+        private readonly PlayerRuntimeState _playerRuntimeState;
+        private readonly EnemyKillStatics _enemyKillStatics;
+        private readonly StageTimeState _stageTimeState;
+    }
 }
