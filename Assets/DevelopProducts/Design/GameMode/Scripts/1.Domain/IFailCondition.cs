456b5932eef35755d7ad64b0afdf08579c379af3
ayato0913a <ayato0913a@gmail.com>
2026-03-16 08:31:21 +0900

    [ feat ] 失敗条件を表すインターフェイスを作成

--- Assets/DevelopProducts/Design/GameMode/Scripts/1.Domain/IFailCondition.cs
+++ Assets/DevelopProducts/Design/GameMode/Scripts/1.Domain/IFailCondition.cs
@@ -1,6 +1,13 @@
-using UnityEngine;
-
-public class IFailCondition
+namespace DevelopProducts.Design.GameMode.Domain
 {
-    
+    /// <summary>
+    ///     失敗条件を表すインターフェース。
+    /// </summary>
+    public interface IFailCondition
+    {
+        /// <summary> 条件を満たしているかどうかを評価するメソッド。 </summary>
+        bool IsSatisfied(StageRuntimeContext context);
+        /// <summary> 条件の説明を取得するメソッド。 </summary>
+        string GetDescription();
+    }
 }
