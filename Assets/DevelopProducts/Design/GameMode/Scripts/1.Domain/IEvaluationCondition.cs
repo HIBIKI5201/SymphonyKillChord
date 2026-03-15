3538ec83f7de40a8e0b0eee4d6a8c08aa0bf76f8
ayato0913a <ayato0913a@gmail.com>
2026-03-16 08:33:46 +0900

    [ feat ] 評価条件を表すインターフェイスを作成

--- Assets/DevelopProducts/Design/GameMode/Scripts/1.Domain/IEvaluationCondition.cs
+++ Assets/DevelopProducts/Design/GameMode/Scripts/1.Domain/IEvaluationCondition.cs
@@ -1,6 +1,13 @@
-using UnityEngine;
-
-public class IEvaluationCondition
+namespace DevelopProducts.Design.GameMode.Domain
 {
-    
+    /// <summary>
+    ///     評価条件を表すインターフェース。
+    /// </summary>
+    public interface IEvaluationCondition
+    {
+        /// <summary> 条件を満たしているかどうかを評価するメソッド。 </summary>
+        bool IsSatisfied(StageRuntimeContext context);
+        /// <summary> 条件の説明を取得するメソッド。 </summary>
+        string GetDescription();
+    }
 }
