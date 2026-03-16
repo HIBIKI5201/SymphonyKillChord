83019c3ce812884d19b839c6676a48b82bd683d5
ayato0913a <ayato0913a@gmail.com>
2026-03-16 19:30:40 +0900

    [ feat ] プレイヤーの当たり判定のviewクラスを作成

--- Assets/DevelopProducts/Design/GameMode/Scripts/5.View/PlayerCollisionView.cs
+++ Assets/DevelopProducts/Design/GameMode/Scripts/5.View/PlayerCollisionView.cs
@@ -1,16 +1,29 @@
+using DevelopProducts.Design.GameMode.Adaptor;
 using UnityEngine;
 
-public class PlayerCollisionView : MonoBehaviour
+namespace DevelopProducts.Design.GameMode.View
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    /// <summary>
+    ///     プレイヤーの当たり判定のビュークラス。
+    ///     プレイヤーが敵と衝突した際の処理を担当する。
+    /// </summary>
+    public class PlayerCollisionView : MonoBehaviour
     {
-        
-    }
+        public void Initialize(PlayerColisionController controller)
+        {
+            _controller = controller;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void OnTriggerEnter(Collider other)
+        {
+            EnemyView enemyView = other.GetComponent<EnemyView>();
+            if (enemyView != null)
+            {
+                // プレイヤーと敵が衝突した場合の処理
+                _controller.HitEnemy(enemyView.RuntimeState, enemyView.EnemyDefinition);
+            }
+        }
+
+        private PlayerColisionController _controller;
     }
 }
