6e132bff841433447e93e3db49e1427a69d716f0
Oikawa <yucchi0424@gmail.com>
2026-03-16 21:54:53 +0900

    [update] PerspectiveRemovalの歪み修正調整

--- Assets/DevelopProducts/Research/ToonShader/Scripts/Runtime/Shaders/HLSL/TestPerspectiveRemoval.hlsl
+++ Assets/DevelopProducts/Research/ToonShader/Scripts/Runtime/Shaders/HLSL/TestPerspectiveRemoval.hlsl
@@ -1,14 +1,17 @@
 #ifndef CUSTOM_PERSPECTIVE_REMOVAL_INCLUDED
+
 #define CUSTOM_PERSPECTIVE_REMOVAL_INCLUDED
 
 void PerspectiveRemoval_float(
     float3 positionOS,
     float3 objectCenterOS,
-    out float3 positionOS_out)
+    out float3 positionOS_out
+)
 {
     float4 posVS = mul(UNITY_MATRIX_MV, float4(positionOS, 1.0));
     float4 centerVS = mul(UNITY_MATRIX_MV, float4(objectCenterOS, 1.0));
 
+    
     float scale = posVS.z / min(centerVS.z, -0.001);
     posVS.xy *= scale;
     
@@ -17,4 +20,5 @@ void PerspectiveRemoval_float(
     float4 posOS = mul(UNITY_MATRIX_I_M, worldPos);
     positionOS_out = posOS.xyz;
 }
+
 #endif
