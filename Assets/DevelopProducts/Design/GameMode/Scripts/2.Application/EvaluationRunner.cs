a7911d626726811a6d6c8abd1c15f306f18ed45a
ayato0913a <ayato0913a@gmail.com>
2026-03-16 12:19:39 +0900

    [ feat ] 達成率の判断クラス追加

--- Assets/DevelopProducts/Design/GameMode/Scripts/2.Application/EvaluationRunner.cs
+++ Assets/DevelopProducts/Design/GameMode/Scripts/2.Application/EvaluationRunner.cs
@@ -1,6 +1,48 @@
+using DevelopProducts.Design.GameMode.Domain;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class EvaluationRunner
+namespace DevelopProducts.Design.GameMode.Application
 {
-    
+    public class EvaluationResult
+    {
+        public EvaluationResult(IReadOnlyList<string> achivedDescriptions)
+        {
+            AchivedDescriptions = achivedDescriptions;
+        }
+
+
+        public IReadOnlyList<string> AchivedDescriptions { get; }
+        public int AchivedCount => AchivedDescriptions.Count;
+    }
+
+    /// <summary>
+    ///     ゲーム終了時の評価条件の達成状況を判定するクラス。
+    /// </summary>
+    public class EvaluationRunner
+    {
+        public EvaluationRunner(StageRuntimeContext runtimeContext, IReadOnlyList<IEvaluationCondition> evaluationConditions)
+        {
+            _runtimeContext = runtimeContext;
+            _evaluationConditions = evaluationConditions;
+        }
+
+        public EvaluationResult Run()
+        {
+            List<string> achived= new();
+
+            foreach (var condition in _evaluationConditions)
+            {
+                if (condition.IsSatisfied(_runtimeContext))
+                {
+                    achived.Add(condition.GetDescription());
+                }
+            }
+
+            return new EvaluationResult(achived);
+        }
+
+        private readonly StageRuntimeContext _runtimeContext;
+        private readonly IReadOnlyList<IEvaluationCondition> _evaluationConditions;
+    }
 }
