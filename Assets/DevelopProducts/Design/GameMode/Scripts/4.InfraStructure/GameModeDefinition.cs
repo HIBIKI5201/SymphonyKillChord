55d95cac06c54710c5d63e528df3e909750f297f
ayato0913a <ayato0913a@gmail.com>
2026-03-16 11:20:42 +0900

    [ fix ] namespaceとか

--- Assets/DevelopProducts/Design/GameMode/Scripts/4.InfraStructure/GameModeDefinition.cs
+++ Assets/DevelopProducts/Design/GameMode/Scripts/4.InfraStructure/GameModeDefinition.cs
@@ -3,19 +3,25 @@ using SymphonyFrameWork.Attribute;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "GameModeDefinition", menuName = "Scriptable Objects/GameModeDefinition")]
-public class GameModeDefinition : ScriptableObject
+namespace DevelopProducts.Design.GameMode.InfraStructure
 {
-    public IClearCondition ClearConditions => _clearConditions;
-    public IFailCondition FailConditions => _failConditions;
-    public List<IEvaluationCondition> EvaluationConditions => _evaluationConditions;
+    /// <summary>
+    ///     ステージのクリア条件、失敗条件、評価条件を定義するクラス。
+    /// </summary>
+    [CreateAssetMenu(fileName = "GameModeDefinition", menuName = "Scriptable Objects/GameModeDefinition")]
+    public class GameModeDefinition : ScriptableObject
+    {
+        public IClearCondition ClearConditions => _clearConditions;
+        public IFailCondition FailConditions => _failConditions;
+        public List<IEvaluationCondition> EvaluationConditions => _evaluationConditions;
 
-    [Header("クリア条件"),SerializeReference,SubclassSelector]
-    private IClearCondition _clearConditions;
+        [Header("クリア条件"), SerializeReference, SubclassSelector]
+        private IClearCondition _clearConditions;
 
-    [Header("失敗条件"), SerializeReference, SubclassSelector]
-    private IFailCondition _failConditions;
+        [Header("失敗条件"), SerializeReference, SubclassSelector]
+        private IFailCondition _failConditions;
 
-    [Header("評価条件"), SerializeReference, SubclassSelector]
-    private List<IEvaluationCondition> _evaluationConditions;
+        [Header("評価条件"), SerializeReference, SubclassSelector]
+        private List<IEvaluationCondition> _evaluationConditions;
+    }
 }
