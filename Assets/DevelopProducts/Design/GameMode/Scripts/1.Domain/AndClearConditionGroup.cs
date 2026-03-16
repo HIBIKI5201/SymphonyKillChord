8612a4865e8b075de4d03b3f0b8f7bde33dfe91c
ayato0913a <ayato0913a@gmail.com>
2026-03-16 10:21:43 +0900

    [ feat ] Andクリア条件の組み合わせ用のクラスを作成

--- Assets/DevelopProducts/Design/GameMode/Scripts/1.Domain/AndClearConditionGroup.cs
+++ Assets/DevelopProducts/Design/GameMode/Scripts/1.Domain/AndClearConditionGroup.cs
@@ -1,6 +1,38 @@
+using SymphonyFrameWork.Attribute;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class AndClearConditionGroup
+namespace DevelopProducts.Design.GameMode.Domain
 {
-    
+    /// <summary>
+    ///     複数のクリア条件を組み合わせるためのクラス。
+    ///     例えば、特定の敵を一定数撃破することと、ステージの経過時間が一定時間以内であることなど、複数の条件をAND条件で組み合わせて管理する。
+    /// </summary>
+    [Serializable]
+    public class AndClearConditionGroup : IClearCondition
+    {
+        public bool IsSatisfied(StageRuntimeContext context)
+        {
+            for (int i = 0; i < _children.Count; i++)
+            {
+                if(!_children[i].IsSatisfied(context))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+
+        [SerializeReference, SubclassSelector, Tooltip("組み合わせるクリア条件のリスト。")]
+        private List<IClearCondition> _children = new();
+
+        public string GetDescription()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
 }
