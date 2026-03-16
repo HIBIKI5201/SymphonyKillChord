21dfacc544daf85c258a6198fc3e4190d7a3dbdc
ayato0913a <ayato0913a@gmail.com>
2026-03-16 10:58:04 +0900

    [ feat ] プレイヤーの死亡に関する失敗条件追加

--- Assets/DevelopProducts/Design/GameMode/Scripts/1.Domain/PlayerDeadFailCondition.cs
+++ Assets/DevelopProducts/Design/GameMode/Scripts/1.Domain/PlayerDeadFailCondition.cs
@@ -1,6 +1,22 @@
+using DevelopProducts.Design.GameMode.Domain;
 using UnityEngine;
 
-public class PlayerDeadFailCondition
+namespace DevelopProducts.Design.GameMode.Domain
 {
-    
+    /// <summary>
+    ///     プレイヤーの死亡に関する失敗条件。
+    ///     例えば、プレイヤーが死亡した場合にステージが失敗となる条件を管理するためのクラス。
+    /// </summary>
+    public class PlayerDeadFailCondition : IFailCondition
+    {
+        public bool IsSatisfied(StageRuntimeContext context)
+        {
+            return context.PlayerRuntimeState.IsDead;
+        }
+
+        public string GetDescription()
+        {
+            return "プレイヤーが死亡する";
+        }
+    }
 }
