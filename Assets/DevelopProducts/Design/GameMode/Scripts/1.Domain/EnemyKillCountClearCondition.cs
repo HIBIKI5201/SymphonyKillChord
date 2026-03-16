e17d2a91d779ad60e4a4b29414437eea3ba8c0b4
ayato0913a <ayato0913a@gmail.com>
2026-03-16 10:45:23 +0900

    [ feat ] 撃破数条件を作成

--- Assets/DevelopProducts/Design/GameMode/Scripts/1.Domain/EnemyKillCountClearCondition.cs
+++ Assets/DevelopProducts/Design/GameMode/Scripts/1.Domain/EnemyKillCountClearCondition.cs
@@ -1,6 +1,28 @@
+using DevelopProducts.Design.GameMode.InfraStructure;
+using System;
 using UnityEngine;
 
-public class EnemyKillCountClearCondition
+namespace DevelopProducts.Design.GameMode.Domain
 {
-    
+    /// <summary>
+    ///     敵の撃破数に関するクリア条件。
+    ///     例えば、特定の敵を一定数撃破することなど、敵の撃破数に関連する条件を管理するためのクラス。
+    /// </summary>
+    [Serializable]
+    public class EnemyKillCountClearCondition : IClearCondition
+    {
+        public bool IsSatisfied(StageRuntimeContext context)
+        {
+            return context.EnemyKillStatics.GetKillCount(_enemyDefinition) >= _requiredCount;
+        }
+
+        public string GetDescription()
+        {
+            string enemyName = _enemyDefinition != null ? _enemyDefinition.DisplayName : "Unknown Enemy";
+            return$"{enemyName}を{_requiredCount}体以上撃破する";
+        }
+
+        [SerializeField] private EnemyDefinition _enemyDefinition;
+        [SerializeField] private int _requiredCount;
+    }
 }
