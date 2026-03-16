415fee7cbfb41aa40d7f7ecff441e536339c5a28
ayato0913a <ayato0913a@gmail.com>
2026-03-16 10:21:24 +0900

    [ feat ] Andクリア条件の組み合わせ用のクラスを作成

--- Assets/DevelopProducts/Design/GameMode/Scripts/1.Domain/AndFailConditionGroup.cs
+++ Assets/DevelopProducts/Design/GameMode/Scripts/1.Domain/AndFailConditionGroup.cs
@@ -1,6 +1,35 @@
+using SymphonyFrameWork.Attribute;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class AndFailConditionGroup
+namespace DevelopProducts.Design.GameMode.Domain
 {
-    
+    /// <summary>
+    ///     複数の失敗条件を組み合わせるためのクラス。
+    /// </summary>
+    public class AndFailConditionGroup : IFailCondition
+    {
+        public bool IsSatisfied(StageRuntimeContext context)
+        {
+            for (int i = 0; i < _children.Count; i++)
+            {
+                if (!_children[i].IsSatisfied(context))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+
+        [SerializeReference, SubclassSelector, Tooltip("組み合わせるクリア条件のリスト。")]
+        private List<IFailCondition> _children = new();
+
+        public string GetDescription()
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
