8b90c5538a1a40513a43cd18ba8df535f419b225
ayato0913a <ayato0913a@gmail.com>
2026-03-16 10:48:21 +0900

    [ feat ] 何秒生存みたいな条件クラス、失敗のやつと被ってるが何したうえでこの生存など汎用的に使える

--- Assets/DevelopProducts/Design/GameMode/Scripts/1.Domain/ElapsedTimeClearCondition.cs
+++ Assets/DevelopProducts/Design/GameMode/Scripts/1.Domain/ElapsedTimeClearCondition.cs
@@ -1,6 +1,23 @@
 using UnityEngine;
 
-public class ElapsedTimeClearCondition
+namespace DevelopProducts.Design.GameMode.Domain
 {
-    
+    /// <summary>
+    ///     ステージの経過時間に関するクリア条件。
+    ///     例えば、何秒間生存することなど、ステージの経過時間に関連する条件を管理するためのクラス。
+    /// </summary>
+    public class ElapsedTimeClearCondition : IClearCondition
+    {
+        public bool IsSatisfied(StageRuntimeContext context)
+        {
+            return context.StageTimeState.ElapsedTime >= _requiredTime;
+        }
+
+        public string GetDescription()
+        {
+            return $"{_requiredTime}秒生存する";
+        }
+
+        [SerializeField] private float _requiredTime;
+    }
 }
