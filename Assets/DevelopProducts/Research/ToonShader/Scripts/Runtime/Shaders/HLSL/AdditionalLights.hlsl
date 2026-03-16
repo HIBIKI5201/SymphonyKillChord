21326839f28de526d7da0f4197c23e8e912bd7c4
Oikawa <yucchi0424@gmail.com>
2026-03-16 17:57:21 +0900

    [update] AdditionalLightの影受け取るよう実装。Face用陰影計算変更。

--- Assets/DevelopProducts/Research/ToonShader/Scripts/Runtime/Shaders/HLSL/AdditionalLights.hlsl
+++ Assets/DevelopProducts/Research/ToonShader/Scripts/Runtime/Shaders/HLSL/AdditionalLights.hlsl
@@ -1,7 +1,16 @@
 #ifndef ADDITIONAL_LIGHTS_INCLUDED
 #define ADDITIONAL_LIGHTS_INCLUDED
 
+
+#ifndef SHADERGRAPH_PREVIEW
 #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
+
+
+#pragma multi_compile _ _MAIN_LIGHT_SHADOWS
+#pragma multi_compile _ _MAIN_LIGHT_SHADOWS_CASCADE
+#pragma multi_compile _ _SHADOWS_SOFT
+
+#endif
 
 void GetAdditionalLights_float(
     float3 WorldPos,
@@ -10,8 +19,7 @@ void GetAdditionalLights_float(
     out float3 DiffuseLight)
 {
     DiffuseLight = float3(0, 0, 0);
-
-#ifndef SHADERGRAPH_PREVIEW
+    
     InputData inputData = (InputData) 0;
     inputData.positionWS = WorldPos;
     inputData.normalWS = normalize(WorldNormal);
@@ -22,13 +30,36 @@ void GetAdditionalLights_float(
     int lightCount = GetAdditionalLightsCount();
     for (int i = 0; i < lightCount; i++)
     {
-        Light light = GetAdditionalLight(i, WorldPos, inputData.shadowMask);
+        Light light = GetAdditionalLight(i, WorldPos, 1);
 
-        float atten = light.distanceAttenuation * light.shadowAttenuation;
+        float atten = light.shadowAttenuation;
         float NdotL = saturate(dot(WorldNormal, light.direction));
         DiffuseLight += light.color * atten * NdotL;
     }
+}
+
+void GetReceiveShadow_float(float ShadowAlpha, float3 PositionWS, out half ShadowAttenuation)
+{
+#ifdef SHADERGRAPH_PREVIEW
+    ShadowAttenuation = 1.0;
+    
+#else
+    half4 shadowCoord = TransformWorldToShadowCoord(PositionWS);
+    Light mainLight = GetMainLight(shadowCoord);
+    half shadow = mainLight.shadowAttenuation;
+    int pixelLightCount = GetAdditionalLightsCount();
+
+    for (int i = 0; i < pixelLightCount; i++)
+    {
+        Light AddLight0 = GetAdditionalLight(i, PositionWS, 1);
+        half shadow0 = AddLight0.shadowAttenuation;
+        shadow *= shadow0;
+    }
+
+    ShadowAttenuation = shadow * ShadowAlpha;
+
 #endif
 }
+
 
 #endif
