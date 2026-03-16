6114f7d3619f4ee391851921fc14aa3ef095d6a6
ayato0913a <ayato0913a@gmail.com>
2026-03-16 10:20:26 +0900

    [ feat ] or失敗条件のクラスを作成

--- Assets/DevelopProducts/Design/GameMode/Scripts/1.Domain/OrFailConditionGroup.cs
+++ Assets/DevelopProducts/Design/GameMode/Scripts/1.Domain/OrFailConditionGroup.cs
@@ -1,6 +1,36 @@
+using DevelopProducts.Design.GameMode.Domain;
+using SymphonyFrameWork.Attribute;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class OrFailConditionGroup
+namespace DevelopProducts.Design.GameMode.Domain
 {
-    
+    /// <summary>
+    ///     リスト内の条件の一つでも失敗した場合、失敗とする条件を組み合わせるためのクラス。
+    /// </summary>
+    public class OrFailConditionGroup : IFailCondition
+    {
+        public bool IsSatisfied(StageRuntimeContext context)
+        {
+            for (int i = 0; i < _children.Count; i++)
+            {
+                if (_children[i].IsSatisfied(context))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
+        [SerializeReference, SubclassSelector, Tooltip("組み合わせるクリア条件のリスト。")]
+        private List<IFailCondition> _children = new();
+
+        public string GetDescription()
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
