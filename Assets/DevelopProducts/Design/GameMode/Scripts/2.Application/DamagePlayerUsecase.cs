86ee9e49d9cecabb94af66ef8fa1264f9a840d5a
ayato0913a <ayato0913a@gmail.com>
2026-03-16 18:11:50 +0900

    [ feat ] プレイヤーにダメージを与えるUsecaseを作成

--- Assets/DevelopProducts/Design/GameMode/Scripts/2.Application/DamagePlayerUsecase.cs
+++ Assets/DevelopProducts/Design/GameMode/Scripts/2.Application/DamagePlayerUsecase.cs
@@ -1,6 +1,23 @@
+using DevelopProducts.Design.GameMode.Domain;
 using UnityEngine;
 
-public class DamagePlayerUsecase
+namespace DevelopProducts.Design.GameMode.Application
 {
-    
+    /// <summary>
+    ///     プライヤーにダメージを与えるユースケース。
+    /// </summary>
+    public class DamagePlayerUsecase
+    {
+        public DamagePlayerUsecase(StageRuntimeContext runtimeContext)
+        {
+            _runtimeContext = runtimeContext;
+        }
+
+        public void Execute(int damage)
+        {
+            _runtimeContext.PlayerRuntimeState.TakeDamage(damage);
+        }
+
+        private readonly StageRuntimeContext _runtimeContext;
+    }
 }
