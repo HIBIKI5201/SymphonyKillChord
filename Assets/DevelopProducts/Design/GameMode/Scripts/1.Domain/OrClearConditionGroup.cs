e76d814f64c9b3d24f69d42ffbc030f370103dda
ayato0913a <ayato0913a@gmail.com>
2026-03-16 10:20:37 +0900

    [ feat ] orクリア条件のクラスを作成

--- Assets/DevelopProducts/Design/GameMode/Scripts/1.Domain/OrClearConditionGroup.cs
+++ Assets/DevelopProducts/Design/GameMode/Scripts/1.Domain/OrClearConditionGroup.cs
@@ -1,6 +1,37 @@
+using SymphonyFrameWork.Attribute;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class OrClearConditionGroup
+namespace DevelopProducts.Design.GameMode.Domain
 {
-    
+    /// <summary>
+    ///     リスト内の条件の一つでもクリアした場合、達成とする条件を組み合わせるためのクラス。
+    /// </summary>
+    [Serializable]
+    public class OrClearConditionGroup : IClearCondition
+    {
+        public bool IsSatisfied(StageRuntimeContext context)
+        {
+            for (int i = 0; i < _children.Count; i++)
+            {
+                if (_children[i].IsSatisfied(context))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
+        [SerializeReference, SubclassSelector, Tooltip("組み合わせるクリア条件のリスト。")]
+        private List<IClearCondition> _children = new();
+
+        public string GetDescription()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
 }
