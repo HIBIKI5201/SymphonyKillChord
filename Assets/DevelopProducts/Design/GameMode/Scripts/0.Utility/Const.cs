c2f9faade0668f06a16bd9e67d1002bd14e45e69
ayato0913a <ayato0913a@gmail.com>
2026-03-17 17:19:11 +0900

    [ add ] 定数管理クラス追加

---
+++ Assets/DevelopProducts/Design/GameMode/Scripts/0.Utility/Const.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace DevelopProducts.Design.GameMode.Utility
+{
+    /// <summary>
+    ///     GameMode関連の定数を管理するクラス。
+    /// </summary>
+    public class Const
+    {
+        /// <summary> アセット作成時のメニューパス。 </summary>
+        public const string CREATE_ASSET_PATH = "DevelopProducts/GameMode/";
+    }
+}
