b87c913df057a403308ebbb3d7cefcd5464a8380
ayato0913a <ayato0913a@gmail.com>
2026-03-17 17:23:20 +0900

    [ fix ] staticに

--- Assets/DevelopProducts/Design/GameMode/Scripts/0.Utility/Const.cs
+++ Assets/DevelopProducts/Design/GameMode/Scripts/0.Utility/Const.cs
@@ -5,7 +5,7 @@ namespace DevelopProducts.Design.GameMode.Utility
     /// <summary>
     ///     GameMode関連の定数を管理するクラス。
     /// </summary>
-    public class Const
+    public static class Const
     {
         /// <summary> アセット作成時のメニューパス。 </summary>
         public const string CREATE_ASSET_PATH = "DevelopProducts/GameMode/";
