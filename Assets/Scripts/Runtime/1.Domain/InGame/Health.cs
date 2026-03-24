e9f269806dba2ce59148fde26dff1ddf7d0241d5
reitaghchi <ren.len2426@gmail.com>
2026-03-24 19:23:21 +0900

    [update]明示的キャスト対応、データ変更をメソッド化

--- Assets/Scripts/Runtime/1.Domain/InGame/Health.cs
+++ Assets/Scripts/Runtime/1.Domain/InGame/Health.cs
@@ -1,14 +1,14 @@
-using UnityEngine;
-
 namespace KillChord.Runtime.Domain
 {
     public readonly struct Health
-	{
-    	public Health(float value) 
-		{
-			   Value = value;
-    	}
-   
+    {
+        public Health(float value)
+        {
+            Value = value;
+        }
+
+        public static explicit operator float(Health health) => health.Value;
+
         public readonly float Value;
     }
 }
--- Assets/Scripts/Runtime/1.Domain/InGame/HealthEntity.cs
+++ Assets/Scripts/Runtime/1.Domain/InGame/HealthEntity.cs
@@ -1,16 +1,20 @@
-using UnityEngine;
-
 namespace KillChord.Runtime.Domain
 {
     public class HealthEntity
     {
         public HealthEntity(float health)
         {
-            CurrentHealth = new (health);
-            MaxHealth = new (health);
+            CurrentHealth = new(health);
+            MaxHealth = new(health);
         }
-		
-        public Health CurrentHealth;
+
+        public Health CurrentHealth { get; private set; }
         public readonly Health MaxHealth;
+
+        public void ChangeHealth(Health value)
+        {
+            if ((float)value > (float)MaxHealth) { value = MaxHealth; }
+            CurrentHealth = value;
+        }
     }
 }
