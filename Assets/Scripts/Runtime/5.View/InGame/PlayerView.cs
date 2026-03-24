3fbe84cd015e34774caa65ca6bfd0a340b0fdd8d
Oikawa <yucchi0424@gmail.com>
2026-03-25 00:16:32 +0900

    [update] Playerにカメラの向きに合わせて操作が合うように実装。

--- Assets/Scripts/Runtime/5.View/InGame/PlayerView.cs
+++ Assets/Scripts/Runtime/5.View/InGame/PlayerView.cs
@@ -7,6 +7,7 @@ namespace KillChord.Runtime.View
     {
         [SerializeField] private string _blendName;
         [SerializeField] private Animator _animator;
+        [SerializeField] private Transform _cameraTransform;
 
         public void Init(PlayerController playerMovementController)
         {
@@ -38,6 +39,8 @@ namespace KillChord.Runtime.View
             if (dir == Vector2.zero)
                 return;
 
+            dir = Rotate(dir, -_cameraTransform.eulerAngles.y);
+
             _cacheTransform.position = _controller.GetMovedPosition(_cacheTransform.position, dir, Time.deltaTime);
             _cacheTransform.rotation = Quaternion.LookRotation(new Vector3(dir.x, 0, dir.y), Vector3.up);
         }
@@ -52,8 +55,18 @@ namespace KillChord.Runtime.View
             if (dir == Vector2.zero)
                 return;
 
+            dir = Rotate(dir, -_cameraTransform.eulerAngles.y);
+
             _cacheTransform.position = _controller.GetDodgedPosition(_cacheTransform.position, dir, Time.time);
             _cacheTransform.rotation = Quaternion.LookRotation(new Vector3(dir.x, 0, dir.y), Vector3.up);
+        }
+
+        private static Vector2 Rotate(in Vector2 v, float degrees)
+        {
+            float sin = Mathf.Sin(degrees * Mathf.Deg2Rad);
+            float cos = Mathf.Cos(degrees * Mathf.Deg2Rad);
+
+            return new Vector2(cos * v.x - sin * v.y, sin * v.x + cos * v.y);
         }
 
         private Transform _cacheTransform;
