7443da9409287f19632a99ea507aed1b94a98271
reitaghchi <ren.len2426@gmail.com>
2026-03-24 23:22:22 +0900

    [add]HUDのViewクラス作成

--- Assets/Scripts/Runtime/5.View/InGame/IngameHudView.cs
+++ Assets/Scripts/Runtime/5.View/InGame/IngameHudView.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace KillChord.Runtime.View
 {
     public class IngameHudView : MonoBehaviour
     {
-        
+        [SerializeField] private Image _hpBarImage;
+
+        private void ChangeHitPoint()
+        {
+            
+        }
     }
 }
