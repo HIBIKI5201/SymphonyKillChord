2bad27fa50a2144dacafc93e9a1b1da4bb31b2ab
Oikawa <yucchi0424@gmail.com>
2026-03-24 10:10:12 +0900

    [add] NormalMap合成HLSL追加

---
+++ Assets/DevelopProducts/Research/ToonShader/Scripts/Runtime/Shaders/HLSL/Fragment/NormalCombine.hlsl
@@ -0,0 +1,23 @@
+#ifndef NORMAL_COMBINE_INCLUDED
+#define NORMAL_COMBINE_INCLUDED
+
+#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+
+float3 GetNormalCombine(
+    TEXTURE2D_PARAM( normalMap, samplerNormalMap),
+    float2 uv,
+    float3 normalWS,
+    float3 tangentWS,
+    float3 bitangentWS,
+    float strength)
+{
+    float4 normalSample = SAMPLE_TEXTURE2D(normalMap, samplerNormalMap, uv);
+    float3 normalTS = UnpackNormal(normalSample);
+
+    normalTS.xy *= strength;
+    normalTS = normalize(normalTS);
+
+    float3x3 TBN = float3x3(tangentWS, bitangentWS, normalWS);
+    return normalize(mul(normalTS, TBN));
+}
+#endif
