c54e4593db8ab96bd86d44e02a262d623f5898c6
reitaghchi <ren.len2426@gmail.com>
2026-03-25 12:15:44 +0900

    [add]Bindメソッド追加

--- Assets/Scripts/Runtime/5.View/InGame/IngameHudView.cs
+++ Assets/Scripts/Runtime/5.View/InGame/IngameHudView.cs
@@ -7,10 +7,16 @@ namespace KillChord.Runtime.View
     {
         [SerializeField] private Image _halthBarImage;
 
+        private IngameHudViewModel _viewModel;
+
+        public void Bind(IngameHudViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+
         private void ChangeHitPoint(float fillAmount)
         {
             _halthBarImage.fillAmount = Mathf.Clamp(fillAmount, 0f, 1f);
         }
     }
 }
-
