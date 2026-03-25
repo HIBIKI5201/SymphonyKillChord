f5e533a9f30288e806042db262615bef4b4eabcd
reitaghchi <ren.len2426@gmail.com>
2026-03-25 19:43:58 +0900

    [add]PlayBGM実装

--- Assets/Scripts/Runtime/5.View/InGame/MusicSyncView.cs
+++ Assets/Scripts/Runtime/5.View/InGame/MusicSyncView.cs
@@ -1,9 +1,28 @@
+using CriWare;
 using UnityEngine;
 
 namespace KillChord.Runtime.View
 {
     public class MusicSyncView : MonoBehaviour
     {
-        
+        private CriAtomSource _cri;
+
+        private CriAtomExPlayback _playback;
+
+        public void Awake()
+        {
+            _cri = GetComponent<CriAtomSource>();
+        }
+
+        private void PlayBgm(string cueName)
+        {
+            _playback.Stop();
+            _cri.cueName = cueName;
+            _playback = _cri.Play();
+        }
+
+        private void Update()
+        {
+        }
     }
 }
