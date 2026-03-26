7b9a25fd25244d3e485b4e6f0323754a5f8a3e92
Oikawa <yucchi0424@gmail.com>
2026-03-26 23:50:44 +0900

    [update] CameraFollow更新

--- Assets/Scripts/Runtime/2.Application/InGame/Camera/CameraFollow.cs
+++ Assets/Scripts/Runtime/2.Application/InGame/Camera/CameraFollow.cs
@@ -12,13 +12,13 @@ namespace KillChord.Runtime.Application
 
         public void Update(
             ref Vector3 cameraCenterPosition,
-            in Vector3 followPostion,
+            in Vector3 followPosition,
             in Vector3 cameraRight,
             bool isLockOn,
             float deltaTime
             )
         {
-            Vector3 targetFollowCenterOffset = -_followVelocity.UpdateFollowVelocity(followPostion, deltaTime);
+            Vector3 targetFollowCenterOffset = -_followVelocity.UpdateFollowVelocity(followPosition, deltaTime);
             targetFollowCenterOffset.y = 0;
             if (targetFollowCenterOffset.sqrMagnitude >= _parameter.FollowOffsetPower * _parameter.FollowOffsetPower)
             {
