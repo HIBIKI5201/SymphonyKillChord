2c166040f629fc08cb4d97948b7064005dbcb0fe
ayato0913a <ayato0913a@gmail.com>
2026-03-27 10:30:11 +0900

    [ fix ] 追加修正

--- Assets/Scripts/Runtime/4.InfraStructure/InGame/CharacterData.cs
+++ Assets/Scripts/Runtime/4.InfraStructure/InGame/CharacterData.cs
@@ -6,14 +6,15 @@ namespace KillChord.Runtime.InfraStructure
     ///     キャラクターの基本的なデータを保持するScriptableObject。
     /// </summary>
     [CreateAssetMenu(fileName = nameof(CharacterData),
-    menuName = "KillChord/Character" + nameof(CharacterData))]
+    menuName = "KillChord/Character?CharacterData")]
     public class CharacterData : ScriptableObject
     {
         public string CharacterName => _characterName;
         public float MaxHealth => _maxHealth;
         public float MoveSpeed => _moveSpeed;
         public float AttackPower => _attackPower;
-        public AttackDefinitionData[] AttackDifinitions => (AttackDefinitionData[])_attackDifinitions.Clone();
+        public AttackDefinitionData[] AttackDifinitions =>
+            _attackDifinitions == null ? null : (AttackDefinitionData[])_attackDifinitions.Clone();
 
         [SerializeField] private string _characterName;
         [SerializeField] private float _maxHealth;
