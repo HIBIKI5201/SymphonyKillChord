5617cee42a9052114395df0ba787c7f55bd5a6d2
Oikawa <yucchi0424@gmail.com>
2026-03-27 17:51:36 +0900

    [update] 一旦TestPlayerにアタックパイプライン導入

--- Assets/Scripts/Runtime/6.Composition/InGame/Player/PlayerInitializer.cs
+++ Assets/Scripts/Runtime/6.Composition/InGame/Player/PlayerInitializer.cs
@@ -15,17 +15,51 @@ namespace KillChord.Runtime.Composition
     {
         [SerializeField] private PlayerConfig _playerConfig;
         [SerializeField] private PlayerView _player;
+
+        [Space]
+
+        [Header("キャラクターデータ（テスト用）")]
+        [SerializeField] private CharacterData _playerData;
+        [SerializeField] private CharacterData _enemyData;
+
+        [Header("アタックパイプライン（テスト用）")]
+        [SerializeField] private AttackPipelineAsset _normalPipelineAsset;
+        [SerializeField] private AttackPipelineAsset _skillAPipelineAsset;
+        [SerializeField] private AttackPipelineAsset _skillBPipelineAsset;
+        [SerializeField] private AttackPipelineAsset _ultimatePipelineAsset;
         private void Awake()
         {
             if (_player == null)
                 Debug.LogError($"{nameof(PlayerView)}がNullです", this);
 
+
+
+            CharacterFactory characterFactory = new CharacterFactory();
+
+            CharacterEntity player = characterFactory.Create(_playerData);
+            CharacterEntity enemy = characterFactory.Create(_enemyData);
+
+            Dictionary<AttackId, AttackPipeline> pipelines = new Dictionary<AttackId, AttackPipeline>
+            {
+                { AttackId.Normal, _normalPipelineAsset.Create() },
+                { AttackId.SkillA, _skillAPipelineAsset.Create() },
+                { AttackId.SkillB, _skillBPipelineAsset.Create() },
+                { AttackId.Ultimate, _ultimatePipelineAsset.Create() },
+            };
+
+
+
+
             PlayerMoveParameter parameter = _playerConfig.ToDomain();
-            AttackPipelineResolver attackPipelineResolver = new(new Dictionary<AttackId, AttackPipeline>());
+            AttackPipelineResolver attackPipelineResolver = new(pipelines);
             AttackExecutor attackExecutor = new(attackPipelineResolver);
 
             AttackCommandState attackCommandState = new();
             AttackBattleState attackBattleState = new();
+            attackBattleState.Setup(player, enemy);
+
+
+
 
 
             PlayerDodgeMovementApplication dodge = new(parameter);
