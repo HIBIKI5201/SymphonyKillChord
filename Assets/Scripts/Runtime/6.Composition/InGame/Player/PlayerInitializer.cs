fbf2381beaef8ffaa1d469d8f569554fbf7c526d
Oikawa <yucchi0424@gmail.com>
2026-03-27 17:36:13 +0900

    [update] Controller,View,InitializerにAttackパイプラインを実装

--- Assets/Scripts/Runtime/6.Composition/InGame/Player/PlayerInitializer.cs
+++ Assets/Scripts/Runtime/6.Composition/InGame/Player/PlayerInitializer.cs
@@ -1,9 +1,11 @@
 using KillChord.Runtime.Adaptor;
 using KillChord.Runtime.Application;
 using KillChord.Runtime.Domain;
+using KillChord.Runtime.InfraStructure;
 using KillChord.Runtime.Utility;
 using KillChord.Runtime.View;
 using KillChord.Structure;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace KillChord.Runtime.Composition
@@ -19,12 +21,18 @@ namespace KillChord.Runtime.Composition
                 Debug.LogError($"{nameof(PlayerView)}がNullです", this);
 
             PlayerMoveParameter parameter = _playerConfig.ToDomain();
+            AttackPipelineResolver attackPipelineResolver = new(new Dictionary<AttackId, AttackPipeline>());
+            AttackExecutor attackExecutor = new(attackPipelineResolver);
+
+            AttackCommandState attackCommandState = new();
+            AttackBattleState attackBattleState = new();
+
 
             PlayerDodgeMovementApplication dodge = new(parameter);
             PlayerMovement move = new(parameter);
             PlayerApplication application = new(move, dodge);
 
-            PlayerController playerMovementController = new(application);
+            PlayerController playerMovementController = new(application, attackExecutor, attackCommandState, attackBattleState);
             _player.Init(playerMovementController);
 
 
