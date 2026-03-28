9f3bfa5bf9a36bbf6434d702eee4e24fac2e6606
ayato0913a <ayato0913a@gmail.com>
2026-03-28 12:50:56 +0900

    [ add ] タイポ修正

---
+++ Assets/Scripts/Develop/EnemyTestSpawner.cs
@@ -0,0 +1,44 @@
+using KillChord.Runtime.View;
+using UnityEngine;
+
+namespace KillChord.Develop
+{
+    /// <summary>
+    ///     テスト用のスポナー。
+    /// </summary>
+    public class EnemyTestSpawner : MonoBehaviour
+    {
+        [SerializeField] private EnemyMoveDebugInitializer _enemyPrefab;
+        [SerializeField] private Transform _target;
+        [SerializeField] private Transform _spawnPoint;
+        [SerializeField] private float _spawnInterval;
+        [SerializeField] private int _maxSpawnCount;
+
+        private float _timer;
+        private int _spawnCount;
+
+        private void Update()
+        {
+            if (_enemyPrefab == null || _target == null || _spawnPoint == null) return;
+            if (_spawnCount >= _maxSpawnCount) return;
+
+            _timer += Time.deltaTime;
+
+            if (_timer >= _spawnInterval)
+            {
+                _timer = 0f;
+                SpawnEnemy();
+            }
+        }
+
+        private void SpawnEnemy()
+        {
+            EnemyMoveDebugInitializer enemyInstance =
+                Instantiate(_enemyPrefab, _spawnPoint.position, _spawnPoint.rotation);
+
+            enemyInstance.Initialize(_target);
+
+            _spawnCount++;
+        }
+    }
+}
