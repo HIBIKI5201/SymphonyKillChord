489b4bdf1d59c5e563e2608ed951c4328cc4ef57
ayato0913a <ayato0913a@gmail.com>
2026-03-29 21:37:47 +0900

    [ add ] 敵の移動や動きを操るクラスを作成

---
+++ Assets/Scripts/Runtime/3.Adaptor/InGame/EnemyAIController.cs
@@ -0,0 +1,92 @@
+using KillChord.Runtime.Application;
+using KillChord.Runtime.Domain;
+using System;
+using UnityEngine;
+using static Codice.CM.Common.Purge.PurgeReport;
+
+namespace KillChord.Runtime.Adaptor
+{
+    public class EnemyAIController : IDisposable
+    {
+        public EnemyAIController(
+            EnemyMoveUsecase enemyMoveUsecase,
+            EnemyAttackReservationUsecase enemyAttackReservationUsecase,
+            EnemyAttackUsecase enemyAttackUsecase,
+            EnemyBattleState enemyBattleState)
+        {
+            _enemyMoveUsecase = enemyMoveUsecase;
+            _enemyAttackReservationUsecase = enemyAttackReservationUsecase;
+            _enemyAttackUsecase = enemyAttackUsecase;
+            _enemyBattleState = enemyBattleState;
+
+            _enemyAttackReservationUsecase.OnReservedTimingReached += HandleReservedTimingReached;
+        }
+
+        // Debug用のイベント。
+        public event Action OnAttackReserved;
+        public event Action OnAttack;
+
+        public EnemyMoveInstruction Tick(Vector3 enemyPosition, Vector3 targetPosition)
+        {
+            EnemyMoveDecision moveDecision = _enemyMoveUsecase.Tick(enemyPosition, targetPosition);
+            Debug.Log($"[EnemyAI] ShouldMove={moveDecision.ShouldMove}, IsInRange={_enemyBattleState.IsInAttackRange}");
+
+            if (moveDecision.ShouldMove)
+            {
+                if (_enemyBattleState.IsInAttackRange)
+                {
+                    _enemyBattleState.ExitRange();
+                    _enemyAttackReservationUsecase.Cancel();
+                }
+
+                return new EnemyMoveInstruction(
+                    moveDecision.ShouldMove,
+                    moveDecision.Destination,
+                    moveDecision.Speed);
+            }
+
+            if (!_enemyBattleState.IsInAttackRange)
+            {
+                _enemyBattleState.EnterRange();
+
+                if (!_enemyAttackReservationUsecase.HasReservation)
+                {
+                    _enemyAttackReservationUsecase.ReserveEncounter();
+                    OnAttackReserved?.Invoke();
+                }
+            }
+
+            return new EnemyMoveInstruction(
+                    moveDecision.ShouldMove,
+                    moveDecision.Destination,
+                    moveDecision.Speed);
+        }
+
+        public void Dispose()
+        {
+            _enemyAttackReservationUsecase.OnReservedTimingReached -= HandleReservedTimingReached;
+            _enemyAttackReservationUsecase.Dispose();
+        }
+
+        private readonly EnemyMoveUsecase _enemyMoveUsecase;
+        private readonly EnemyAttackReservationUsecase _enemyAttackReservationUsecase;
+        private readonly EnemyAttackUsecase _enemyAttackUsecase;
+        private readonly EnemyBattleState _enemyBattleState;
+
+        private void HandleReservedTimingReached()
+        {
+            _enemyAttackUsecase.ExecuteAttack(
+                _enemyBattleState.Attacker,
+                _enemyBattleState.Target,
+                _enemyBattleState.AttackId);
+
+            OnAttack?.Invoke();
+
+            if (_enemyBattleState.IsInAttackRange)
+            {
+                _enemyAttackReservationUsecase.ReserveBattle();
+                OnAttackReserved?.Invoke();
+            }
+        }
+    }
+}
