e74d61b9ac2f31a016fcad0b3e3376b3e99fd033
HIBIKI_5201 <takuto5201@gmail.com>
2026-03-30 14:36:11 +0900

    [update]クラス名を変更

--- Assets/Scripts/Runtime/2.Application/Persistent/Input/InputBufferingQueue.cs
+++ Assets/Scripts/Runtime/2.Application/Persistent/Input/InputBufferingQueue.cs
@@ -1,16 +1,16 @@
-using KillChord.Runtime.Domain;
+using KillChord.Runtime.Domain.Persistent.Input;
 using KillChord.Runtime.Utility;
 using System;
 
-namespace KillChord.Runtime.Application
+namespace KillChord.Runtime.Application.Persistent.Input
 {
     /// <summary>
     ///     入力をバッファリングするクラス。
     ///     一定数の入力を保持し、新しい入力が追加されると古い入力が削除される。
     /// </summary>
-    public class BufferedInputBuffer
+    public class InputBufferingQueue
     {
-        public BufferedInputBuffer(int capacity)
+        public InputBufferingQueue(int capacity)
         {
             _buffer = new RingBuffer<BufferedInput>(capacity);
         }
@@ -31,6 +31,7 @@ namespace KillChord.Runtime.Application
         public BufferedInput GetLast(int offset = 0) => _buffer.PeekLast(offset);
 
         public void Clear() => _buffer.Clear();
+
         private readonly RingBuffer<BufferedInput> _buffer;
     }
 }
