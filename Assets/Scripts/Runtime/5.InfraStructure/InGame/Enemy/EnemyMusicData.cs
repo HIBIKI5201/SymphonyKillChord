7fa6de3ce32cc0179f7bdeb13c15ba4f7d3d9def
HIBIKI_5201 <takuto5201@gmail.com>
2026-03-31 20:13:40 +0900

    [update]Utilityのパス定数を使用

--- Assets/Scripts/Runtime/5.InfraStructure/InGame/Enemy/EnemyMusicData.cs
+++ Assets/Scripts/Runtime/5.InfraStructure/InGame/Enemy/EnemyMusicData.cs
@@ -1,13 +1,18 @@
+using KillChord.Runtime.Utility;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = nameof(EnemyMusicData), menuName = "KillChord/Enemy/" + nameof(EnemyMusicData))]
-public class EnemyMusicData : ScriptableObject
+namespace KillChord.Runtime.InfraStructure.InGame.Enemy
 {
-    public byte BarFlag => _barFlag;
-    public long TimeSignature => _timeSignature;
-    public long TargetBeat => _targetBeat;
+    [CreateAssetMenu(fileName = nameof(EnemyMusicData),
+    menuName = PathConst.CREATE_ASSET_MENU_PATH + "Enemy/" + nameof(EnemyMusicData))]
+    public class EnemyMusicData : ScriptableObject
+    {
+        public byte BarFlag => _barFlag;
+        public long TimeSignature => _timeSignature;
+        public long TargetBeat => _targetBeat;
 
-    [SerializeField] private byte _barFlag;
-    [SerializeField] private long _timeSignature;
-    [SerializeField] private long _targetBeat;
+        [SerializeField] private byte _barFlag;
+        [SerializeField] private long _timeSignature;
+        [SerializeField] private long _targetBeat;
+    }
 }
