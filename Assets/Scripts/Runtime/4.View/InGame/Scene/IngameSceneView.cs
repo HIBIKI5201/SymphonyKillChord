400e226106472d7504bcbead279dbfc779f62be6
reitaghchi <ren.len2426@gmail.com>
2026-04-07 21:53:16 +0900

    [update]シーンロードと一括アンロードを作成

--- Assets/Scripts/Runtime/4.View/InGame/Scene/IngameSceneView.cs
+++ Assets/Scripts/Runtime/4.View/InGame/Scene/IngameSceneView.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SymphonyFrameWork.System.SceneLoad;
 using UnityEngine;
 
 namespace KillChord.Runtime.View
@@ -7,6 +11,37 @@ namespace KillChord.Runtime.View
     /// </summary>
     public class IngameSceneView : MonoBehaviour
     {
-        
+        private List<string> _loadedScenes = new();
+
+        public ValueTask<bool> LoadScene(string sceneName)
+        {
+            _loadedScenes.Add(sceneName);
+            return SceneLoader.LoadScene(sceneName);
+        }
+
+        public ValueTask<bool> UnloadScene(string sceneName)
+        {
+            if (_loadedScenes.Contains(sceneName))
+            {
+                _loadedScenes.Remove(sceneName);
+                return SceneLoader.UnloadScene(sceneName);
+            }
+            else
+            {
+                Debug.LogError($"[IngameSceneView] Scene {sceneName} does not exist.]");
+                return default;
+            }
+        }
+
+        public async ValueTask UnloadAllScenes()
+        {
+            var unloadTasks = new Task<bool>[_loadedScenes.Count];
+            for (int i = 0; i < _loadedScenes.Count; i++)
+            {
+                unloadTasks[i] = SceneLoader.UnloadScene(_loadedScenes[i]).AsTask();
+            }
+
+            await Task.WhenAll(unloadTasks);
+        }
     }
 }
