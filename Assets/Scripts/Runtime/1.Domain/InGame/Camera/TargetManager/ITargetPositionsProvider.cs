3602d8ff32c84656fff131e582a04ac8453b1f0f
Oikawa <yucchi0424@gmail.com>
2026-04-07 19:27:42 +0900

    [add] ITargetPositionsProviderを追加

---
+++ Assets/Scripts/Runtime/1.Domain/InGame/Camera/TargetManager/ITargetPositionsProvider.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KillChord.Runtime.Domain
+{
+    public interface ITargetPositionsProvider
+    {
+        public IReadOnlyList<Vector3> TargetPositions { get; }
+        public void UpdatePositions();
+    }
+}
