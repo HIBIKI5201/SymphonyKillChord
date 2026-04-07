6dcd54dea8702f01571aa356e5318c96da9b90be
HIBIKI_5201 <takuto5201@gmail.com>
2026-04-08 04:24:46 +0900

    [update]ブレンド時間をついあk

--- Assets/DevelopProducts/Research/AnimationControl/Scripts/Runtime/AnimationBlendClipData.cs
+++ Assets/DevelopProducts/Research/AnimationControl/Scripts/Runtime/AnimationBlendClipData.cs
@@ -6,18 +6,26 @@ namespace DevelopProducts.AnimationControl
 {
     public readonly struct AnimationBlendClipData
     {
-        public readonly AnimationClip Clip;
-        public readonly AnimationClipPlayable ClipPlayable;
+        public AnimationClip Clip => _clip;
+        public AnimationClipPlayable ClipPlayable => _clipPlayable;
+        public float EnterDuration => _enterDuration;
+        public float ExitDuration => _exitDuration;
 
         public bool IsValid => ClipPlayable.IsValid();
 
         public AnimationBlendClipData(
-            AnimationClip clip,
-            AnimationClipPlayable clipPlayable
-        )
+            AnimationBlendClipRequest request,
+            AnimationClipPlayable clipPlayable)
         {
-            Clip = clip;
-            ClipPlayable = clipPlayable;
+            _clip = request.Clip;
+            _clipPlayable = clipPlayable;
+            _enterDuration = request.EnterDuration;
+            _exitDuration = request.ExitDuration;
         }
+
+        private readonly AnimationClip _clip;
+        private readonly AnimationClipPlayable _clipPlayable;
+        private readonly float _enterDuration;
+        private readonly float _exitDuration;
     }
 }
