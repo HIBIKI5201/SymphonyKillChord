9dbcaaeb81a2905f4d43903d867cbaae42d8437b
HIBIKI_5201 <takuto5201@gmail.com>
2026-04-09 04:28:01 +0900

    [update]アセットに命名変更

--- Assets/DevelopProducts/Research/Achievement/Scripts/Runtime/AchievementElementAsset.cs
+++ Assets/DevelopProducts/Research/Achievement/Scripts/Runtime/AchievementElementAsset.cs
@@ -3,9 +3,9 @@ using SymphonyFrameWork.Attribute;
 
 namespace DevelopProducts.Achievement
 {
-    [CreateAssetMenu(fileName = nameof(AchievementElement),
-        menuName = Const.CREATE_ASSET_MENU + nameof(AchievementElement))]
-    public class AchievementElement : ScriptableObject
+    [CreateAssetMenu(fileName = nameof(AchievementElementAsset),
+        menuName = Const.CREATE_ASSET_MENU + nameof(AchievementElementAsset))]
+    public class AchievementElementAsset : ScriptableObject
     {
         [SerializeReference, SubclassSelector]
         private IAchievementCondition[] _conditions;
