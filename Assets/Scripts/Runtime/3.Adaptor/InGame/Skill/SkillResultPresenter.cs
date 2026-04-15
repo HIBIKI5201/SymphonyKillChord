42e01882d3663e6de74c3dae3ebee026a3050f5f
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2026-04-15 23:54:36 +0900

    [add]skillpresenter

---
+++ Assets/Scripts/Runtime/3.Adaptor/InGame/Skill/SkillResultPresenter.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace KillChord.Runtime.Adaptor
+{
+    public class SkillResultPresenter : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
