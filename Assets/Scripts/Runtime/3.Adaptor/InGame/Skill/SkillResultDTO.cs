0cc05ea70b9c317eee9e335e063173145eed29fb
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2026-04-15 23:32:51 +0900

    [update]skillresultDTO

--- Assets/Scripts/Runtime/3.Adaptor/InGame/Skill/SkillResultDTO.cs
+++ Assets/Scripts/Runtime/3.Adaptor/InGame/Skill/SkillResultDTO.cs
@@ -2,8 +2,22 @@ using UnityEngine;
 
 namespace KillChord.Runtime.Adaptor
 {
+    /// <summary>
+    ///     スキルの結果を表すデータ転送オブジェクト（DTO）。
+    /// </summary>
     public readonly struct SkillResultDTO
     {
-        
+        public SkillResultDTO(int skillId, int[] skillPattern, string skillName)
+        {
+            SkillId = skillId;
+            SkillPattern = skillPattern;
+            SkillName = skillName;
+        }
+
+        public int SkillId { get; }
+
+        public int[] SkillPattern { get; }
+
+        public string SkillName { get; }
     }
 }
