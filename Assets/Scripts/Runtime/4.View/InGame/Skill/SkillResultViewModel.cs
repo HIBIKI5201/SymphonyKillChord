5f77de704815226c462a23a04641441a7444d9e3
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2026-04-17 13:11:32 +0900

    [add]script

---
+++ Assets/Scripts/Runtime/4.View/InGame/Skill/SkillResultViewModel.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace KillChord.Runtime.View
+{
+    public class SkillResultViewModel : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
