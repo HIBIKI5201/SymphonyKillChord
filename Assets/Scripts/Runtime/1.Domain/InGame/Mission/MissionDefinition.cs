0c2af63873953dd912f4a96437076d8c5c742f1f
ayato0913a <ayato0913a@gmail.com>
2026-04-20 05:18:02 +0900

    [ add ] ミッション情報関連のクラスを作成

---
+++ Assets/Scripts/Runtime/1.Domain/InGame/Mission/MissionDefinition.cs
@@ -0,0 +1,40 @@
+using KillChord.Runtime.Domain.InGame.Mission.ClearCondition;
+using KillChord.Runtime.Domain.InGame.Mission.EvaluationCondition;
+using KillChord.Runtime.Domain.InGame.Mission.FailCondition;
+using System.Collections.Generic;
+
+namespace KillChord.Runtime.Domain.InGame.Mission
+{
+    /// <summary>
+    ///     ミッションの定義を表すクラス。
+    /// </summary>
+    public class MissionDefinition
+    {
+        public MissionDefinition(
+            MissionId missionId,
+            string desplayName,
+            string description,
+            string mainMissionText,
+            IMissionClearCondition clearCondition,
+            IMissionFailCondition failCondition,
+            IReadOnlyList<IMissionEvaluationCondition> evaluationConditions)
+        {
+            MissionId = missionId;
+            DesplayName = desplayName;
+            Description = description;
+            MainMissionText = mainMissionText;
+            ClearCondition = clearCondition;
+            FailCondition = failCondition;
+            EvaluationConditions = evaluationConditions;
+        }
+
+        public MissionId MissionId { get; }
+        public string DesplayName { get; }
+        public string Description { get; }
+        public string MainMissionText { get; }
+
+        public IMissionClearCondition ClearCondition { get; }
+        public IMissionFailCondition FailCondition { get; }
+        public IReadOnlyList<IMissionEvaluationCondition> EvaluationConditions { get; }
+    }
+}
