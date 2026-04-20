fb29b99dfffc6397d672fb9741911f9160dcb15e
reitaghchi <ren.len2426@gmail.com>
2026-04-20 18:09:55 +0900

    [update]カメラの入力経路をInputSystemに改修開始

--- Assets/Scripts/Runtime/4.View/InGame/Camera/CameraSystemView.cs
+++ Assets/Scripts/Runtime/4.View/InGame/Camera/CameraSystemView.cs
@@ -1,9 +1,12 @@
 using System;
 using KillChord.Runtime.Adaptor.InGame.Camera;
+using KillChord.Runtime.Adaptor.Persistent.Input;
 using KillChord.Runtime.Utility;
+using KillChord.Runtime.View.Persistent.Input;
 using SymphonyFrameWork.Debugger.HUD;
 using SymphonyFrameWork.System.ServiceLocate;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace KillChord.Runtime.View.InGame.Camera
 {
@@ -17,18 +20,17 @@ namespace KillChord.Runtime.View.InGame.Camera
         [SerializeField] private UpdateModeEnum _updateMode;
 
         private CameraSystemController _controller;
+        private PlayerInputView _inputView;
         private Transform _playerT;
         private Vector2 _input;
 
-        public void Init(CameraSystemController controller, Transform playerT)
+        public void Init(CameraSystemController controller, Transform playerT, PlayerInputView playerInputView)
         {
             _controller = controller;
             _playerT = playerT;
-        }
+            _inputView = playerInputView;
 
-        private void Start()
-        {
-            Cursor.lockState = CursorLockMode.Locked;
+            _inputView.OnLookInput += OnLook;
         }
 
         private void FixedUpdate()
@@ -55,6 +57,26 @@ namespace KillChord.Runtime.View.InGame.Camera
             if (_updateMode != UpdateModeEnum.LateUpdate)
                 return;
             Tick(Time.deltaTime);
+        }
+
+        private void OnLook(InputContext<Vector2> context)
+        {
+        }
+
+        private void OnNextLockOn(InputContext<float> context)
+        {
+            if (context.Phase == InputActionPhase.Started)
+            {
+                _controller.ToggleLockOnState(_playerT.position);
+            }
+        }
+
+        private void OnPreviousLockOn(InputContext<float> context)
+        {
+            if (context.Phase == InputActionPhase.Started)
+            {
+                _controller.TryActiveAutoLockOn(_playerT.position);
+            }
         }
 
 
