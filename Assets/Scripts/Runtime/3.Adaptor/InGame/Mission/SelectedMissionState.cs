619f6045144d72fcbd402bac1e0a82607b17159a
ayato0913a <ayato0913a@gmail.com>
2026-04-21 16:51:42 +0900

    [ fix ] ガード

--- Assets/Scripts/Runtime/3.Adaptor/InGame/Mission/SelectedMissionState.cs
+++ Assets/Scripts/Runtime/3.Adaptor/InGame/Mission/SelectedMissionState.cs
@@ -1,4 +1,5 @@
 using KillChord.Runtime.Domain.InGame.Mission;
+using System;
 
 namespace KillChord.Runtime.Adaptor.InGame.Mission
 {
@@ -7,11 +8,27 @@ namespace KillChord.Runtime.Adaptor.InGame.Mission
     /// </summary>
     public class SelectedMissionState
     {
-        public MissionId CurrentMissionId { get; private set; }
+        public MissionId CurrentMissionId
+        {
+            get
+            {
+                if (!HasSelectedMission)
+                {
+                    throw new InvalidOperationException("Mission has not been selected.");
+                }
+
+                return _currentMissionId;
+            }
+        }
+
+        public bool HasSelectedMission { get; private set; }
 
         public void SelectMission(MissionId missionId)
         {
-            CurrentMissionId = missionId;
+            _currentMissionId = missionId;
+            HasSelectedMission = true;
         }
+
+        private MissionId _currentMissionId;
     }
 }
