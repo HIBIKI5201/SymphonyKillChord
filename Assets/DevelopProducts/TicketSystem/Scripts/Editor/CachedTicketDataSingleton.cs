8418035845bed07a242934cbb6b729e7dd9ba970
AoiTomiyama <TadoO.o2209@gmail.com>
2026-04-24 20:49:46 +0900

    [add] チケットキャッシュに要素のソートを実装。

--- Assets/DevelopProducts/TicketSystem/Scripts/Editor/CachedTicketDataSingleton.cs
+++ Assets/DevelopProducts/TicketSystem/Scripts/Editor/CachedTicketDataSingleton.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEngine;
 
 namespace DevelopProducts.TicketSystem
 {
@@ -11,21 +12,65 @@ namespace DevelopProducts.TicketSystem
     {
         private readonly List<TicketData> cachedTickets = new();
 
+        /// <summary>
+        /// キャッシュされたチケットデータをすべて取得する。
+        /// </summary>
+        /// <returns></returns>
         public List<TicketData> GetAll()
         {
             return cachedTickets;
         }
 
+        /// <summary>
+        /// キャッシュされたチケットデータをすべてクリアする。
+        /// </summary>
         public void Clear()
         {
             cachedTickets.Clear();
         }
-        
-        public void Set(IEnumerable<TicketData> ticketDataList)
+
+        /// <summary>
+        /// チケットデータのリストをキャッシュにセットする。
+        /// </summary>
+        /// <param name="ticketDataList"></param>
+        public void Set(List<TicketData> ticketDataList)
         {
             cachedTickets.Clear();
             cachedTickets.AddRange(ticketDataList);
+            Sort(cachedTickets);
             OnTicketAdded?.Invoke();
+        }
+
+        /// <summary>
+        /// チケットデータのリストをソートする。
+        /// ソートの優先順位は以下の通り。
+        /// 1. 自分のチケットを優先
+        /// 2. 空きチケットを優先
+        /// 3. シーン名でソート
+        /// </summary>
+        /// <param name="ticketDataList"></param>
+        private static void Sort(List<TicketData> ticketDataList)
+        {
+            var currentUserName = TicketSystemSettings.instance.userName;
+            if (string.IsNullOrEmpty(currentUserName))
+            {
+                Debug.LogWarning($"ユーザー名が設定されていません。");
+                return;
+            }
+
+            ticketDataList.Sort((a, b) =>
+            {
+                // 自分のチケットを優先
+                var aIsMine = a.userName == currentUserName;
+                var bIsMine = b.userName == currentUserName;
+                if (aIsMine != bIsMine) return aIsMine ? -1 : 1;
+
+                // 次に空きチケットを優先
+                if (a.isInUse != b.isInUse) return a.isInUse ? 1 : -1;
+
+                // 最後にシーン名でソート
+                return string.Compare(a.sceneName, b.sceneName, StringComparison.Ordinal);
+            });
         }
 
         /// <summary>
