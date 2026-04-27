69126d7c1eeec22c15c172c17fcc873de11ad0aa
YamamiyaShota <153970961+YamamiyaShota@users.noreply.github.com>
2026-04-27 17:25:09 +0900

    [add] アウトゲーム UI のイベントを管理するクラスを追加。

---
+++ Assets/Scripts/Runtime/4.View/OutGame/Screen/OutGameUIEvent.cs
@@ -0,0 +1,66 @@
+using SymphonyFrameWork.System.ServiceLocate;
+using System;
+using UnityEngine;
+
+namespace KillChord.Runtime.View.OutGame.Screen
+{
+    /// <summary>
+    ///     アウトゲーム UI のイベントを管理するクラス。
+    ///     イベント管理クラスのため、他のクラスよりも早く初期化されるように
+    ///     DefaultExecutionOrder を -100 に設定しています。
+    /// </summary>
+    [DefaultExecutionOrder(-100)]
+    public class OutGameUIEvent : MonoBehaviour
+    {
+        /// <summary> ホーム画面を表示するイベント。 </summary>
+        public Action OnShownHomeScreen;
+
+        /// <summary> 作戦画面を表示するイベント。 </summary>
+        public Action OnShownStageSelectionScreen;
+
+        /// <summary> 研究画面を表示するイベント。 </summary>
+        public Action OnShownSkillTreeScreen;
+
+        /// <summary> 改造画面を表示するイベント。 </summary>
+        public Action OnShownSkillBuildScreen;
+
+        /// <summary> 設定画面を表示するイベント。 </summary>
+        public Action OnShownSettingScreen;
+
+        /// <summary> 画面を閉じるイベント。 </summary>
+        public Action OnScreenClosed;
+
+        /// <summary>
+        ///     アウトゲームのUIイベントを ServiceLocator に登録します。
+        /// </summary>
+        public void RegisterOutGameUIEvent()
+        {
+            ServiceLocator.RegisterInstance(this);
+        }
+
+        /// <summary>
+        ///     アウトゲームのUIイベントを ServiceLocator から登録解除します。
+        /// </summary>
+        public void UnregisterOutGameUIEvent()
+        {
+            ServiceLocator.UnregisterInstance(this);
+        }
+
+        /// <summary>
+        ///     ServiceLocator にインスタンスを登録する。
+        /// </summary>
+        private void Awake()
+        {
+            RegisterOutGameUIEvent();
+        }
+
+        /// <summary>
+        ///     ServiceLocator からインスタンスを登録解除する。
+        ///     自身で登録解除を呼び出すとエラーが起きるため、コメントアウトしています。
+        /// </summary>
+        private void OnDestroy()
+        {
+            //UnregisterOutGameUIEvent();
+        }
+    }
+}
