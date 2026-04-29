10bfe2a4081e6759287a7706340bf708b72cddf1
HIBIKI_5201 <takuto5201@gmail.com>
2026-04-30 03:46:43 +0900

    [update]パスを表示

--- Assets/Editor/Scripts/AutoBuilder/AutoBuilderProvider.cs
+++ Assets/Editor/Scripts/AutoBuilder/AutoBuilderProvider.cs
@@ -22,12 +22,20 @@ namespace KillChord.Editor.AutoBuilder
         public override void OnGUI(string searchContext)
         {
             SerializedObject so = new SerializedObject(AutoBuilderSettings.instance);
+
+            SerializedProperty masterPath = so.FindProperty(nameof(AutoBuilderSettings.instance.MasterPath));
             SerializedProperty masterProp = so.FindProperty(nameof(AutoBuilderSettings.instance.MasterBuildProfiles));
+            
+            SerializedProperty devPath = so.FindProperty(nameof(AutoBuilderSettings.instance.DevelopPath));
             SerializedProperty devProp = so.FindProperty(nameof(AutoBuilderSettings.instance.DevelopBuildProfiles));
 
             EditorGUI.BeginChangeCheck();
 
+            masterPath.stringValue = EditorGUILayout.TextField(masterPath.stringValue);
             EditorGUILayout.PropertyField(masterProp, true);
+
+            EditorGUILayout.Space(10);
+            devPath.stringValue = EditorGUILayout.TextField(devPath.stringValue);
             EditorGUILayout.PropertyField(devProp, true);
 
             if (EditorGUI.EndChangeCheck()) { AutoBuilderSettings.Save(); }
