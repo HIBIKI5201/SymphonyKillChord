b653509f24a0c13aa8ecc8e6c1520086e68857b5
HIBIKI_5201 <takuto5201@gmail.com>
2026-04-30 03:34:25 +0900

    [update]Settingを表示するように

--- Assets/Editor/Scripts/AutoBuilder/AutoBuilderProvider.cs
+++ Assets/Editor/Scripts/AutoBuilder/AutoBuilderProvider.cs
@@ -1,14 +1,34 @@
+using KillChord.Editor.Utility;
 using System.Collections.Generic;
 using UnityEditor;
 
 namespace KillChord.Editor.AutoBuilder
 {
+    /// <summary>
+    ///     オートビルダーの設定画面。
+    /// </summary>
     public class AutoBuilderProvider : SettingsProvider
     {
         public AutoBuilderProvider(string path, SettingsScope scopes, IEnumerable<string> keywords = null) : base(path, scopes, keywords)
         {
         }
 
+        [SettingsProvider]
+        public static SettingsProvider CreateSettingsProvider()
+        {
+            return new AutoBuilderProvider(SETTINGS_PATH, SettingsScope.Project);
+        }
 
+        public override void OnGUI(string searchContext)
+        {
+            SerializedObject so = new SerializedObject(AutoBuilderSettings.instance);
+            SerializedProperty prop = so.FindProperty("BuildProfiles");
+
+            EditorGUILayout.PropertyField(prop, true);
+
+            so.ApplyModifiedProperties();
+        }
+
+        private const string SETTINGS_PATH = ProviderConst.PROJECT_PATH + "AutoBuilder";
     }
 }
