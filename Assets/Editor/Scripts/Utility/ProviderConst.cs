57f42e888f4a119f798844efcd5831a9a0407e89
HIBIKI_5201 <takuto5201@gmail.com>
2026-04-30 03:33:41 +0900

    [add]データパスを追加

--- Assets/Editor/Scripts/Utility/ProviderConst.cs
+++ Assets/Editor/Scripts/Utility/ProviderConst.cs
@@ -1,7 +1,14 @@
 using UnityEngine;
 
-public static class ProviderConst
+namespace KillChord.Editor.Utility
 {
-    public const string PROJECT_PATH = "Project/";
-    public const string PREFERENCE_PATH = "Preference/";
+    public static class ProviderConst
+    {
+        public const string PROJECT_PATH = "Project/";
+        public const string PREFERENCE_PATH = "Preference/";
+        public const string ASSET_EXT = ".asset";
+
+        public const string USER_SETTINGS_PATH = "UserSettings/";
+        public const string PROJECT_SETTINGS_PATH = "ProjectSettings/";
+    }
 }
