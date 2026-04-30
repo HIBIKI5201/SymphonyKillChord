f384070ad9f1c1f995ad85cf5818435baebd7c07
HIBIKI_5201 <takuto5201@gmail.com>
2026-04-30 21:36:45 +0900

    [fix]細かいパスの指定を変更

--- Assets/Editor/Scripts/Utility/ProviderConst.cs
+++ Assets/Editor/Scripts/Utility/ProviderConst.cs
@@ -7,11 +7,11 @@ namespace KillChord.Editor.Utility
     /// </summary>
     public static class ProviderConst
     {
-        public const string PROJECT_PATH = "Project/";
-        public const string PREFERENCE_PATH = "Preference/";
+        public const string PROJECT_PATH = "Project/KillChord/";
+        public const string PREFERENCE_PATH = "Preference/KillChord/";
         public const string ASSET_EXT = ".asset";
 
-        public const string USER_SETTINGS_PATH = "UserSettings/";
-        public const string PROJECT_SETTINGS_PATH = "ProjectSettings/";
+        public const string USER_SETTINGS_PATH = "UserSettings/KillChord/";
+        public const string PROJECT_SETTINGS_PATH = "ProjectSettings/KillChord/";
     }
 }
