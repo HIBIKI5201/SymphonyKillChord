a0c1bbea0f4e186ddadfa5e5d023782ee16a5c53
kenkatou354 <kenkatou354@gmail.com>
2026-04-30 20:29:45 +0900

    [add] 回避クールダウンのVOを追加

---
+++ Assets/Scripts/Runtime/1.Domain/InGame/Prameter/DodgeCooldown.cs
@@ -0,0 +1,48 @@
+using System;
+
+namespace KillChord.Runtime.Domain
+{
+    /// <summary>
+    ///     回避クールダウンを表すVO。
+    /// </summary>
+    public readonly struct DodgeCooldown
+    {
+        public DodgeCooldown(float value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("value must be non-negative.", nameof(value));
+            }
+            if (!float.IsFinite(value))
+            {
+                throw new ArgumentException("value must be finite.", nameof(value));
+            }
+
+            Value = value;
+        }
+
+        /// <summary>
+        ///     回避クールダウンの値。
+        /// </summary>
+        public readonly float Value;
+
+        public static explicit operator float(DodgeCooldown value)
+            => value.Value;
+        public static bool operator ==(DodgeCooldown left, DodgeCooldown right)
+            => left.Value == right.Value;
+        public static bool operator !=(DodgeCooldown left, DodgeCooldown right)
+            => left.Value != right.Value;
+        public override bool Equals(object obj)
+        {
+            return obj is DodgeCooldown dodgeCooldown && dodgeCooldown.Value == Value;
+        }
+        public bool Equals(DodgeCooldown other)
+        {
+            return other.Value == Value;
+        }
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+    }
+}
