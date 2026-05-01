6fc559ab8f2562eff38b44bd5116ef4018e4e9ca
ayato0913a <ayato0913a@gmail.com>
2026-05-01 16:45:22 +0900

    [ fix ] VIewModelからStateに

--- Assets/Scripts/Runtime/6.Composition/InGame/Enemy/ShellInitializer.cs
+++ Assets/Scripts/Runtime/6.Composition/InGame/Enemy/ShellInitializer.cs
@@ -1,5 +1,5 @@
-using KillChord.Runtime.Adaptor;
 using KillChord.Runtime.Adaptor.InGame.Enemy;
+using KillChord.Runtime.Adaptor.InGame.Music;
 using KillChord.Runtime.Application.InGame.Battle;
 using KillChord.Runtime.Application.InGame.Enemy;
 using KillChord.Runtime.Composition.InGame.Music;
@@ -14,7 +14,7 @@ namespace KillChord.Runtime.Composition.InGame.Enemy
     ///     TODO 敵砲弾のコントローラーを初期化する。
     ///     将来リファクタリングする予想。毎回初期化は負荷が重いので、キャッシュや再利用などの手段にあわせて実装したい。
     /// </summary>
-    public class ShellInitializer : MonoBehaviour , IShellInitializer
+    public class ShellInitializer : MonoBehaviour, IShellInitializer
     {
         private void Awake()
         {
@@ -22,15 +22,15 @@ namespace KillChord.Runtime.Composition.InGame.Enemy
         }
         public ShellController InitAndGetShellController(ShellView shellView, EnemyAIController enemyAIController)
         {
-            if(!_musicSyncInitializer) _musicSyncInitializer = FindFirstObjectByType<MusicSyncInitializer>();
-            if(!_musicSyncView) _musicSyncView = FindAnyObjectByType<MusicSyncView>();
+            if (!_musicSyncInitializer) _musicSyncInitializer = FindFirstObjectByType<MusicSyncInitializer>();
+            if (!_musicSyncView) _musicSyncView = FindAnyObjectByType<MusicSyncView>();
 
-            if (_musicSyncView.MusicSyncViewModel == null)
+            if (_musicSyncView.MusicSyncState == null)
             {
-                Debug.LogError("MusicSyncViewが見つかりません。", this);
+                Debug.LogError("MusicSyncStateが見つかりません。", this);
                 return null;
             }
-            IMusicActionScheduler musicActionScheduler = new MusicSchedulerAdaptor(_musicSyncView.MusicSyncViewModel, _musicSyncInitializer.MusicSyncService);
+            IMusicActionScheduler musicActionScheduler = new MusicSchedulerAdaptor(_musicSyncView.MusicSyncState, _musicSyncInitializer.MusicSyncService);
             ShellReservationUsecase reservationUsecase = new ShellReservationUsecase(musicActionScheduler);
             ShellController controller = new ShellController(shellView, reservationUsecase, enemyAIController);
             return controller;
