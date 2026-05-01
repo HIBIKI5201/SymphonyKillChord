9fed9c1511e4c07bcd0a58595ea0c3b68c6ae528
ayato0913a <ayato0913a@gmail.com>
2026-05-01 16:25:51 +0900

    [ fix ] Beatをdouble型にスローするか補正するかは要相談

--- Assets/Scripts/Runtime/1.Domain/Persistent/Music/Beat.cs
+++ Assets/Scripts/Runtime/1.Domain/Persistent/Music/Beat.cs
@@ -8,8 +8,18 @@ namespace KillChord.Runtime.Domain.Persistent.Music
     [Serializable]
     public readonly struct Beat
     {
-        public Beat(float signature, float count)
+        public Beat(double signature, double count)
         {
+            // TODO: 後で相談。
+            if (signature <= 0d)
+            {
+                throw new ArgumentOutOfRangeException(nameof(signature), "拍子は正の数でなければなりません。");
+            }
+
+            if(count <= 0d)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count), "拍数は正の数でなければなりません。");
+            }
 
             _signature = Math.Max(signature, 1);
             _count = count;
@@ -20,13 +30,15 @@ namespace KillChord.Runtime.Domain.Persistent.Music
 
         public static double GetLength(Beat beat, double bpm)
         {
-            double beatSeconds = 60d / bpm;
-
-            double barSeconds = beatSeconds * 4d; // 1小節は4/4固定。
+            double beatSeconds = SECONDS_PER_MINUTE / bpm;
+            double barSeconds = beatSeconds * FOUR_FOUR_BEAT_COUNT;
             double unitSeconds = barSeconds / beat._signature;
 
             return unitSeconds * beat._count;
         }
+
+        private const double SECONDS_PER_MINUTE = 60d;
+        private const double FOUR_FOUR_BEAT_COUNT = 4d; // 1小節は4/4固定。
 
         private readonly double _signature;
         private readonly double _count;
