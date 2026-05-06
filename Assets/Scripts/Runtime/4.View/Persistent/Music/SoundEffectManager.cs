b0ffa63490556e24980faeaabe55040c4d068b5c
reitaghchi <ren.len2426@gmail.com>
2026-05-06 18:14:04 +0900

    [update]SoundEffectManager作成

--- Assets/Scripts/Runtime/4.View/Persistent/Music/SoundEffectManager.cs
+++ Assets/Scripts/Runtime/4.View/Persistent/Music/SoundEffectManager.cs
@@ -1,4 +1,3 @@
-using CriWare;
 using KillChord.Runtime.View.SoundEffect;
 using UnityEngine;
 
@@ -6,6 +5,29 @@ namespace KillChord.Runtime.View
 {
     public class SoundEffectManager : MonoBehaviour
     {
+        [SerializeField] private int _bufferCount;
+        [SerializeField] private SoundEffectPlayer _sePrefab;
         private RecycleBuffer<SoundEffectPlayer> _recycleBuffer;
+
+        public void Initialize()
+        {
+            SoundEffectPlayer[] players = new SoundEffectPlayer[_bufferCount];
+            for (int i = 0; i < _bufferCount; i++)
+            {
+                players[i] = Instantiate(_sePrefab);
+            }
+
+            _recycleBuffer = new(players);
+        }
+
+        public void PlaySe(string cueName)
+        {
+            _recycleBuffer.Get().Play(cueName);
+        }
+
+        public void StopAllSe()
+        {
+            _recycleBuffer.RecycleAll();
+        }
     }
 }
