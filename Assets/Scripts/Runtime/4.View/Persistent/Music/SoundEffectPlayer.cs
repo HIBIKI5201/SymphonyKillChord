e2ece1ed96053394f5b7cf08a3a3e08648ab0769
reitaghchi <ren.len2426@gmail.com>
2026-05-06 18:58:30 +0900

    [update]完成

--- Assets/Scripts/Runtime/4.View/Persistent/Music/SoundEffectPlayer.cs
+++ Assets/Scripts/Runtime/4.View/Persistent/Music/SoundEffectPlayer.cs
@@ -1,24 +1,10 @@
 using CriWare;
-using KillChord.Runtime.View.SoundEffect;
 using UnityEngine;
 
-namespace KillChord.Runtime.View
+namespace KillChord.Runtime.View.Persistent.SoundEffect
 {
-    public class SoundEffectPlayer : MonoBehaviour, IRecyclable
+    public class SoundEffectPlayer : MonoBehaviour
     {
-        private CriAtomSource _source;
-        private CriAtomExPlayback _playback;
-        public int RecycleId { get; set; }
 
-        public void OnRecycle()
-        {
-            _playback.Stop();
-        }
-
-        public void Play(string cueName)
-        {
-            _source.cueName = cueName;
-            _playback = _source.Play();
-        }
     }
 }
