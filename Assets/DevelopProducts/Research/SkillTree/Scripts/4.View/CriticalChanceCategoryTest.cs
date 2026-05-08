0149ada19af50b82372bc08310a1e63e85f9d337
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2026-05-08 16:29:36 +0900

    [add]クラス追加

---
+++ Assets/DevelopProducts/Research/SkillTree/Scripts/4.View/CriticalChanceCategoryTest.cs
@@ -0,0 +1,12 @@
+namespace DevelopProducts.SkillTree
+{
+    public class CriticalChanceCategoryTest : IEdgeParameterCategory
+    {
+        public float Value => throw new System.NotImplementedException();
+
+        public void Apply()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
