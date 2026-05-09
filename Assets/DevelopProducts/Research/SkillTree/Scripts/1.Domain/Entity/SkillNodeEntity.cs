336fe95371b86c867f5284907a0a9309d6e585d5
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-05-10 05:49:57 +0900

    [update]ノードのエンティティ

--- Assets/DevelopProducts/Research/SkillTree/Scripts/1.Domain/Entity/SkillNodeEntity.cs
+++ Assets/DevelopProducts/Research/SkillTree/Scripts/1.Domain/Entity/SkillNodeEntity.cs
@@ -2,13 +2,36 @@ namespace DevelopProducts.SkillTree
 {
     public class SkillNodeEntity
     {
-        public SkillNodeEntity(int nodeId, SkillNodeEntity parent = null)
+        public SkillNodeEntity(int nodeId,
+                               int cost,
+                               INodeUnlockEffect[] nodeUnlockEffects, 
+                               bool isUnlocked,
+                               bool isRoot = false,
+                               SkillNodeEntity parent = null)
         {
-            SkillNodeIdVOTest = new SkillNodeIdVOTest(nodeId);
+            SkillNodeIdVO = new SkillNodeIdVo(nodeId);
+            UnlockCost = new UnlockCost(cost);
+            NodeUnlockEffects = nodeUnlockEffects;
+            _isUnlocked = isUnlocked;
+            IsRoot = isRoot;
             Parent = parent;
         }
+        public SkillNodeIdVo SkillNodeIdVO { get; }
+        public UnlockCost UnlockCost { get; }
+        public INodeUnlockEffect[] NodeUnlockEffects { get; }
+        public bool IsUnlocked => _isUnlocked;
+        public bool IsRoot { get; }
         public SkillNodeEntity Parent { get; }
-        public SkillNodeIdVOTest SkillNodeIdVOTest { get; }
+        
+        public void Unlock() => _isUnlocked = true;
 
+        public void Lock()
+        {
+            //  原点ノードだった場合ロックできない
+            if (IsRoot) return;
+            
+            _isUnlocked = false;
+        }
+        private bool _isUnlocked;
     }
 }
