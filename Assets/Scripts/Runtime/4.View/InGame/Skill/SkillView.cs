5a3ba076608cdd162e1d1300bf832faa056dd87a
ayato0913a <ayato0913a@gmail.com>
2026-05-09 20:47:57 +0900

    [ fix ] 音が鳴るように

--- Assets/Scripts/Runtime/4.View/InGame/Skill/SkillView.cs
+++ Assets/Scripts/Runtime/4.View/InGame/Skill/SkillView.cs
@@ -1,3 +1,4 @@
+using CriWare;
 using KillChord.Runtime.Adaptor.InGame.Skill;
 using UnityEngine;
 
@@ -8,9 +9,24 @@ namespace KillChord.Runtime.View.InGame.Skill
         public int Id => _id;
         public void Execute()
         {
+            Debug.Log($"SkillView Execute: {Id}");
             //実際のViewで起こる演出など
+            if (_source == null)
+            {
+                _source = this.gameObject.GetComponent<CriAtomSource>();
+            }
+
+            if (_source != null && !string.IsNullOrEmpty(_cueName))
+            {
+                Debug.Log($"Playing sound: {_cueName}");
+                _source.Play(_cueName);
+            }
         }
 
         [SerializeField] private int _id;
+
+        [SerializeField] private CriAtomSource _source;
+
+        [SerializeField] private string _cueName;
     }
 }
