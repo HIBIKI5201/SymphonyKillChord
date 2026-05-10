515151676d68bbd52999d12bba0bba25d5fe2f17
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-05-10 21:03:08 +0900

    [update]NodeEntityみ解除可能かのフラグ関連追加

--- Assets/DevelopProducts/Research/SkillTree/Scripts/1.Domain/Entity/SkillNodeEntity.cs
+++ Assets/DevelopProducts/Research/SkillTree/Scripts/1.Domain/Entity/SkillNodeEntity.cs
@@ -1,37 +1,46 @@
+using System.Collections.Generic;
+
 namespace DevelopProducts.SkillTree
 {
     public class SkillNodeEntity
     {
         public SkillNodeEntity(int nodeId,
-                               int cost,
-                               INodeUnlockEffect[] nodeUnlockEffects, 
-                               bool isUnlocked,
-                               bool isRoot = false,
-                               SkillNodeEntity parent = null)
+            int cost,
+            INodeUnlockEffect[] nodeUnlockEffects,
+            bool isUnlocked,
+            bool isRoot = false,
+            bool isEnable = false)
         {
             SkillNodeIdVO = new SkillNodeIdVo(nodeId);
             UnlockCost = new UnlockCost(cost);
             NodeUnlockEffects = nodeUnlockEffects;
             _isUnlocked = isUnlocked;
             IsRoot = isRoot;
-            Parent = parent;
+            IsEnable = isEnable;
         }
+
         public SkillNodeIdVo SkillNodeIdVO { get; }
         public UnlockCost UnlockCost { get; }
         public INodeUnlockEffect[] NodeUnlockEffects { get; }
         public bool IsUnlocked => _isUnlocked;
         public bool IsRoot { get; }
-        public SkillNodeEntity Parent { get; }
-        
+        public bool IsEnable { get; }
+
         public void Unlock() => _isUnlocked = true;
+
+        public bool CanLock()
+        {
+            return true;
+        }
 
         public void Lock()
         {
             //  原点ノードだった場合ロックできない
             if (IsRoot) return;
-            
+
             _isUnlocked = false;
         }
+
         private bool _isUnlocked;
     }
 }
