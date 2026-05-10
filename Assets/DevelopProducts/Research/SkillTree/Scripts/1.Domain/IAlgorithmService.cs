ba6549fde26743fccbcd75362f9d1138423c3cd8
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-05-10 20:52:47 +0900

    [update]スキルツリーの一括開放用のアルゴリズムサービス

--- Assets/DevelopProducts/Research/SkillTree/Scripts/1.Domain/IAlgorithmService.cs
+++ Assets/DevelopProducts/Research/SkillTree/Scripts/1.Domain/IAlgorithmService.cs
@@ -1,9 +1,11 @@
-using UnityEngine;
+using System.Collections.Generic;
 
 namespace DevelopProducts.SkillTree
 {
     public interface IAlgorithmService
     {
-        
+        IReadOnlyList<SkillNodeEntity> FindPath(
+            SkillNodeEntity target,
+            SkillTreeEntity tree);
     }
 }
