72f03476bd10f143f6048f16056b0afa6ffcb76c
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-05-12 03:07:49 +0900

    [add]単体ノードのデータ

---
+++ Assets/DevelopProducts/Research/SkillTree/Scripts/5.Infrastructure/SkillNodeAsset.cs
@@ -0,0 +1,29 @@
+using SymphonyFrameWork.Attribute;
+using UnityEngine;
+
+namespace DevelopProducts.SkillTree
+{
+    [CreateAssetMenu(fileName = "SkillNodeAsset", menuName = "DevelopProducts/SkillTree/SkillNodeAsset")]
+    public class SkillNodeAsset : ScriptableObject
+    {
+        public SkillNodeEntity SkillNodeEntity => _skillNodeEntity;
+        public INodeUnlockEffect[] NodeUnlockEffects => _nodeUnlockEffets;
+        public SkillNodeAsset[] Parents => _parents;
+        public int Cost => _cost;
+
+        public SkillNodeEntity ToDomain()
+        {
+            _skillNodeEntity = new SkillNodeEntity(_id, _cost, _nodeUnlockEffets, _isUnlocked, _isEnable);
+            return _skillNodeEntity;
+        }
+        [SerializeReference, SubclassSelector] private INodeUnlockEffect[] _nodeUnlockEffets;
+
+        [SerializeField] private int _id;
+        [SerializeField] private bool _isUnlocked;
+        [SerializeField] private bool _isEnable;
+        [SerializeField] private SkillNodeAsset[] _parents;
+        [SerializeField] private int _cost;
+
+        private SkillNodeEntity _skillNodeEntity;
+    }
+}
