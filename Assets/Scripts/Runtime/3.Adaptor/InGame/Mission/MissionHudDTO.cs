b2989b88c83e9d5e192e68d65e9741230daf7429
ayato0913a <ayato0913a@gmail.com>
2026-05-12 16:04:12 +0900

    [ feat ] 評価ミッションのリストを追加

--- Assets/Scripts/Runtime/3.Adaptor/InGame/Mission/MissionHudDTO.cs
+++ Assets/Scripts/Runtime/3.Adaptor/InGame/Mission/MissionHudDTO.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace KillChord.Runtime.Adaptor.InGame.Mission
 {
     /// <summary>
@@ -5,13 +7,30 @@ namespace KillChord.Runtime.Adaptor.InGame.Mission
     /// </summary>
     public readonly ref struct MissionHudDTO
     {
-        public MissionHudDTO(string mainMissionText, string resultText)
+        /// <summary>
+        ///     MissionHudDTO クラスの新しいインスタンスを初期化します。
+        /// </summary>
+        /// <param name="mainMissionText"> メインミッションの説明テキスト。 </param>
+        /// <param name="resultText"> ミッションの結果を示すテキスト。 </param>
+        /// <param name="evaluationItems"> ミッション評価項目のコレクション。各項目は読み取り専用で渡されます。 </param>
+        public MissionHudDTO(
+            string mainMissionText,
+            string resultText,
+            ReadOnlySpan<MissionEvaluationItemDTO> evaluationItems
+            )
         {
             MainMissionText = mainMissionText;
             ResultText = resultText;
+            EvaluationItems = evaluationItems;
         }
 
+        /// <summary> メインミッションの説明文。 </summary>
         public string MainMissionText { get; }
+
+        /// <summary> ミッションの結果文。 </summary>
         public string ResultText { get; }
+
+        /// <summary> 評価ミッションの項目リスト。 </summary>
+        public ReadOnlySpan<MissionEvaluationItemDTO> EvaluationItems { get; }
     }
 }
