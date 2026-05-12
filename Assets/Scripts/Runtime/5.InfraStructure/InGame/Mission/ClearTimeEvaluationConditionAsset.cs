90eb7ad0fb1046888ecd4f6952b914f04838a51f
ayato0913a <ayato0913a@gmail.com>
2026-05-12 17:06:54 +0900

    [ fix ] サマリやコメント付け

--- Assets/Scripts/Runtime/5.InfraStructure/InGame/Mission/ClearTimeEvaluationConditionAsset.cs
+++ Assets/Scripts/Runtime/5.InfraStructure/InGame/Mission/ClearTimeEvaluationConditionAsset.cs
@@ -4,19 +4,30 @@ using UnityEngine;
 
 namespace KillChord.Runtime.InfraStructure.InGame.Mission
 {
+    /// <summary>
+    ///     クリアタイム評価条件のアセットクラス。
+    /// </summary>
     [Serializable]
     public class ClearTimeEvaluationConditionAsset : MissionEvaluationConditionAssetBase
     {
+        /// <summary>
+        ///     評価条件を生成します。
+        /// </summary>
+        /// <returns>評価条件。</returns>
         public override IMissionEvaluationCondition Create()
         {
             return new ClearTimeEvaluationCondition(_clearTime, GetDisplayText());
         }
 
+        /// <summary>
+        ///     サマリーを構築します。
+        /// </summary>
+        /// <returns>サマリー文字列。</returns>
         protected override string BuildSummary()
         {
             return $"クリアタイムが{_clearTime}秒以下である条件";
         }
 
-        [SerializeField] private float _clearTime;
+        [SerializeField, Tooltip("この秒数以内にクリアすると条件達成となります。")] private float _clearTime;
     }
 }
