b939d6fcd47f156628acee38dcfd873d96a6af91
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2026-05-13 19:53:34 +0900

    [add]charactoranimationstate

---
+++ Assets/Scripts/Runtime/1.Domain/InGame/Animation/CharacterAnimationState.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace KillChord.Runtime.Domain
+{
+    /// <summary>
+    ///     アニメーション状態を表すドメインクラス。
+    /// </summary>
+    public enum CharacterAnimationState
+    {
+        Idle = 0,
+        Walk = 1,
+    }
+}
