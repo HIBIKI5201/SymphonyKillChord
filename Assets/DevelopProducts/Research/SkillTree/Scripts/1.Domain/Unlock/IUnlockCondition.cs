eadb46673791e091dfc180840dadf5a1d0526ea2
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-05-16 13:27:26 +0900

    [delete]解放条件インターフェースまたは実装クラス全削除

--- Assets/DevelopProducts/Research/SkillTree/Scripts/1.Domain/Unlock/IUnlockCondition.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace DevelopProducts.SkillTree
-{
-    public interface IUnlockCondition
-    {
-        bool IsSatisfied(SkillNodeEntity skillNodeEntity, SkillTreeEntity skillTreeEntity);
-    }
-}
