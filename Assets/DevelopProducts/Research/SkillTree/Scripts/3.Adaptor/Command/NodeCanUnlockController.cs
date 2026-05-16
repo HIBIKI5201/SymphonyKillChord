5bd8c117666e1dd94f62a3462226f6c336cb25e5
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-05-16 13:23:01 +0900

    [update]ノードにアルゴリズムを持たせたため変数消した

--- Assets/DevelopProducts/Research/SkillTree/Scripts/3.Adaptor/Command/NodeCanUnlockController.cs
+++ Assets/DevelopProducts/Research/SkillTree/Scripts/3.Adaptor/Command/NodeCanUnlockController.cs
@@ -5,13 +5,11 @@ namespace DevelopProducts.SkillTree
     public class NodeCanUnlockController
     {
         public NodeCanUnlockController(ISkillTreeRepository skillTreeRepository,
-                                       IAlgorithmService algorithmService,
                                        CanUnlockUsecase unlockUsecase,
                                        SkillNodePresenter presenter,
                                        SkillTreeEntity skillTree)
         {
             _skillTreeRepository = skillTreeRepository;
-            _algorithmService = algorithmService;
             _unlockUsecase = unlockUsecase;
             _skillsNodePresenter = presenter;
             _skillTree = skillTree;
@@ -19,13 +17,12 @@ namespace DevelopProducts.SkillTree
         public void CanUnlock(int nodeId)
         {
             var node = _skillTreeRepository.GetNode(nodeId);
-            var path = _algorithmService.FindPath(node, _skillTree);
+            var path = node.AlgorithmService.FindPath(node, _skillTree);
             var canUnlock = _unlockUsecase.CheckUnlock(path.TotalCost.Cost);
 
             _skillsNodePresenter.CanUnlock(nodeId, canUnlock);
         }
         private readonly ISkillTreeRepository _skillTreeRepository;
-        private readonly IAlgorithmService _algorithmService;
         private readonly CanUnlockUsecase _unlockUsecase;
         private readonly SkillNodePresenter _skillsNodePresenter;
         private readonly SkillTreeEntity _skillTree;
