ff9b75bf6372a6be8b4422636f786f3d1ae844c2
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-05-16 13:27:49 +0900

    [add]ノードのViewです

--- Assets/DevelopProducts/Research/SkillTree/Scripts/4.View/View/NodeSelectPanelView.cs
+++ Assets/DevelopProducts/Research/SkillTree/Scripts/4.View/View/NodeSelectPanelView.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,20 +7,58 @@ namespace DevelopProducts.SkillTree
 {
     public class NodeSelectPanelView : MonoBehaviour
     {
-
-        public void SetNode()
+        public void Initialize(NodeUnlockController nodeUnlockController, 
+            NodeCanUnlockController nodeCanUnlockController,
+            SkillNodePresenter skillNodePresenter)
         {
-
+            _nodeUnlockController = nodeUnlockController;
+            _nodeCanUnlockController = nodeCanUnlockController;
+            _skillNodePresenter = skillNodePresenter;
         }
 
-        [SerializeField]private Button _unlockButton;
+        public void SetNode(NodeView nodeView)
+        {
+            _currentNodeCost = _skillNodePresenter.GetTotalCost(nodeView.Id);
+            _nodeNameText.text = $"必要コスト {_currentNodeCost}";
+            _currentNodeId = nodeView.Id;
+        }
+        [SerializeField] private TMP_Text _nodeNameText;
+        [SerializeField] private Button _unlockButton;
+        [SerializeField] private SkillTreeRepository _skillTreeRepository;
+        private int _currentNodeId = -1;
+        private int _currentNodeCost = 0;
+        private NodeUnlockController _nodeUnlockController;
+        private NodeCanUnlockController _nodeCanUnlockController;
+        private SkillNodePresenter _skillNodePresenter;
         private void OnUnlockButtonClicked()
         {
-            // Unlock button click logic here
+            _nodeUnlockController.UnlockNode(_currentNodeId);
+            CanUnlockNodes();
+        }
+        private void CanUnlockNodes()
+        {
+            var enableNodes = _skillTreeRepository.AllSkillNodes.Where(n => n.IsEnable && !n.IsUnlocked);
+            foreach (var node in enableNodes)
+            {
+                _nodeCanUnlockController.CanUnlock(node.SkillNodeIdVO.Id);
+            }
         }
         private void Awake()
         {
             _unlockButton.onClick.AddListener(OnUnlockButtonClicked);
+        }
+        private void Start()
+        {
+            var unlockedNodes = _skillTreeRepository.AllSkillNodes.Where(n => n.IsUnlocked);
+            foreach (var node in unlockedNodes)
+            {
+                _nodeUnlockController.UnlockNode(node.SkillNodeIdVO.Id);
+            }
+            var enableNodes = _skillTreeRepository.AllSkillNodes.Where(n => n.IsEnable && !n.IsUnlocked);
+            foreach (var node in enableNodes)
+            {
+                _nodeCanUnlockController.CanUnlock(node.SkillNodeIdVO.Id);
+            }
         }
         private void OnDestroy()
         {
