a49df547b46eecfb63669b23fd5b55059eeb754e
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-05-16 13:24:03 +0900

    [add]NodeEntityの情報をＩＤで結びついてるＶＭに渡すプレゼンタ

--- Assets/DevelopProducts/Research/SkillTree/Scripts/3.Adaptor/Query/SkillNodePresenter.cs
+++ Assets/DevelopProducts/Research/SkillTree/Scripts/3.Adaptor/Query/SkillNodePresenter.cs
@@ -2,17 +2,40 @@ namespace DevelopProducts.SkillTree
 {
     public class SkillNodePresenter
     {
-        public SkillNodePresenter(NodeRegistry nodeRegistry)
+        public SkillNodePresenter(NodeRegistry nodeRegistry,
+            SkillTreeEntity skillTree,
+            ISkillTreeRepository skillTreeRepository)
         {
             _nodeRegistry = nodeRegistry;
+            _skillTree = skillTree;
+            _skillTreeRepository = skillTreeRepository;
         }
         public void CanUnlock(int nodeId, bool canUnlock)
         {
             if (_nodeRegistry.TryGet(nodeId, out var canUnlockVM))
             {
-                canUnlockVM.Push(new CanUnlockDTO(canUnlock));
+                canUnlockVM.Check(new CanUnlockDTO(canUnlock));
             }
         }
+        public void Unlock(int nodeId, bool isUnlock)
+        {
+            if (_nodeRegistry.TryGet(nodeId, out var canUnlockVM))
+            {
+                canUnlockVM.Unlock(new UnlockDTO(isUnlock));
+            }
+        }
+        public int GetTotalCost(int nodeId)
+        {
+            if (_nodeRegistry.TryGet(nodeId, out var canUnlockVM))
+            {
+                var node = _skillTreeRepository.GetNode(nodeId);
+                var pathResult = node.AlgorithmService.FindPath(node, _skillTree);
+                return pathResult.TotalCost.Cost;
+            }
+            return 0;
+        }
         private readonly NodeRegistry _nodeRegistry;
+        private readonly SkillTreeEntity _skillTree;
+        private readonly ISkillTreeRepository _skillTreeRepository;
     }
 }
