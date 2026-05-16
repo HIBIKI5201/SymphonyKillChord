8748d4b1165b8cd0a3f670d25731f66b28a3a17c
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-05-16 13:29:13 +0900

    [update]引き算メソッド実装

--- Assets/DevelopProducts/Research/SkillTree/Scripts/5.Infrastructure/SkillPointRepository.cs
+++ Assets/DevelopProducts/Research/SkillTree/Scripts/5.Infrastructure/SkillPointRepository.cs
@@ -10,6 +10,11 @@ namespace DevelopProducts.SkillTree
             return new UnlockPoint(_point);
         }
 
+        public void UsePoints(int points)
+        {
+            _point = _point - points;
+        }
+
         [SerializeField] private int _point;
     }
 }
