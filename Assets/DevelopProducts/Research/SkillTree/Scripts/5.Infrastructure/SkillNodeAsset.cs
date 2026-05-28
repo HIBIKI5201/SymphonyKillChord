6d2be3b3076473dad49c46c1fc473ff405bf2bfd
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2026-05-28 20:30:33 +0900

    [update]スキルを取得する報酬とパラメーターを強化する報酬に分けた

--- Assets/DevelopProducts/Research/SkillTree/Scripts/5.Infrastructure/SkillNodeAsset.cs
+++ Assets/DevelopProducts/Research/SkillTree/Scripts/5.Infrastructure/SkillNodeAsset.cs
@@ -8,16 +8,20 @@ namespace DevelopProducts.SkillTree
     {
         public int Id => _id;
         public SkillNodeEntity SkillNodeEntity => _skillNodeEntity;
-        public INodeUnlockEffect[] NodeUnlockEffects => _nodeUnlockEffets;
+        public IParameterUpgradeEffect[] ParameterUpgradeEffects => _nodeUnlockEffets;
         public SkillNodeAsset[] Parents => _parents;
         public int Cost => _cost;
 
         public SkillNodeEntity ToDomain()
         {
-            _skillNodeEntity = new SkillNodeEntity(_id, _cost, _nodeUnlockEffets, _algorithmService, _isUnlocked, _isEnable);
+            _skillNodeEntity = new SkillNodeEntity(_id, _cost, _algorithmService, _isUnlocked, _isEnable);
             return _skillNodeEntity;
         }
-        [SerializeReference, SubclassSelector] private INodeUnlockEffect[] _nodeUnlockEffets;
+        [Header("ノードを解放した時の報酬")]
+        [SerializeReference, SubclassSelector] private IParameterUpgradeEffect[] _nodeUnlockEffets;
+        [SerializeReference, SubclassSelector] private ISkillUnlockEffect[] _skillUnlockEffets;
+
+        [Header("解放するにあたっての必要条件")]
         [SerializeReference, SubclassSelector] private IAlgorithmService _algorithmService;
 
         [SerializeField] private int _id;
