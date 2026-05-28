7b4622e519d5947f6e0855f86adff589f07b6444
ChouTakushin <takushin.chou@gmail.com>
2026-05-29 02:00:46 +0900

    [fix] DevelopProducts古いコードがあり、存在しなくなったソースを参照してエラーを起こしているため、削除する

--- Assets/DevelopProducts/Research/BehaviorGraph/Scripts/Runtime/3.Adaptor/InGame/Battle/IViewModelDamage.cs
+++
@@ -1,7 +0,0 @@
-namespace DevelopProducts.BehaviorGraph.Runtime.Adaptor.InGame.Battle
-{
-    public interface IViewModelDamage
-    {
-        public void OnDamage(DamageDTO dto);
-    }
-}
--- Assets/DevelopProducts/Research/BehaviorGraph/Scripts/Runtime/3.Adaptor/InGame/UI/IIngameHudViewModel.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace DevelopProducts.BehaviorGraph.Runtime.Adaptor
-{
-    public interface IIngameHudViewModel
-    {
-        public void UpdateHealth(in IngameHudDTO dto);
-    }
-}
