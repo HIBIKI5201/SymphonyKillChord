6ce02188f11145b86870b93078419671a537e7e3
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2026-05-29 13:25:17 +0900

    [update]メソッド追加

--- Assets/DevelopProducts/Research/SkillTree/Scripts/2.Application/ISkillTreeRepository.cs
+++ Assets/DevelopProducts/Research/SkillTree/Scripts/2.Application/ISkillTreeRepository.cs
@@ -1,11 +1,14 @@
 using System.Collections.Generic;
-using UnityEngine;
 
 namespace DevelopProducts.SkillTree
 {
     public interface ISkillTreeRepository
     {
+        SkillNodeEntity[] AllSkillNodes { get; }
+        int PhaseCount { get; }
         SkillNodeEntity GetNode(int id);
         IReadOnlyList<SkillNodeEntity> GetParentNodes(int id);
+
+        public IReadOnlyList<SkillNodeEntity> GetNodesByPhase(int phase);
     }
 }
