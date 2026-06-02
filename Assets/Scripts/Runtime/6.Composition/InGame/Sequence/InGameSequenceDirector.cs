32152190176364d7463d093bcc45e1759bf075ea
ayato0913a <ayato0913a@gmail.com>
2026-06-02 20:43:57 +0900

    [ add ] 非同期 + 任意のタイミングでの再生、開始するクラスを作成

---
+++ Assets/Scripts/Runtime/6.Composition/InGame/Sequence/InGameSequenceDirector.cs
@@ -0,0 +1,60 @@
+using KillChord.Runtime.View.InGame.Sequence;
+using System.Threading;
+using UnityEngine;
+
+namespace KillChord.Runtime.Composition.InGame.Sequence
+{
+    public class InGameSequenceDirector
+    {
+        public InGameSequenceDirector(
+            StageSequenceView stageSequenceView, 
+            StageResultUIView stageResultUIView, 
+            IGameplayControllable gameplayControllable)
+        {
+            _stageSequenceView = stageSequenceView;
+            _stageResultUIView = stageResultUIView;
+            _gameplayControllable = gameplayControllable;
+        }
+
+        public async Awaitable StartAsync(CancellationToken cancellationToken)
+        {
+            _gameplayControllable.StopGameplay();
+            _stageResultUIView?.Hide();
+
+            if(_stageSequenceView != null)
+            {
+                await _stageSequenceView.PlayStageStartAsync(cancellationToken);
+            }
+
+            _gameplayControllable.StartGameplay();
+        }
+
+        public async Awaitable ClearAsync(CancellationToken cancellationToken)
+        {
+            _gameplayControllable.StopGameplay();
+
+            if (_stageSequenceView != null)
+            {
+                await _stageSequenceView.PlayStageClearAsync(cancellationToken);
+            }
+
+            _stageResultUIView?.ShowClear();
+        }
+
+        public async Awaitable GameOverAsync(CancellationToken cancellationToken)
+        {
+            _gameplayControllable.StopGameplay();
+
+            if (_stageSequenceView != null)
+            {
+                await _stageSequenceView.PlayGameOverAsync(cancellationToken);
+            }
+
+            _stageResultUIView?.ShowGameOver();
+        }
+
+        private readonly StageSequenceView _stageSequenceView;
+        private readonly StageResultUIView _stageResultUIView;
+        private readonly IGameplayControllable _gameplayControllable;
+    }
+}
