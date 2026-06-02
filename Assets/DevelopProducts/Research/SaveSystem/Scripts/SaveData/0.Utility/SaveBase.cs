c2571e4bf235109d5c2ad1976c31d5b4fb18a2c4
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-06-02 17:00:28 +0900

    [add]セーブシステム追加

--- Assets/DevelopProducts/Research/SaveSystem/Scripts/SaveData/0.Utility/SaveBase.cs
+++ Assets/DevelopProducts/Research/SaveSystem/Scripts/SaveData/0.Utility/SaveBase.cs
@@ -1,6 +1,24 @@
+using System.IO;
+using System.Threading.Tasks;
 using UnityEngine;
 
-public class SaveBase
+namespace DevelopProducts.SaveSystem
 {
-    
+    public abstract class SaveBase
+    {
+        internal async ValueTask ReadAsync()
+        {
+            if (!File.Exists(FilePath)) return;
+
+            var json = await File.ReadAllTextAsync(FilePath);
+            JsonUtility.FromJsonOverwrite(json, this);
+        }
+        internal async ValueTask WriteAsync()
+        {
+            var json = JsonUtility.ToJson(this, prettyPrint: true);
+            await File.WriteAllTextAsync(FilePath, json);
+        }
+        protected virtual string SaveKey => GetType().Name;
+        private string FilePath => Path.Combine(Application.persistentDataPath, $"{SaveKey}.json");
+    }
 }
