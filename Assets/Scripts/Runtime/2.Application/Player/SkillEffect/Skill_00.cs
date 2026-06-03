80d33f8f39c815456a493519934012ce2c3ee3f8
kenkatou354 <kenkatou354@gmail.com>
2026-06-03 13:44:02 +0900

    [ add ] スキルログを追加。

--- Assets/Scripts/Runtime/2.Application/Player/SkillEffect/Skill_00.cs
+++ Assets/Scripts/Runtime/2.Application/Player/SkillEffect/Skill_00.cs
@@ -13,6 +13,7 @@ namespace KillChord.Runtime.Application.Player.SkillEffect
         {
             Damage damage =  new Damage(context.PlayerBaseAttackPower * 5f); //基礎攻撃力の500%に相当するダメージを計算する例
             context.TargetEntity.TakeDamage(damage); //ターゲットに基礎攻撃力の10%に相当するダメージを与える例
+            Debug.Log($"<color=green>Skill_00 を実行しました:{damage.Value}ダメージです。 </color>");
         }
     }
 }
