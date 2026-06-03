ffa1c142e27b098723613e8296c7aa66a5e4492b
kenkatou354 <kenkatou354@gmail.com>
2026-06-03 13:37:14 +0900

    [ add ] プレイヤーの基礎攻撃力を追加。

--- Assets/Scripts/Runtime/1.Domain/Player/SkillEffectContext.cs
+++ Assets/Scripts/Runtime/1.Domain/Player/SkillEffectContext.cs
@@ -8,12 +8,19 @@ namespace KillChord.Runtime.Domain.Player
     /// </summary>
     public readonly struct SkillEffectContext
     {
-        public SkillEffectContext( CharacterEntity targetEntity)
+        public SkillEffectContext( CharacterEntity targetEntity, float playerBaseAttackPower)
         {
             TargetEntity = targetEntity;
+            PlayerBaseAttackPower = playerBaseAttackPower;
         }
-
+        /// <summary>
+        /// スキルの効果が対象とするキャラクターエンティティ。スキル効果の実行に必要な情報を提供する。
+        /// </summary>
         public CharacterEntity TargetEntity { get; }
+        /// <summary>
+        /// プレイヤーの基礎攻撃力。スキル効果の計算に使用される。
+        /// </summary>
+        public float PlayerBaseAttackPower { get; } 
 
 
     }
