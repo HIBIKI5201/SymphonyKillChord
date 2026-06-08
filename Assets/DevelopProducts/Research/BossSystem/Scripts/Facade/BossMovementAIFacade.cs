e903bc4e50481df736f90a062d1a5f260fe24fa5
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-06-08 18:24:56 +0900

    [add]ボス用の動きを管理するFacade

---
+++ Assets/DevelopProducts/Research/BossSystem/Scripts/Facade/BossMovementAIFacade.cs
@@ -0,0 +1,35 @@
+using KillChord.Runtime.Adaptor.InGame.Enemy.EnemyAIFacadeInterface;
+using UnityEngine;
+
+namespace DevelopProducts.Boss
+{
+    /// <summary>
+    ///     ボスAI用ファサード：移動系。EnemyMovementAIFacade のボス専用複製。
+    /// </summary>
+    public class BossMovementAIFacade : MonoBehaviour, IEnemyMovementAIFacade
+    {
+        /// <summary>
+        ///     初期化処理。
+        /// </summary>
+        public void Initialize(BossMoveView moveView)
+        {
+            _moveView = moveView;
+        }
+
+        /// <summary> 指示：攻撃可能な位置に移動する。 </summary>
+        public void MoveToAttack()
+        {
+            if (_moveView == null) return;
+            _moveView.MoveToAttack();
+        }
+
+        /// <summary> 指示：移動を停止する。 </summary>
+        public void StopMoving()
+        {
+            if (_moveView == null) return;
+            _moveView.StopMoving();
+        }
+
+        private BossMoveView _moveView;
+    }
+}
