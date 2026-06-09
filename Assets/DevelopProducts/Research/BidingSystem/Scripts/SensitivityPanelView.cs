87f184ce39b2e5872455aa14b00f4c19c7901f89
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2026-06-09 20:02:09 +0900

    [add]クラス追加

---
+++ Assets/DevelopProducts/Research/BidingSystem/Scripts/SensitivityPanelView.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+namespace DevelopProducts.BindingSystem
+{
+    public class SensitivityPanelView : MonoBehaviour,IPanelView
+    {
+        public void BackToTheDefault()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void BackToTheInitial()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetPanelActive(bool active)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
