1c4065281c12e8219d85fdc9ad650e0747a1c23b
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2026-06-11 12:02:11 +0900

    [update]階層移動

--- Assets/DevelopProducts/Research/BidingSystem/Scripts/View/OtherSettingPanelView.cs
+++ Assets/DevelopProducts/Research/BidingSystem/Scripts/View/OtherSettingPanelView.cs
@@ -18,7 +18,7 @@ namespace DevelopProducts.BindingSystem
         {
             throw new System.NotImplementedException();
         }
-    {
+    
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
