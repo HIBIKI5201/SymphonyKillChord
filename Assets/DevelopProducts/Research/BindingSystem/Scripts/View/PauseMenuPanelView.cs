24deca8bced4fc1c4bc578932bda7ad5a03ccf28
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2026-06-15 18:36:41 +0900

    [add]パネル

--- Assets/DevelopProducts/Research/BindingSystem/Scripts/View/PauseMenuPanelView.cs
+++ Assets/DevelopProducts/Research/BindingSystem/Scripts/View/PauseMenuPanelView.cs
@@ -9,14 +9,29 @@ namespace DevelopProducts.BindingSystem
     /// </summary>
     public class PauseMenuPanelView : MonoBehaviour
     {
-
+        public CriAtom CriAtom => _criAtom;
         private void Awake()
         {
             foreach (var panel in _settingPanels)
             {
                 panel.PanelView.Initialize(this);
+                panel.Button.onClick.AddListener(() => SwitchPanel(panel.PanelView));
             }
-            _currentSettingPanel = _settingPanels[0].PanelView;
+            SwitchPanel(_settingPanels[0].PanelView);
+        }
+
+        /// <summary>
+        ///     表示するパネルを切り替える。それまで表示していたパネルは非表示にする。
+        /// </summary>
+        /// <param name="panel"></param>
+        private void SwitchPanel(SettingPanelView panel)
+        {
+            if (_currentSettingPanel != null)
+            {
+                _currentSettingPanel.SetPanelActive(false);
+            }
+            panel.SetPanelActive(true);
+            _currentSettingPanel = panel;
         }
         [SerializeField] private InputActionAsset _inputActionAsset;
         [SerializeField] private CriAtom _criAtom;
