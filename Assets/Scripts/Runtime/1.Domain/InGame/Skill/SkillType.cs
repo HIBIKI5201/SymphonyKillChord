507097f97c5b8f0aecb46cda6f62d8234ae1a05a
YamamiyaShota <dragonsyouta10ys@gmail.com>
2026-06-18 15:19:35 +0900

    [add] スキルの種類を表す列挙型を作成。

---
+++ Assets/Scripts/Runtime/1.Domain/InGame/Skill/SkillType.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace KillChord.Runtime.Domain.InGame.Skill
+{
+    /// <summary>
+    ///    スキルの種類を表す列挙型。
+    /// </summary>
+    public enum SkillType
+    {
+    
+    }
+}
