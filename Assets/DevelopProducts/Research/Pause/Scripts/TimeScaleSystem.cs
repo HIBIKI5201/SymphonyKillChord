45c2da869857cdf94796f8d5ea9496d56c80c890
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-06-20 13:05:56 +0900

    [add]とりあえずのITimeScalableのTickを呼び出すクラス

--- Assets/DevelopProducts/Research/Pause/Scripts/TimeScaleSystem.cs
+++ Assets/DevelopProducts/Research/Pause/Scripts/TimeScaleSystem.cs
@@ -1,12 +1,33 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace DevelopProducts.Pause
 {
+    /// <summary>
+    ///     ITimeScalableオブジェクトのライフサイクル管理を行うクラス
+    /// </summary>
     public class TimeScaleSystem : MonoBehaviour
     {
-        void Update()
+        public static TimeScaleManager TimeScaleManager { get; } = new();
+
+        private static TimeScaleSystem _instance;
+
+        private void Awake()
         {
-        
+            if (_instance != null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            _instance = this;
+        }
+
+        private void Update()
+        {
+            foreach (var obj in TimeScaleManager.GetAll())
+            {
+                obj.Tick();
+            }
         }
     }
 }
