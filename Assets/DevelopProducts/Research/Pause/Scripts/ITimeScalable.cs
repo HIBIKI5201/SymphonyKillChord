bd77905bdea084000414ee03fc163f0c1ab8d395
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-06-20 13:04:45 +0900

    [add]メソッド追加

--- Assets/DevelopProducts/Research/Pause/Scripts/ITimeScalable.cs
+++ Assets/DevelopProducts/Research/Pause/Scripts/ITimeScalable.cs
@@ -5,7 +5,9 @@ namespace DevelopProducts.Pause
     /// </summary>
     public interface ITimeScalable
     {
+        public int InstanceId { get; }
         public TimeScaleType TimeScaleType { get; }
         public void Inject(TimeScaleData data);
+        public void Tick();
     }
 }
