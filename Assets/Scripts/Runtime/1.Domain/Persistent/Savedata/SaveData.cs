a53db6c2b45ced9a3d6b3dbc6571378dd84f2e36
YamamiyaShota <153970961+YamamiyaShota@users.noreply.github.com>
2026-06-23 15:01:10 +0900

    [fix] セーブデータを読み込んだ後のフックを追加し、 null 検証を行い、必要に応じて初期化する処理を追加。

--- Assets/Scripts/Runtime/1.Domain/Persistent/Savedata/SaveData.cs
+++ Assets/Scripts/Runtime/1.Domain/Persistent/Savedata/SaveData.cs
@@ -11,9 +11,18 @@ namespace KillChord.Runtime.Domain.Persistent.Savedata
     public sealed class SaveData : SaveBase
     {
         /// <summary> プレイヤーのスキル解放情報のセーブデータを表すプロパティ。 </summary>
-        public SkillUnlockData SkillUnlock = new();
+        public SkillUnlockData SkillUnlock { get; private set; } = new();
 
         /// <summary> プレイヤーの装備スキル構成のセーブデータを表すプロパティ。 </summary>
-        public SkillBuildData SkillBuild = new();
+        public SkillBuildData SkillBuild { get; private set; } = new();
+
+        /// <summary>
+        ///     セーブデータを読み込んだ後に null チェックを行い、必要に応じて初期化する。
+        /// </summary>
+        protected override void OnAfterDeserialize()
+        {
+            SkillUnlock ??= new();
+            SkillBuild ??= new();
+        }
     }
 }
