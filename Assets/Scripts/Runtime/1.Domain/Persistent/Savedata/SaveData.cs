a63cb3e10c686dc0ecddca0caec40aac252e1383
YamamiyaShota <153970961+YamamiyaShota@users.noreply.github.com>
2026-06-28 15:25:45 +0900

    [update] SaveData クラスに TutorialData を追加。

--- Assets/Scripts/Runtime/1.Domain/Persistent/Savedata/SaveData.cs
+++ Assets/Scripts/Runtime/1.Domain/Persistent/Savedata/SaveData.cs
@@ -1,5 +1,6 @@
 using KillChord.Runtime.Utility.OutGame.Savedata;
 using System;
+using UnityEngine;
 
 namespace KillChord.Runtime.Domain.Persistent.Savedata
 {
@@ -16,6 +17,16 @@ namespace KillChord.Runtime.Domain.Persistent.Savedata
         /// <summary> プレイヤーの装備スキル構成のセーブデータを表すプロパティ。 </summary>
         public SkillBuildData SkillBuild { get; private set; } = new();
 
+        /// <summary> プレイヤーのチュートリアル進行状況のセーブデータを表すプロパティ。 </summary>
+        public TutorialData Tutorial
+        {
+            get => _tutorial;
+            set => _tutorial = value ?? throw new ArgumentNullException(nameof(value), "TutorialData は null にできません。");
+        }
+
+        [SerializeField, Tooltip("プレイヤーのチュートリアル進行状況のセーブデータ")]
+        private TutorialData _tutorial;
+
         /// <summary>
         ///     セーブデータを読み込んだ後に null チェックを行い、必要に応じて初期化する。
         /// </summary>
@@ -23,6 +34,7 @@ namespace KillChord.Runtime.Domain.Persistent.Savedata
         {
             SkillUnlock ??= new();
             SkillBuild ??= new();
+            _tutorial ??= new();
         }
     }
 }
