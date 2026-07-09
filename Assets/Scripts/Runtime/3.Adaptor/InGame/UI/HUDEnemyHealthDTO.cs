92226953a59b3bd5ebb3ebd98793442fff712bef
Oikawa <yucchi0424@gmail.com>
2026-07-09 15:50:20 +0900

    [update] HUDEnemyHealthに敵追従とシェイクを実装

--- Assets/Scripts/Runtime/3.Adaptor/InGame/UI/HUDEnemyHealthDTO.cs
+++ Assets/Scripts/Runtime/3.Adaptor/InGame/UI/HUDEnemyHealthDTO.cs
@@ -1,14 +1,18 @@
+using UnityEngine;
+
 namespace KillChord.Runtime.Adaptor.InGame.UI
 {
     public readonly ref struct HUDEnemyHealthDTO
     {
-        public HUDEnemyHealthDTO(float currentHealth, float maxHealth, bool isLockon)
+        public HUDEnemyHealthDTO(float currentHealth, float maxHealth, bool isLockon, in Vector3 targetPosition)
         {
             CurrentHealth = currentHealth;
             MaxHealth = maxHealth;
             IsLockon = isLockon;
+            TargetPosition = targetPosition;
         }
 
+        public readonly Vector3 TargetPosition;
         public readonly float CurrentHealth;
         public readonly float MaxHealth;
         public readonly bool IsLockon;
