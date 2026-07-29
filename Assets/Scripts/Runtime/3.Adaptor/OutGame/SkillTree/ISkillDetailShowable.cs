e9f01a85acf62a8aa406ac726f652a0f1dafb4c6
Oikawa <yucchi0424@gmail.com>
2026-07-29 11:21:38 +0900

    [fix] コメントの誤字修正

--- Assets/Scripts/Runtime/3.Adaptor/OutGame/SkillTree/ISkillDetailShowable.cs
+++ Assets/Scripts/Runtime/3.Adaptor/OutGame/SkillTree/ISkillDetailShowable.cs
@@ -4,12 +4,12 @@ using System.Threading.Tasks;
 namespace KillChord.Runtime.Adaptor.OutGame.SkillTree
 {
     /// <summary>
-    ///     スキル詳細面面の表示用インタフェース。
+    ///     スキル詳細画面の表示用インタフェース。
     /// </summary>
     public interface ISkillDetailShowable
     {
         /// <summary>
-        ///     面面を表示します。
+        ///     画面を表示します。
         /// </summary>
         ValueTask Show(CancellationToken cancellationToken = default);
     }
