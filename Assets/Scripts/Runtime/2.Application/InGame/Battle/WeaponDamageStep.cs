3372bac78711f3100b5fd15c6e4f1ceb4735df4d
reitaghchi <ren.len2426@gmail.com>
2026-08-05 12:25:16 +0900

    [add]上げ忘れ

---
+++ Assets/Scripts/Runtime/2.Application/InGame/Battle/WeaponDamageStep.cs
@@ -0,0 +1,28 @@
+using KillChord.Runtime.Domain.InGame.Battle;
+
+namespace KillChord.Runtime.Application.InGame.Battle
+{
+    /// <summary>
+    ///     武器ごとのダメージ倍率を適用する攻撃処理ステップ。
+    ///     拍子ごとの倍率は攻撃定義データが持つため、このステップは拍子の数値を参照しない。
+    /// </summary>
+    public class WeaponDamageStep : IAttackStep
+    {
+        /// <summary>
+        ///     攻撃処理ステップを実行する。
+        /// </summary>
+        /// <param name="context"> 攻撃処理の文脈。 </param>
+        /// <returns> 倍率を適用した攻撃処理の文脈。 </returns>
+        public AttackStepContext Execute(in AttackStepContext context)
+        {
+            float resultDamage = context.Damage.Value * context.AttackDefinition.WeaponDamageMultiplier;
+
+            if (context.IsJustHit)
+            {
+                resultDamage *= context.AttackDefinition.JustDamageMultiplier;
+            }
+
+            return new AttackStepContext(new Damage(resultDamage), context.CriticalCount, context);
+        }
+    }
+}
