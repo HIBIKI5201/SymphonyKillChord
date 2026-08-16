3022c713759ad09ed74848a2d36a2557a8b148f5
Oikawa <yucchi0424@gmail.com>
2026-08-16 19:22:46 +0900

    [remove / add / update] CameraShakeパラメータをScrObjに分けた。

--- Assets/Scripts/Runtime/4.View/InGame/Camera/Calculation/CameraShakeCalculator.cs
+++ Assets/Scripts/Runtime/4.View/InGame/Camera/Calculation/CameraShakeCalculator.cs
@@ -15,35 +15,36 @@ namespace KillChord.Runtime.View.InGame.Camera
         ///     シェイクの発生を要求する。
         ///     発生中のシェイクより弱い要求は、演出が弱まらないよう無視する。
         /// </summary>
-        /// <param name="parameter"> 要求するシェイクのパラメータ。</param>
-        public void RequestShake(in CameraShakeParameter parameter)
+        /// <param name="config"> 要求するシェイクの設定。</param>
+        public void RequestShake(CameraShakeConfig config)
         {
+            if (config == null)
+            {
+                return;
+            }
+
             // 継続時間か強さが無い設定は演出として成立しないため無視する。
-            if (parameter.Priority < _currentPriority)
+            if (config.Priority < _currentPriority)
             {
                 return;
             }
 
             // 発生中のモーションを破棄してから、新しいシェイクへ差し替える。
             Reset();
-            float duration = parameter.Duration;
             _positionHandle = LSequence.Create()
-                .Join(LMotion.Punch.Create(0f, Random.Range(parameter.StrengthRangeX.x, parameter.StrengthRangeX.y), parameter.Duration)
-                    .WithFrequency(Mathf.RoundToInt(Random.Range( parameter.Frequency.x, parameter.Frequency.y)))
-                    .WithDampingRatio(parameter.DampingRatio)
+                .Join(LMotion.Punch.Create(0f, Random.Range(config.StrengthRangeX.x, config.StrengthRangeX.y), config.Duration)
+                    .WithFrequency(Mathf.RoundToInt(Random.Range(config.Frequency.x, config.Frequency.y)))
                     .Bind(this, static (value, state) => state._positionOffset.x = value))
 
-                .Join(LMotion.Punch.Create(0f, Random.Range(parameter.StrengthRangeY.x, parameter.StrengthRangeY.y), parameter.Duration)
-                    .WithFrequency(Mathf.RoundToInt(Random.Range(parameter.Frequency.x, parameter.Frequency.y)))
-                    .WithDampingRatio(parameter.DampingRatio)
+                .Join(LMotion.Punch.Create(0f, Random.Range(config.StrengthRangeY.x, config.StrengthRangeY.y), config.Duration)
+                    .WithFrequency(Mathf.RoundToInt(Random.Range(config.Frequency.x, config.Frequency.y)))
                     .Bind(this, static (value, state) => state._positionOffset.y = value))
 
-                .Join(LMotion.Punch.Create(0f, Random.Range(parameter.StrengthRangeZ.x, parameter.StrengthRangeZ.y), parameter.Duration)
-                    .WithFrequency(Mathf.RoundToInt(Random.Range(parameter.Frequency.x, parameter.Frequency.y)))
-                    .WithDampingRatio(parameter.DampingRatio)
+                .Join(LMotion.Punch.Create(0f, Random.Range(config.StrengthRangeZ.x, config.StrengthRangeZ.y), config.Duration)
+                    .WithFrequency(Mathf.RoundToInt(Random.Range(config.Frequency.x, config.Frequency.y)))
                     .Bind(this, static (value, state) => state._positionOffset.z = value))
                 .Run(x => x.WithOnComplete(() => _currentPriority = int.MinValue));
-            _currentPriority = parameter.Priority;
+            _currentPriority = config.Priority;
         }
 
         /// <summary>
--- Assets/Scripts/Runtime/4.View/InGame/Camera/CameraSystemView.cs
+++ Assets/Scripts/Runtime/4.View/InGame/Camera/CameraSystemView.cs
@@ -194,6 +194,15 @@ namespace KillChord.Runtime.View.InGame.Camera
         [SerializeField, Tooltip("モバイルのカメラ感度")]
         private int _mobileLookSensitivity = 10;
 
+        [SerializeField, Tooltip("敵を撃破した時のカメラシェイク設定")]
+        private CameraShakeConfig _enemyDefeatedShakeConfig;
+
+        [SerializeField, Tooltip("プレイヤーが攻撃を実行した時のカメラシェイク設定")]
+        private CameraShakeConfig _playerAttackShakeConfig;
+
+        [SerializeField, Tooltip("プレイヤーが被弾した時のカメラシェイク設定")]
+        private CameraShakeConfig _playerDamageShakeConfig;
+
         private PlayerInputView _inputView;
         private UnityEngine.Camera _camera;
         private Transform _playerT;
@@ -395,9 +404,7 @@ namespace KillChord.Runtime.View.InGame.Camera
         /// <param name="eventData"> 敵撃破イベント。 </param>
         private void EnemyDefeatedHandler(EOnEnemyDefeated eventData)
         {
-            if (_viewSettings == null) { return; }
-
-            RequestShake(_viewSettings.EnemyDefeatedShake);
+            RequestShake(_enemyDefeatedShakeConfig);
         }
 
         /// <summary>
@@ -406,9 +413,7 @@ namespace KillChord.Runtime.View.InGame.Camera
         /// <param name="eventData"> プレイヤー攻撃実行イベント。 </param>
         private void PlayerAttackExecutedHandler(EOnPlayerAttackExecuted eventData)
         {
-            if (_viewSettings == null) { return; }
-
-            RequestShake(_viewSettings.PlayerAttackShake);
+            RequestShake(_playerAttackShakeConfig);
         }
 
         /// <summary>
@@ -417,23 +422,21 @@ namespace KillChord.Runtime.View.InGame.Camera
         /// <param name="eventData"> プレイヤー被弾イベント。 </param>
         private void PlayerTakeDamageHandler(EOnPlayerTakeDamage eventData)
         {
-            if (_viewSettings == null) { return; }
-
-            RequestShake(_viewSettings.PlayerDamageShake);
+            RequestShake(_playerDamageShakeConfig);
         }
 
         /// <summary>
         ///     カメラシェイクの発生を要求する。
         /// </summary>
-        /// <param name="parameter"> 要求するシェイクのパラメータ。 </param>
-        private void RequestShake(in CameraShakeParameter parameter)
+        /// <param name="config"> 要求するシェイクの設定。 </param>
+        private void RequestShake(CameraShakeConfig config)
         {
             if (_shakeCalculator == null)
             {
                 return;
             }
 
-            _shakeCalculator.RequestShake(parameter);
+            _shakeCalculator.RequestShake(config);
         }
 
         /// <summary>
